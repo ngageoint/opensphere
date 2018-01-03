e877f4faf7a50ea0096b71a7ddfc845bd925f96d
William Wall <wwall@caci.com>
2018-01-03 12:30:15 -0700

    test(thread): use lolex in thread tests to prevent timing failures

--- test/os/thread/thread.test.js
+++ test/os/thread/thread.test.js
@@ -1,9 +1,21 @@
+goog.require('mock.thread.Job');
 goog.require('os.thread.EventType');
 goog.require('os.thread.Thread');
-goog.require('mock.thread.Job');
 
 
 describe('os.thread.Thread', function() {
+  var clock;
+
+  beforeEach(function() {
+    clock = lolex.install();
+    goog.Timer.defaultTimerObject = clock;
+  });
+
+  afterEach(function() {
+    clock.uninstall();
+    goog.Timer.defaultTimerObject = window;
+  });
+
   it('should execute properly', function() {
     var job = new mock.thread.Job();
     var t = new os.thread.Thread(job);
@@ -22,21 +34,14 @@ describe('os.thread.Thread', function() {
     t.listen(os.thread.EventType.PROGRESS, listener);
     t.listen(os.thread.EventType.COMPLETE, listener);
 
-    runs(function() {
-      t.start();
-    });
+    t.start();
+    clock.tick(job.total * 5);
 
-    waitsFor(function() {
-      return count.threadComplete === 1;
-    }, 'thread start and completion');
-
-    runs(function() {
-      expect(count.threadStart).toBe(1);
-      expect(count.threadProgress).toBe(3);
-      expect(count.threadComplete).toBe(1);
-      // job should not be disposed
-      expect(job.isDisposed()).toBe(false);
-    });
+    expect(count.threadStart).toBe(1);
+    expect(count.threadProgress).toBe(3);
+    expect(count.threadComplete).toBe(1);
+    // job should not be disposed
+    expect(job.isDisposed()).toBe(false);
   });
 
   it('should stop when told', function() {
@@ -57,19 +62,12 @@ describe('os.thread.Thread', function() {
     t.listen(os.thread.EventType.PROGRESS, listener);
     t.listen(os.thread.EventType.COMPLETE, listener);
 
-    runs(function() {
-      t.start();
-    });
-
-    waitsFor(function() {
-      return count.threadProgress === 2;
-    }, 'thread to get going');
-
-    runs(function() {
-      t.stop();
-      expect(count.threadStart).toBe(1);
-      expect(count.threadProgress).toBe(2);
-      expect(count.threadComplete).toBe(0);
-    });
+    t.start();
+    clock.tick((job.total - 1) * 5);
+    t.stop();
+    clock.tick(5);
+    expect(count.threadStart).toBe(1);
+    expect(count.threadProgress).toBe(2);
+    expect(count.threadComplete).toBe(0);
   });
 });
