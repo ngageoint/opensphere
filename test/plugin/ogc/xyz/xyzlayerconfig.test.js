f4a4470b93457b0fc3284ee9ec63b646984153be
Brad Hards <bradh@frogmouth.net>
2018-01-27 10:25:54 +1100

    fix(xyz): Add support for URLs containing server ranges.
    Includes updates to OSM cookbook docs.

--- test/plugin/ogc/xyz/xyzlayerconfig.test.js
+++ test/plugin/ogc/xyz/xyzlayerconfig.test.js
@@ -29,4 +29,108 @@ describe('plugin.xyz.XYZLayerConfig', function() {
 
     expect(proxyUrl).toBe(os.net.ProxyHandler.getProxyUri(originalUrl));
   });
+
+  it('should handle single URLs properly', function() {
+    var options = {
+      url: 'http://a.tiles.example.com/osm/{z}/{x}/{y}.png',
+      title: 'TestA'
+    };
+
+    var lc = new plugin.xyz.XYZLayerConfig();
+    var layer = lc.createLayer(options);
+
+    var tileFn = layer.getSource().getTileUrlFunction();
+
+    var tileCoord = [3, 2, -1];
+    var pixelRatio = 1;
+    var proj = ol.proj.get('EPSG:3857');
+
+    var resultURL = tileFn(tileCoord, pixelRatio, proj);
+
+    expect(resultURL).toBe('http://a.tiles.example.com/osm/3/2/0.png');
+  });
+
+  it('should handle single URLs as array properly', function() {
+    var options = {
+      urls: ['http://b.tiles.example.com/osm/{z}/{x}/{y}.png'],
+      title: 'TestB'
+    };
+
+    var lc = new plugin.xyz.XYZLayerConfig();
+    var layer = lc.createLayer(options);
+
+    var tileFn = layer.getSource().getTileUrlFunction();
+
+    var tileCoord = [3, 2, -1];
+    var pixelRatio = 1;
+    var proj = ol.proj.get('EPSG:3857');
+
+    var resultURL = tileFn(tileCoord, pixelRatio, proj);
+
+    expect(resultURL).toBe('http://b.tiles.example.com/osm/3/2/0.png');
+  });
+
+  it('should handle multiple URLs as array properly', function() {
+    var options = {
+      urls: ['http://a.tiles.example.com/osm/{z}/{x}/{y}.png', 'http://b.tiles.example.com/osm/{z}/{x}/{y}.png', 'http://c.tiles.example.com/osm/{z}/{x}/{y}.png'],
+      title: 'Test2'
+    };
+
+    var lc = new plugin.xyz.XYZLayerConfig();
+    var layer = lc.createLayer(options);
+
+    var tileFn = layer.getSource().getTileUrlFunction();
+
+    var tileCoord = [3, 2, -1];
+    var pixelRatio = 1;
+    var proj = ol.proj.get('EPSG:3857');
+
+    var resultURL = tileFn(tileCoord, pixelRatio, proj);
+
+    expect(resultURL).toMatch('http://[a-c].tiles.example.com/osm/3/2/0.png');
+  });
+
+  it('should handle wildcard URLs properly', function() {
+    var options = {
+      url: 'http://{a-c}.tiles.example.com/osm/{z}/{x}/{y}.png',
+      title: 'TestABC'
+    };
+
+    var lc = new plugin.xyz.XYZLayerConfig();
+    var layer = lc.createLayer(options);
+
+    var tileFn = layer.getSource().getTileUrlFunction();
+
+    var tileCoord = [3, 2, -1];
+    var pixelRatio = 1;
+    var proj = ol.proj.get('EPSG:3857');
+
+    var resultURL = tileFn(tileCoord, pixelRatio, proj);
+    expect(resultURL).toMatch('http://[a-c].tiles.example.com/osm/3/2/0.png');
+
+    resultURL = tileFn(tileCoord, pixelRatio, proj);
+    expect(resultURL).toMatch('http://[a-c].tiles.example.com/osm/3/2/0.png');
+  });
+
+  it('should handle wildcard URLs with numeric values properly', function() {
+    var options = {
+      url: 'http://{0-4}.tiles.example.com/osm/{z}/{x}/{y}.png',
+      title: 'Test04'
+    };
+
+    var lc = new plugin.xyz.XYZLayerConfig();
+    var layer = lc.createLayer(options);
+
+    var tileFn = layer.getSource().getTileUrlFunction();
+
+    var tileCoord = [3, 2, -1];
+    var pixelRatio = 1;
+    var proj = ol.proj.get('EPSG:3857');
+
+    var resultURL = tileFn(tileCoord, pixelRatio, proj);
+    expect(resultURL).toMatch('http://[0-4].tiles.example.com/osm/3/2/0.png');
+
+    resultURL = tileFn(tileCoord, pixelRatio, proj);
+    expect(resultURL).toMatch('http://[0-4].tiles.example.com/osm/3/2/0.png');
+  });
 });
