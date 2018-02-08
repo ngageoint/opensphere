4f31b0d3f76dead51d1fd0734664aa203f08a763
Greg Royal <groyal@caci.com>
2018-02-08 11:22:10 -0700

    feat(THIN-10598): Added function to jquery externs

--- externs/jquery-plugins.js
+++ externs/jquery-plugins.js
@@ -16,6 +16,12 @@ jQuery.prototype.datepicker = function(opt_arg1, opt_arg2) {};
  * @param {(string|Object)=} opt_arg1
  * @return {!jQuery}
  */
+jQuery.prototype.draggable = function(opt_arg1) {};
+
+/**
+ * @param {(string|Object)=} opt_arg1
+ * @return {!jQuery}
+ */
 jQuery.prototype.modal = function(opt_arg1) {};
 
 /**
