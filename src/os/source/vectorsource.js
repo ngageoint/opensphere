f84a890b4f4aa85901d030cb1945eb9bd5e02a0d
Jeremy Salankey <jsalankey@caci.com>
2018-02-13 07:32:00 -0700

    feat(THIN-10847): adds uniqueId field to sources and extern for track options

--- src/os/source/vectorsource.js
+++ src/os/source/vectorsource.js
@@ -17,6 +17,7 @@ goog.require('os');
 goog.require('os.Fields');
 goog.require('os.alert.AlertEventSeverity');
 goog.require('os.alert.AlertManager');
+goog.require('os.data.ColumnDefinition');
 goog.require('os.data.DataManager');
 goog.require('os.data.RecordField');
 goog.require('os.data.event.DataEvent');
@@ -356,9 +357,16 @@ os.source.Vector = function(opt_options) {
   this.refreshEnabled = false;
 
   /**
+   * Features queued to be cleared.
    * @type {?Array<ol.Feature>}
    */
   this.toClear = null;
+
+  /**
+   * Unique ID column.
+   * @type {?os.data.ColumnDefinition}
+   */
+  this.uniqueId_ = null;
 
   if (!options['disableAreaSelection']) {
     os.dispatcher.listen(os.action.EventType.SELECT, this.onFeatureAction_, false, this);
@@ -1117,7 +1125,6 @@ os.source.Vector.prototype.getMaxDate = function() {
  * Gets the layer title
  * @param {boolean=} opt_doNoUseTypeInName turns off the inclusion of the explicit type in the name
  * @return {!string} The title
- * @override
  */
 os.source.Vector.prototype.getTitle = function(opt_doNoUseTypeInName) {
   var layer = /** @type {os.layer.ILayer} */ (os.MapContainer.getInstance().getLayer(this.getId()));
@@ -3146,6 +3153,26 @@ os.source.Vector.prototype.defaultFeatureHover_ = function(feature) {
 
 
 /**
+ * Gets the unique ID used by features in the source.
+ * @return {os.data.ColumnDefinition}
+ */
+os.source.Vector.prototype.getUniqueId = function() {
+  return this.uniqueId_;
+};
+
+
+/**
+ * Sets the unique ID used by features in the source.
+ * @param {os.data.ColumnDefinition} value
+ */
+os.source.Vector.prototype.setUniqueId = function(value) {
+  var old = this.uniqueId_;
+  this.uniqueId_ = value;
+  this.dispatchEvent(new os.events.PropertyChangeEvent(os.source.PropertyChange.UNIQUE_ID, value, old));
+};
+
+
+/**
  * @inheritDoc
  */
 os.source.Vector.prototype.persist = function(opt_to) {
@@ -3155,6 +3182,10 @@ os.source.Vector.prototype.persist = function(opt_to) {
   options['timeEnabled'] = this.getTimeEnabled();
   options['altitudeEnabled'] = this.hasAltitudeEnabled();
   options['refreshInterval'] = this.refreshInterval;
+
+  if (this.uniqueId_) {
+    options['uniqueId'] = this.uniqueId_.persist();
+  }
 
   if (this.colorModel) {
     options['colorModel'] = this.colorModel.persist();
@@ -3197,5 +3228,11 @@ os.source.Vector.prototype.restore = function(config) {
     colorModel.restore(config['colorModel']);
 
     this.setColorModel(colorModel);
+  }
+
+  if (config['uniqueId']) {
+    var columnDef = new os.data.ColumnDefinition();
+    columnDef.restore(config['uniqueId']);
+    this.setUniqueId(columnDef);
   }
 };
