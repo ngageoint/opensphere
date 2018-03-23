b01e16c302733f12b7b8b8719f7c67448991cfc8
William Wall <wwall@caci.com>
2018-03-22 18:19:20 -0600

    feat(filter): add support for ECQL format
    see http://docs.geoserver.org/stable/en/user/filter/ecql_reference.html

---
+++ src/os/filter/impl/ecql/exclusionformatter.js
@@ -0,0 +1,14 @@
+goog.provide('os.filter.impl.ecql.ExclusionFormatter');
+
+goog.require('os.filter.impl.ecql.AreaFormatter');
+
+/**
+ * @param {string=} opt_column Optional geometry column name
+ * @extends {os.filter.impl.ecql.AreaFormatter}
+ * @constructor
+ */
+os.filter.impl.ecql.ExclusionFormatter = function(opt_column) {
+  os.filter.impl.ecql.ExclusionFormatter.base(this, 'constructor', opt_column);
+  this.spatialPredicate = 'DISJOINT';
+};
+goog.inherits(os.filter.impl.ecql.ExclusionFormatter, os.filter.impl.ecql.AreaFormatter);
