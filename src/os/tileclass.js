e81d72bbb62d6d8c2ea3d3eec43b794ea3724b23
William Wall <wwall@caci.com>
2018-03-30 13:46:17 -0600

    feat(tilelayer): added support for setting the tile class
    Moved the misnamed 'tileClass' to 'layerClass' and added 'tileClass'
    
    BREAKING CHANGE: confg['tileClass'] -> config['layerClass']

---
+++ src/os/tileclass.js
@@ -0,0 +1,8 @@
+goog.provide('os.TileClass');
+
+/**
+ * @typedef {function(new: ol.ImageTile, ol.TileCoord, ol.TileState, string, ?string, ol.TileLoadFunctionType, olx.TileOptions=)}
+ */
+os.TileClass;
+
+
