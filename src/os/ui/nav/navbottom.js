03d6d58cb6976a3e55bd62df9d1bb2b13011b44d
Greg Royal <groyal@caci.com>
2018-04-04 07:11:17 -0600

    refactor(navbar): Refactored navbar options into own file

--- src/os/ui/nav/navbottom.js
+++ src/os/ui/nav/navbottom.js
@@ -2,12 +2,6 @@ goog.provide('os.ui.navBottomDirective');
 
 goog.require('os.defines');
 goog.require('os.ui.NavBarCtrl');
-goog.require('os.ui.alert.alertButtonDirective');
-goog.require('os.ui.list');
-goog.require('os.ui.muteButtonDirective');
-goog.require('os.ui.nav');
-goog.require('os.ui.scaleLineDirective');
-goog.require('os.ui.serversButtonDirective');
 
 
 /**
@@ -30,11 +24,3 @@ os.ui.navBottomDirective = function() {
  * Add the directive to the module.
  */
 os.ui.Module.directive('navBottom', [os.ui.navBottomDirective]);
-
-os.ui.list.add(os.ui.nav.Location.BOTTOM_LEFT, '<li id="zoom-level"></li>', 100);
-os.ui.list.add(os.ui.nav.Location.BOTTOM_LEFT, 'scale-line', 200);
-os.ui.list.add(os.ui.nav.Location.BOTTOM_LEFT, '<li id="mouse-position"></li>', 300);
-
-os.ui.list.add(os.ui.nav.Location.BOTTOM_RIGHT, 'servers-button', 100);
-os.ui.list.add(os.ui.nav.Location.BOTTOM_RIGHT, 'alert-button', 200);
-os.ui.list.add(os.ui.nav.Location.BOTTOM_RIGHT, 'mute-button', 300);
