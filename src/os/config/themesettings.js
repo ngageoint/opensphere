c58b95dfd1e32b83fbcdca0b39d43494a90511e2
Greg Royal <groyal@caci.com>
2018-05-02 13:48:50 -0600

    refactor(theming): updated method of switching themes

--- src/os/config/themesettings.js
+++ src/os/config/themesettings.js
@@ -3,7 +3,6 @@ goog.provide('os.config.ThemeSettingsCtrl');
 
 goog.require('goog.Promise');
 goog.require('os.config.Settings');
-goog.require('os.net.Request');
 goog.require('os.ui.config.SettingPlugin');
 
 
@@ -124,55 +123,40 @@ goog.exportProperty(
 
 
 /**
- * The original stylesheet deployed with the application. Used as extreme backup if theres bad network connection
- * @type {string}
- */
-os.config.ThemeSettings.originalStylesheet = '';
-
-
-/**
  * Set Theme
  * @return {goog.Promise}
  */
 os.config.ThemeSettings.setTheme = function() {
-  var themeRegEx = /(themes\/).*?(\..*\.css)/;
-  var stylesheets = $('[rel=stylesheet]');
-  var ssEl = goog.array.find(stylesheets, function(el) {
-    return themeRegEx.test(el.href);
-  });
-
-  if (ssEl) {
-    return new goog.Promise(function(resolve, reject) {
-      // Save off the original stylesheet incase the network is having trouble getting others
-      if (!os.config.ThemeSettings.originalStylesheet) {
-        os.config.ThemeSettings.originalStylesheet = ssEl.href;
-      }
-
-      // The theme that is displayed to the user
-      var displayTheme = os.settings.get(os.config.ThemeSettings.keys.THEME, 'none');
-
-      // The actual theme filename
-      var theme = os.settings.get(os.config.ThemeSettings.keys.THEMES, {})[displayTheme];
-
-      // If there is a theme specified, use it! otherwise just use what comes with the application
-      if (goog.isDef(theme)) {
-        // The url to the new theme
-        var cssFile = ssEl.href.replace(themeRegEx, '$1' + theme + '$2');
-        var backupCssFile = ssEl.href;
-
-        // Ok lets trust but verify that the stylesheet is good. Otherwise you see a flicker when loading the app
-        // loading a non-default stylesheet
-        if (ssEl.href != cssFile) {
-          // Awesome! lets load it!
-          os.config.ThemeSettings.changeTheme(ssEl, cssFile, backupCssFile, resolve);
-        }
-      } else {
-        resolve();
-      }
+  return new goog.Promise(function(resolve, reject) {
+    var themeRegEx = /(themes\/).*?(\..*\.css)/;
+    var stylesheets = $('[rel=stylesheet]');
+    var ssEl = goog.array.find(stylesheets, function(el) {
+      return themeRegEx.test(el.href);
     });
-  } else {
-    return goog.Promise.resolve();
-  }
+
+    // The theme that is displayed to the user
+    var displayTheme = os.settings.get(os.config.ThemeSettings.keys.THEME, 'none');
+
+    // The actual theme filename
+    var theme = os.settings.get(os.config.ThemeSettings.keys.THEMES, {})[displayTheme];
+
+    // If there is a theme specified, use it! otherwise just use what comes with the application
+    if (goog.isDef(theme)) {
+      // No theme is the default theme
+      if (!theme) {
+        theme = 'default';
+      }
+
+      // The url to the new theme
+      var cssFile = ssEl.href.replace(themeRegEx, '$1' + theme + '$2');
+      if (ssEl.href != cssFile) {
+        // Awesome! lets load it!
+        os.config.ThemeSettings.changeTheme(cssFile);
+      }
+    }
+
+    resolve();
+  });
 };
 
 
@@ -185,56 +169,36 @@ os.config.ThemeSettings.updateTheme = function() {
 
 
 /**
- * Clean up the request for the stylesheet
- * @param {goog.events.Event} event
- */
-os.config.ThemeSettings.cleanupRequest = function(event) {
-  /** @type {os.net.Request} */ (event.target).dispose();
-};
-
-
-/**
  * Change the stylesheet
- * @param {!HTMLLinkElement} ssEl
  * @param {string} cssFile
- * @param {string} backupCssFile
- * @param {function()} resolve - resolve the promise after the css is loaded
  */
-os.config.ThemeSettings.changeTheme = function(ssEl, cssFile, backupCssFile, resolve) {
-  var request = new os.net.Request();
-  request.setUri(cssFile);
-  request.listenOnce(goog.net.EventType.SUCCESS, function(event) {
-    os.config.ThemeSettings.cleanupRequest(event);
+os.config.ThemeSettings.changeTheme = function(cssFile) {
+  // onload doesnt work correctly for link for all browsers. Theres a hacky workaround to use img to to know when
+  // the css is loaded
+  var link = $('<link rel="stylesheet" type="text/css" href="' + cssFile + '"></link>');
+  $('head').append(link);
 
-    var link = $('<link />', {
-      rel: 'stylesheet',
-      type: 'text/css',
-      href: cssFile
+
+  var img = document.createElement('img');
+
+  /**
+   * Failproof way to check if the css is loaded
+   */
+  img.onerror = function() {
+    document.body.removeChild(img);
+
+    // Get the current theme(s)
+    var themeRegEx = /(themes\/).*?(\..*\.css)/;
+    var stylesheets = $('[rel=stylesheet]');
+    var ourThemes = goog.array.filter(stylesheets, function(el) {
+      return themeRegEx.test(el.href);
     });
-    $('head').append(link);
-    resolve();
 
-    // If angular is already bootstraped. We want to wait for the new css file to load.
-    // Otherwise its loaded so we are good
-    if (os.ui.injector) {
-      os.ui.injector.get('$timeout')(function() {
-        ssEl.remove();
-      }, 200);
-    } else {
-      ssEl.remove();
+    for (var i = 0; i < ourThemes.length - 1; i++) {
+      ourThemes[i].remove();
     }
-  }, false);
-  request.listenOnce(goog.net.EventType.ERROR, function(event) {
-    os.config.ThemeSettings.cleanupRequest(event);
+  };
 
-    // If we've already tried to load the backup and THAT failed. Then just bootstrap and hope for the best.
-    // Should never happen (??)
-    if (backupCssFile == os.config.ThemeSettings.originalStylesheet) {
-      resolve();
-    } else {
-      // Revert!
-      os.config.ThemeSettings.changeTheme(ssEl, backupCssFile, os.config.ThemeSettings.originalStylesheet, resolve);
-    }
-  }, false);
-  request.load();
+  document.body.appendChild(img);
+  img.src = cssFile;
 };
