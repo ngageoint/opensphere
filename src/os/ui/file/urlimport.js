10dc38b6e1e4c01c473ee663a0e0418cc5742828
Kevin Schmidt <kschmidt@caci.com>
2018-06-26 08:47:49 -0600

    refactor(validation): Use validation directive in import forms.

--- src/os/ui/file/urlimport.js
+++ src/os/ui/file/urlimport.js
@@ -1,6 +1,8 @@
 goog.provide('os.ui.file.UrlImportCtrl');
 goog.provide('os.ui.file.urlImportDirective');
+
 goog.require('os.ui.Module');
+goog.require('os.ui.util.validationMessageDirective');
 
 
 /**
