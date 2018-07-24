1268fb67da38e73cc7760aee97ca920ee9c53f6a
William Wall <wwall@caci.com>
2018-07-24 09:50:38 -0600

    fix(kml): KML detection should use root tag OR root NS

--- src/plugin/file/kml/mime.js
+++ src/plugin/file/kml/mime.js
@@ -1,5 +1,6 @@
 goog.provide('plugin.file.kml.mime');
 
+goog.require('goog.Promise');
 goog.require('os.file.mime.xml');
 goog.require('os.file.mime.zip');
 
@@ -9,9 +10,26 @@ goog.require('os.file.mime.zip');
  */
 plugin.file.kml.mime.TYPE = 'application/vnd.google-earth.kml+xml';
 
+
+/**
+ * @param {ArrayBuffer} buffer
+ * @param {os.file.File=} opt_file
+ * @param {*=} opt_context
+ * @return {!goog.Promise<*|undefined>}
+ */
+plugin.file.kml.detect = function(buffer, opt_file, opt_context) {
+  var retVal;
+  if (opt_context && (
+      (/^(document|folder|kml)$/i.test(opt_context.rootTag)) ||
+      (/\/kml\//i.test(opt_context.rootNS)))) {
+    retVal = opt_context;
+  }
+
+  return goog.Promise.resolve(retVal);
+};
 os.file.mime.register(
     plugin.file.kml.mime.TYPE,
-    os.file.mime.xml.createDetect(/^(document|folder|kml)$/i, /\/kml\//i),
+    plugin.file.kml.detect,
     0, os.file.mime.xml.TYPE);
 
 
