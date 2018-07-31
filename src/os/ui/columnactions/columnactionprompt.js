3d496fc3d55e2601e5dbf17a1aad315f17193316
John Stacy <johnstacy@seed-innovations.com>
2018-07-31 10:05:09 -0600

    fix(columnactionprompt): BS4 styling - THIN-11924

--- src/os/ui/columnactions/columnactionprompt.js
+++ src/os/ui/columnactions/columnactionprompt.js
@@ -132,6 +132,6 @@ os.ui.columnactions.launchColumnActionPrompt = function(matched, value, colDef) 
     'no-scroll': 'true'
   };
 
-  var template = '<columnactions></columnactions>';
+  var template = '<columnactions class="d-flex flex-fill"></columnactions>';
   os.ui.window.create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
 };
