91174bbdd48288e00eb590dd5c3f97543ae39cb3
John Stacy <johnstacy@seed-innovations.com>
2018-08-01 10:14:21 -0600

    fix(bs4): PR findings

--- src/os/ui/columnactions/columnactionprompt.js
+++ src/os/ui/columnactions/columnactionprompt.js
@@ -117,7 +117,7 @@ os.ui.columnactions.launchColumnActionPrompt = function(matched, value, colDef) 
     'colDef': colDef,
     'value': value,
     'doneText': 'Close',
-    'doneIcon': 'fa fa-check icon-blue'
+    'doneIcon': 'fa fa-check'
   };
 
   var windowOptions = {
