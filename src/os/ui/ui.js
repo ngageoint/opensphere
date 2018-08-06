0c9a6ae635baff6f27d9706dc3aeef24d22359d4
Kevin Schmidt <kschmidt@caci.com>
2018-08-06 11:31:58 -0600

    fix(ui): Support line breaks when measuring text.

--- src/os/ui/ui.js
+++ src/os/ui/ui.js
@@ -144,7 +144,7 @@ os.ui.measureText = function(text, opt_classes, opt_font) {
     el[0].setAttribute('class', opt_classes ? opt_classes : '');
     el.css('font', opt_font || '');
 
-    el.html(text);
+    el.html(text.replace('\n', '<br>'));
     return new goog.math.Size(/** @type {number} */ (el.width()), /** @type {number} */ (el.height()));
   }
 
