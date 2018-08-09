5260ba4d4651f7796d348444c1ceff6d91601dcb
Kevin Schmidt <kschmidt@caci.com>
2018-08-09 10:09:03 -0600

    fix(search): Fix height calculation for search results.
    Adds a new 'parent' property to autoheight to define the parent used to determine maximum height.

--- src/os/ui/nav/navbaroptions.js
+++ src/os/ui/nav/navbaroptions.js
@@ -44,7 +44,7 @@ os.ui.navbaroptions.searchbox = '<search-box show-clear="true"></search-box>';
  * Search results template.
  * @type {string}
  */
-os.ui.navbaroptions.searchresults = 'searchresults';
+os.ui.navbaroptions.searchresults = '<searchresults parent="#main"></searchresults>';
 
 
 /**
