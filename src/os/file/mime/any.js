d74178692d617c8b33e1779b73780178a0a6c008
William Wall <wwall@caci.com>
2018-08-17 11:47:22 -0600

    fix(mime): any type should use byteLength rather than length
    Also fixed typo in README

--- src/os/file/mime/any.js
+++ src/os/file/mime/any.js
@@ -18,7 +18,7 @@ os.file.mime.any.TYPE = '*/*';
  * @return {!goog.Promise<*|undefined>}
  */
 os.file.mime.any.isSomething = function(buffer, opt_file, opt_context) {
-  return /** @type {!goog.Promise<*|undefined>} */ (goog.Promise.resolve(!!(buffer && buffer.length)));
+  return /** @type {!goog.Promise<*|undefined>} */ (goog.Promise.resolve(!!(buffer && buffer.byteLength)));
 };
 
 os.file.mime.register(os.file.mime.any.TYPE, os.file.mime.any.isSomething, 1000000);
