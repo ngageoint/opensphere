e56a3f9dfe798fa6095b41878c3f4c3dad281ba8
Lily T Sikes <lily.sikes@caci.com>
2018-11-13 11:51:59 -0700

    feat(placesImport): Files/URLs can be imported as places.
    #336

--- src/plugin/places/placesplugin.js
+++ src/plugin/places/placesplugin.js
@@ -4,6 +4,11 @@ goog.require('goog.log');
 goog.require('os.plugin.AbstractPlugin');
 goog.require('os.ui.clear.ClearEntry');
 goog.require('os.ui.clearManager');
+goog.require('os.ui.file.method.ImportMethod');
+goog.require('plugin.area.CSVAreaImportUI');
+goog.require('plugin.area.GeoJSONAreaImportUI');
+goog.require('plugin.area.KMLAreaImportUI');
+goog.require('plugin.area.SHPAreaImportUI');
 goog.require('plugin.places');
 goog.require('plugin.places.PlacesClear');
 goog.require('plugin.places.PlacesManager');
@@ -62,4 +67,27 @@ plugin.places.PlacesPlugin.prototype.init = function() {
   } catch (e) {
     goog.log.error(plugin.places.PlacesPlugin.LOGGER_, 'Failed initializing Places plugin:', e);
   }
+  var pim = os.placesImportManager;
+  var pfm = os.placesFileManager;
+
+  // register file import method
+  pfm.registerFileMethod(new os.ui.file.method.ImportMethod(false));
+
+  // csv
+  pim.registerImportUI(os.file.mime.csv.TYPE, new plugin.area.CSVAreaImportUI());
+  pim.registerImportDetails('CSV', true);
+
+  // geojson
+  pim.registerImportUI(plugin.file.geojson.mime.TYPE, new plugin.area.GeoJSONAreaImportUI());
+  pim.registerImportDetails('GeoJSON', true);
+
+  // kml
+  pim.registerImportUI(plugin.file.kml.mime.TYPE, new plugin.area.KMLAreaImportUI());
+  pim.registerImportUI(plugin.file.kml.mime.KMZ_TYPE, new plugin.area.KMLAreaImportUI());
+  pim.registerImportDetails('KML/KMZ', true);
+
+  // shp
+  pim.registerImportUI(plugin.file.shp.mime.TYPE, new plugin.area.SHPAreaImportUI());
+  pim.registerImportUI(plugin.file.shp.mime.ZIP_TYPE, new plugin.area.SHPAreaImportUI());
+  pim.registerImportDetails('Shapefile (SHP/DBF or ZIP)', true);
 };
