582884c3e92f1a245dc74916b5ace247b3f469e6
luethd <daniel.lueth@caci.com>
2018-12-14 11:45:12 -0700

    chore(navbarctrl): Move navbar footer logic to controller
    THIN-12389

--- src/os/ui/nav/navbarctrl.js
+++ src/os/ui/nav/navbarctrl.js
@@ -72,36 +72,65 @@ os.ui.NavBarCtrl.prototype.getNavContentSize = function() {
 
 
 /**
- * Returns true if navbar only contains elements from one list
- * @param {?Array.<string>} listIds Array containing the list ids of the navbar lists
- * @return {?boolean} true if NavBar elements come from a single list
+ * Get footer ng-class string based on loaded navbar lists
+ * @param {string=} opt_leftListId The optional left list id
+ * @param {string=} opt_middleListId The optional middle list id
+ * @param {string=} opt_rightListId The optional right list id
+ * @return {Array.<string>} The resulting class string
  * @export
  */
-os.ui.NavBarCtrl.prototype.navItemsFromSingleList = function(listIds) {
-  if (listIds == null) {
-    return null;
+os.ui.NavBarCtrl.prototype.getFooterDynamicClasses = function(opt_leftListId, opt_middleListId, opt_rightListId) {
+  var classes = [];
+
+  var ids = [opt_leftListId, opt_middleListId, opt_rightListId].filter(function(el) {
+    return el;
+  });
+
+  if (this.navItemsFromSingleList_(ids)) {
+    var numItemsLeft = this.numNavItems_(opt_leftListId);
+    var numItemsMiddle = this.numNavItems_(opt_middleListId);
+    var numItemsRight = this.numNavItems_(opt_rightListId);
+
+    if (numItemsLeft || numItemsMiddle) {
+      classes.push('mr-auto');
+    } else if (numItemsRight || numItemsMiddle) {
+      classes.push('ml-auto');
+    }
+  } else {
+    classes.push('flex-fill');
   }
 
+  return classes;
+};
+
+
+/**
+ * Returns true if navbar only contains elements from one list
+ * @param {Array.<string>} listIds Array containing the list ids of the navbar lists
+ * @return {boolean} true if NavBar elements come from a single list
+ * @private
+ */
+os.ui.NavBarCtrl.prototype.navItemsFromSingleList_ = function(listIds) {
   var numListsPopulated = 0;
   listIds.forEach(function(id) {
     var list = os.ui.list.get(id);
-    if (list) {
+    if (list && list.length > 0) {
       numListsPopulated++;
     }
   });
 
-  return numListsPopulated < 2;
+  return numListsPopulated == 1;
 };
 
 
 /**
  * Number of navItems within the given list id
- * @param {string} listId The list id
+ * @param {string=} opt_listId The list id
  * @return {number} the number of navItems in the list
- * @export
+ * @private
  */
-os.ui.NavBarCtrl.prototype.numNavItems = function(listId) {
-  return os.ui.list.get(listId) ? os.ui.list.get(listId).length : 0;
+os.ui.NavBarCtrl.prototype.numNavItems_ = function(opt_listId) {
+  return opt_listId && os.ui.list.get(opt_listId) ? os.ui.list.get(opt_listId).length : 0;
 };
 
 
