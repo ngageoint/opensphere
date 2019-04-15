b3dafbd52247ed5e89c399163b3d5ab4f45be3c0
Justin Denton <justin.denton@caci.com>
2019-04-15 12:01:22 -0600

    test(cypress): Renames test-state to test-state-arcgis

--- cypress/integration/smoke-tests/load-state-file-arcgis.spec.js
+++ cypress/integration/smoke-tests/load-state-file-arcgis.spec.js
@@ -37,7 +37,7 @@ describe('Import state file', function() {
     // Test
     cy.get(os.Toolbar.addData.OPEN_FILE_BUTTON).click();
     cy.get(os.importDataDialog.DIALOG).should('be.visible');
-    cy.upload('smoke-tests/load-state-file-arcgis/test-state_state.xml');
+    cy.upload('smoke-tests/load-state-file-arcgis/test-state-arcgis_state.xml');
     cy.get(os.importDataDialog.NEXT_BUTTON).click();
     cy.get(os.importStateDialog.DIALOG).should('be.visible');
     cy.get(os.importStateDialog.CLEAR_CHECKBOX).check();
