371dd8f3099670de188742349830641e155dd615
Kevin Schmidt <kschmidt@caci.com>
2019-06-12 11:09:49 -0600

    build(cypress): Disable Cypress in Travis.
    This also disables API doc generation due to problems with the current version of js-dossier.

--- ci-script.sh
+++ ci-script.sh
@@ -3,5 +3,19 @@
 set -e
 yarn run build
 yarn run test
-./cypress/support/execute-tests.sh ci cli all
-yarn run apidoc
+
+#
+# Cypress tests are disabled in CI until they can reliably run without false positives, or inconsistent behavior
+# caused by bugs in Cypress itself (ie, fixed element visibility).
+#
+
+# ./cypress/support/execute-tests.sh ci cli all
+
+#
+# js-dossier does not currently work with OpenSphere's version of the Closure Library, and will need a compiler
+# upgrade to resolve this problem.
+#
+# See: https://github.com/jleyba/js-dossier/issues/111
+#
+
+# yarn run apidoc
