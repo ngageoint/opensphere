48b493aa3ad62df45ac842aee3f55798a34f3a35
Greg Royal <groyal@caci.com>
2019-06-13 13:58:41 -0600

    refactor(editor): cleaned up comments

--- src/os/ui/text/simplemde.js
+++ src/os/ui/text/simplemde.js
@@ -8,7 +8,6 @@ goog.require('os.ui.Module');
 
 
 /**
- * The count by directive
  * @return {angular.Directive}
  */
 os.ui.text.simpleMDEDirective = function() {
@@ -122,7 +121,6 @@ os.ui.text.SimpleMDE.BASICTOOLBAR = [
 
 
 /**
- * Controller class for the source switcher
  * @param {!angular.Scope} $scope
  * @param {!angular.JQLite} $element
  * @param {!angular.$timeout} $timeout
