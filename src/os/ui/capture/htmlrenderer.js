2668bd1fdb4618bc8c91a60ea87a68d136e1e1d5
Greg Royal <groyal@caci.com>
2019-06-23 10:19:37 -0600

    feat(htmlrender): made more extensible

--- src/os/ui/capture/htmlrenderer.js
+++ src/os/ui/capture/htmlrenderer.js
@@ -13,6 +13,14 @@ goog.require('os.ui.capture.ElementRenderer');
  */
 os.ui.capture.HtmlRenderer = function(opt_options) {
   os.ui.capture.HtmlRenderer.base(this, 'constructor', opt_options);
+
+  this.getCanvasElement = opt_options['getCanvasElement'] || plugin.capture.getMapCanvas;
+
+  if (opt_options['useRelativePosition']) {
+    this.getPosition = this.getRelativePosition;
+  }
+
+  this.extraOptions = opt_options['html2canvasOptions'] || {};
 };
 goog.inherits(os.ui.capture.HtmlRenderer, os.ui.capture.ElementRenderer);
 
@@ -23,16 +31,51 @@ goog.inherits(os.ui.capture.HtmlRenderer, os.ui.capture.ElementRenderer);
 os.ui.capture.HtmlRenderer.prototype.getCanvas = function() {
   return new goog.Promise(function(resolve, reject) {
     var htmlElement = this.getRenderElement();
-    var mapCanvas = plugin.capture.getMapCanvas();
-    if (htmlElement && mapCanvas) {
-      var mapRect = mapCanvas.getBoundingClientRect();
-      var pixelRatio = mapCanvas.width / mapRect.width;
-      html2canvas(htmlElement, /** @type {html2canvas.Options} */ ({
+    var canvas = this.getCanvasElement();
+    if (htmlElement && canvas) {
+      var rect = canvas.getBoundingClientRect();
+      var pixelRatio = canvas.width / rect.width;
+      var options = {
         backgroundColor: null,
+        canvas: canvas,
         scale: pixelRatio
-      })).then(resolve, reject);
+      };
+
+      goog.object.extend(options, this.extraOptions);
+      html2canvas(htmlElement, /** @type {html2canvas.Options} */ (options)).then(resolve, reject);
     } else {
       resolve(null);
     }
   }, this);
+};
+
+
+/**
+ * Get the starting draw position of the canvas as [x, y].
+ * @param {!HTMLCanvasElement} canvas The canvas
+ * @return {!Array<number>}
+ * @protected
+ */
+os.ui.capture.HtmlRenderer.prototype.getRelativePosition = function(canvas) {
+  var x = 0;
+  var y = 0;
+
+  // NOTE: For High DPI Displays such as Apple Retina Screens, canvas
+  // pixels do not directly correspond to CSS pixels.
+  var referenceCanvas = this.getCanvasElement();
+  var htmlElement = this.getRenderElement();
+
+  if (htmlElement && referenceCanvas) {
+    // Since OpenLayers allows for specifying the pixel ratio on a map (rather than always
+    // using window.devicePixelRatio directly), we will calculate it
+    var rect = referenceCanvas.getBoundingClientRect();
+    var pixelRatio = referenceCanvas.width / rect.width;
+
+    var overlayRect = htmlElement.getBoundingClientRect();
+    // determine the overlay's position over the map
+    x = pixelRatio * overlayRect.x;
+    y = pixelRatio * (overlayRect.y - rect.y);
+  }
+
+  return [x, y];
 };
