3b496fa255744ab8c1c4863f67e7a56db08c5147
Kevin Schmidt <kschmidt@caci.com>
2019-07-09 16:24:48 -0600

    refactor(search): Extract generic local facet code from descriptor facets.
    BREAKING CHANGE: `plugin.descriptor.facet.BaseFacet` has been renamed
    `os.search.BaseFacet` and API params have changed slightly. Any
    extending classes should rename the parent and change `descriptor` to
    `item`.

--- src/plugin/descriptor/facet/typefacet.js
+++ src/plugin/descriptor/facet/typefacet.js
@@ -1,16 +1,16 @@
 goog.provide('plugin.descriptor.facet.Type');
-goog.require('plugin.descriptor.facet.BaseFacet');
+goog.require('os.search.BaseFacet');
 
 
 
 /**
  * @constructor
- * @extends {plugin.descriptor.facet.BaseFacet}
+ * @extends {os.search.BaseFacet<!os.data.IDataDescriptor>}
  */
 plugin.descriptor.facet.Type = function() {
   plugin.descriptor.facet.Type.base(this, 'constructor');
 };
-goog.inherits(plugin.descriptor.facet.Type, plugin.descriptor.facet.BaseFacet);
+goog.inherits(plugin.descriptor.facet.Type, os.search.BaseFacet);
 
 
 /**
@@ -36,11 +36,11 @@ plugin.descriptor.facet.Type.prototype.valueToLabel = function(value) {
 /**
  * @inheritDoc
  */
-plugin.descriptor.facet.Type.prototype.load = function(descriptor, facets) {
-  var type = descriptor.getType();
+plugin.descriptor.facet.Type.prototype.load = function(item, facets) {
+  var type = item.getType();
 
   if (type) {
-    plugin.descriptor.facet.BaseFacet.update('Type', type, facets);
+    os.search.BaseFacet.update('Type', type, facets);
   }
 };
 
@@ -48,12 +48,12 @@ plugin.descriptor.facet.Type.prototype.load = function(descriptor, facets) {
 /**
  * @inheritDoc
  */
-plugin.descriptor.facet.Type.prototype.test = function(descriptor, facets, results) {
+plugin.descriptor.facet.Type.prototype.test = function(item, facets, results) {
   var values = facets['Type'];
 
   if (values) {
-    var t = descriptor.getType();
+    var t = item.getType();
     var x = t ? values.indexOf(t) : -1;
-    plugin.descriptor.facet.BaseFacet.updateResults('Type', results, x > -1 ? 1 : 0);
+    os.search.BaseFacet.updateResults('Type', results, x > -1 ? 1 : 0);
   }
 };
