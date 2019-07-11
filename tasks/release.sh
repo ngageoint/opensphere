3dde7e402806e9cb18bf5b5c75567f32ab2b2358
William Wall <wwall@caci.com>
2019-07-11 14:52:27 -0600

    chore(release-prep): release build should use a full clone

--- tasks/release.sh
+++ tasks/release.sh
@@ -8,11 +8,17 @@ echo "  * login via: travis login --org"
 
 token=$(travis token --org | awk '{print $NF}')
 
+# The git.depth = false thing is because Travis, by default, clones with --depth 50. The
+# semantic-release scripts will be examining git for previous release tags, and we need
+# to be sure the local clone has them
 body='{
   "request": {
     "branch": "master",
     "config": {
       "merge_mode": "deep_merge",
+      "git": {
+        "depth": false
+      },
       "env": {
         "global": {
           "RELEASE": true
