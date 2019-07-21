399c78fa86c49d50de676e3a332118a820eb453b
Brad Hards <bradh@frogmouth.net>
2019-07-21 20:48:40 +1000

    test(metrics): Additional unit tests for metrics.

--- test/os/metrics/metrics.test.js
+++ test/os/metrics/metrics.test.js
@@ -1,4 +1,5 @@
 goog.require('os.metrics.Metrics');
+goog.require('os.mock');
 
 describe('os.metrics.Metrics', function() {
   it('should update a metric', function() {
@@ -11,5 +12,22 @@ describe('os.metrics.Metrics', function() {
     expect(target.getMetric(key)).toBe(1);
     target.updateMetric(key, 1);
     expect(target.getMetric(key)).toBe(2);
+  });
+
+  it('should have metrics disabled by default', function() {
+    var metrics = new os.metrics.Metrics();
+    expect(metrics.isEnabled()).toBe(false);
+    var key = 'test.key2';
+    metrics.updateMetric(key, 1);
+    expect(metrics.getMetric(key)).toBe(0);
+  });
+
+  it('should have default metric value of 0', function() {
+    var metrics = new os.metrics.Metrics();
+    metrics.enabled_ = true;
+    var key = 'xyzzy';
+    expect(metrics).not.toBe(null);
+    expect(metrics.hasMetric(key)).toBe(false);
+    expect(metrics.getMetric(key)).toBe(0);
   });
 });
