c0137a1467cf7ac5c38f4fc13da486b36b4d8683
Jeremy Salankey <jsalankey@caci.com>
2019-07-26 09:20:21 -0600

    fix(annotations): fixes line style to be an actual line, cleans up some UI quirks
    THIN-13882

--- src/os/annotation/featureannotationui.js
+++ src/os/annotation/featureannotationui.js
@@ -346,13 +346,8 @@ os.annotation.FeatureAnnotationCtrl.prototype.updateTailAbsolute = function() {
     var cardCenter = [cardOffsetX + cardRect.width / 2, cardOffsetY + cardRect.height / 2];
     // Changes the annotation tail style
     var anchorWidth = Math.min(cardRect.height, cardRect.width) * .33;
-    if (this['options'].showTail === os.annotation.TailStyle.NOTAIL) {
-      anchorWidth = 0;
-    }
-    if (this['options'].showTail === os.annotation.TailStyle.LINETAIL) {
-      anchorWidth = 4;
-    }
-    var linePath = os.annotation.AbstractAnnotationCtrl.createTailPath(cardCenter, pathTarget, anchorWidth);
+    var linePath = os.annotation.AbstractAnnotationCtrl.createTailPath(cardCenter, pathTarget, anchorWidth,
+        this['options'].showTail);
 
     svg.attr('width', svgWidth);
     svg.attr('height', svgHeight);
@@ -414,13 +409,8 @@ os.annotation.FeatureAnnotationCtrl.prototype.updateTailFixed = function() {
     var cardCenter = [cardRect.x + cardRect.width / 2, cardRect.y + cardRect.height / 2];
     // Changes the annotation tail style
     var anchorWidth = Math.min(cardRect.height, cardRect.width) * .33;
-    if (this['options'].showTail === os.annotation.TailStyle.NOTAIL) {
-      anchorWidth = 0;
-    }
-    if (this['options'].showTail === os.annotation.TailStyle.LINETAIL) {
-      anchorWidth = 4;
-    }
-    var linePath = os.annotation.AbstractAnnotationCtrl.createTailPath(cardCenter, targetPixel, anchorWidth);
+    var linePath = os.annotation.AbstractAnnotationCtrl.createTailPath(cardCenter, targetPixel, anchorWidth,
+        this['options'].showTail);
 
     this.element.find('path').attr('d', linePath);
 
