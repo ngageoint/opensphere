ac7083abc54c21f6e5f8c1f7ca4a308e8fee9b73
Justin Denton <justin.denton@caci.com>
2019-08-06 13:08:39 -0600

    test(cypress): Adds run-script-os to Windows patch

--- cypress/support/windows/patch-windows.sh
+++ cypress/support/windows/patch-windows.sh
@@ -9,5 +9,9 @@ cp cypress/support/windows/cypress node_modules/.bin
 cp cypress/support/windows/cypress.cmd node_modules/.bin
 cp cypress/support/windows/http-server node_modules/.bin
 cp cypress/support/windows/http-server.cmd node_modules/.bin
+cp cypress/support/windows/run-os node_modules/.bin
+cp cypress/support/windows/run-os.cmd node_modules/.bin
+cp cypress/support/windows/run-script-os node_modules/.bin
+cp cypress/support/windows/run-script-os.cmd node_modules/.bin
 
 echo 'INFO: Patch script completed!'
