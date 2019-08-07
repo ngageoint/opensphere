cb863d16281f1f7311fe77e8640c4f1a17760df0
Justin Denton <justin.denton@caci.com>
2019-08-07 10:46:12 -0600

    test(cypress): Fixes patch script

--- cypress/support/windows/patch-windows.cmd
+++ cypress/support/windows/patch-windows.cmd
@@ -1,3 +1,5 @@
 del node_modules\.bin\cypress
 del node_modules\.bin\http-server
-copy cypress\support\windows\.bin\* node_modules\.bin
+del node_modules\.bin\run-os
+del node_modules\.bin\run-script-os
+copy cypress\support\windows\.bin\ node_modules\.bin
