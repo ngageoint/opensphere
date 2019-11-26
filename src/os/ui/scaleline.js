abb67df38a6af51f5b8c491b9524b23bc807bf77
Kevin Schmidt <kschmidt@caci.com>
2019-11-26 16:10:38 -0700

    refactor(goog): convert the log window and scale line

--- src/os/ui/scaleline.js
+++ src/os/ui/scaleline.js
@@ -1,49 +1,50 @@
-goog.provide('os.ui.ScaleLineCtrl');
-goog.provide('os.ui.scaleLineDirective');
+goog.module('os.ui.scaleLineDirective');
+goog.module.declareLegacyNamespace();
 
 goog.require('os.ui.Module');
 goog.require('os.ui.menu.MenuButtonCtrl');
-goog.require('os.ui.menu.unit');
 
 
 /**
- * The scale line directive
+ * Controller for the scale line directive.
+ * @extends {os.ui.menu.MenuButtonCtrl}
+ */
+class ScaleLineCtrl extends os.ui.menu.MenuButtonCtrl {
+  /**
+   * Constructor.
+   * @param {!angular.Scope} $scope The Angular scope.
+   * @param {!angular.JQLite} $element The root DOM element.
+   * @ngInject
+   */
+  constructor($scope, $element) {
+    super($scope, $element);
+
+    this.menu = os.ui.menu.UNIT;
+    this.btnPosition = 'right top';
+    this.menuPosition = 'right bottom-4';
+  }
+}
+
+/**
+ * The scale line directive.
  *
  * @return {angular.Directive}
  */
-os.ui.scaleLineDirective = function() {
+const scaleLineDirective = function() {
   return {
     restrict: 'E',
     replace: true,
     scope: true,
     template: '<li class="pointer" id="scale-line" ng-click="ctrl.openMenu()" ng-right-click="ctrl.openMenu()">' +
         '<div class="unit-group"></div></li>',
-    controller: os.ui.ScaleLineCtrl,
+    controller: ScaleLineCtrl,
     controllerAs: 'ctrl'
   };
 };
 
-
 /**
  * Add the directive to the module.
  */
-os.ui.Module.directive('scaleLine', [os.ui.scaleLineDirective]);
+os.ui.Module.directive('scaleLine', [scaleLineDirective]);
 
-
-
-/**
- * Controller function for the scale line directive
- *
- * @param {!angular.Scope} $scope
- * @param {!angular.JQLite} $element
- * @extends {os.ui.menu.MenuButtonCtrl}
- * @constructor
- * @ngInject
- */
-os.ui.ScaleLineCtrl = function($scope, $element) {
-  os.ui.ScaleLineCtrl.base(this, 'constructor', $scope, $element);
-  this.menu = os.ui.menu.UNIT;
-  this.btnPosition = 'right top';
-  this.menuPosition = 'right bottom-4';
-};
-goog.inherits(os.ui.ScaleLineCtrl, os.ui.menu.MenuButtonCtrl);
+exports = scaleLineDirective;
