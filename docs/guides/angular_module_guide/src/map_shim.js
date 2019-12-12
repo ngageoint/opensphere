27f62fb4706f2b75532481a12d593a64d81f0377
Kevin Schmidt <kschmidt@caci.com>
2019-12-12 06:24:14 -0700

    docs(ng): guide for converting Angular UIs to modules

---
+++ docs/guides/angular_module_guide/src/map_shim.js
@@ -0,0 +1,14 @@
+goog.provide('os.ui.MapCtrl');
+goog.provide('os.ui.mapDirective');
+
+goog.require('os.ui.Map');
+
+/**
+ * @deprecated Please use goog.require('os.ui.Map').Controller instead.
+ */
+os.ui.MapCtrl = os.ui.Map.Controller;
+
+/**
+ * @deprecated Please use goog.require('os.ui.Map').directive instead.
+ */
+os.ui.mapDirective = os.ui.Map.directive;
