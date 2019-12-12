9d9e331f5f148a471ecb23c92afefb7263380947
Kevin Schmidt <kschmidt@caci.com>
2019-12-12 06:59:52 -0700

    refactor(scaleline): use new module format for scaleline directive

---
+++ src/os/ui/scaleline_shim.js
@@ -0,0 +1,8 @@
+goog.provide('os.ui.scaleLineDirective');
+
+goog.require('os.ui.ScaleLine');
+
+/**
+ * @deprecated Please use goog.require('os.ui.ScaleLine').directive instead.
+ */
+os.ui.scaleLineDirective = os.ui.ScaleLine.directive;
