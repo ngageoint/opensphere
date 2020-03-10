dff80d9908e5ab589c9bff7a0b2efeac908af992
William Wall <wwall@caci.com>
2020-03-10 12:37:02 -0600

    refactor(cesium): split updateBillboard function
    for readability.

--- src/plugin/cesium/sync/point.js
+++ src/plugin/cesium/sync/point.js
@@ -44,6 +44,43 @@ const createBillboard = (feature, geometry, style, context, opt_flatCoords, opt_
 
 
 /**
+ * Update a Cesium Billboard from an OpenLayers image style.
+ *
+ * @param {!Feature} feature
+ * @param {!(Point|MultiPoint)} geometry
+ * @param {!OLImageStyle} style
+ * @param {!VectorContext} context
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ * @param {Array<number>=} opt_flatCoords
+ * @param {number=} opt_offset
+ * @param {number=} opt_index
+ */
+const updateBillboard = (feature, geometry, style, context, bb, opt_flatCoords, opt_offset, opt_index) => {
+  // rotate on z-axis, so rotation references the cardinal direction.
+  // note: Cesium doesn't handle this well when the camera is rotated more than +/- 90 degrees from north.
+  bb.alignedAxis = Cesium.Cartesian3.UNIT_Z;
+
+  bb.horizontalOrigin = Cesium.HorizontalOrigin.CENTER;
+  bb.verticalOrigin = Cesium.VerticalOrigin.CENTER;
+
+  const scale = style.getScale();
+  bb.scale = scale != null ? scale : 1.0;
+  bb.rotation = -style.getRotation() || 0;
+
+  updateGeometry(geometry, bb, opt_flatCoords, opt_offset);
+  updateImage(feature, geometry, style, context, bb, opt_index);
+  updateColorAlpha(style, context, bb);
+
+  bb.heightReference = getHeightReference(context.layer, feature, geometry, opt_index);
+
+  if (bb instanceof Cesium.Billboard) {
+    // mark as updated so it isn't deleted
+    bb.dirty = false;
+  }
+};
+
+
+/**
  * @type {ol.Coordinate}
  * @const
  */
@@ -58,20 +95,12 @@ const scratchCoord2 = [];
 
 
 /**
- * Update a Cesium Billboard from an OpenLayers image style.
- *
- * @param {!Feature} feature
  * @param {!(Point|MultiPoint)} geometry
- * @param {!OLImageStyle} style
- * @param {!VectorContext} context
  * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
  * @param {Array<number>=} opt_flatCoords
  * @param {number=} opt_offset
- * @param {number=} opt_index
  */
-const updateBillboard = (feature, geometry, style, context, bb, opt_flatCoords, opt_offset, opt_index) => {
-  // update the position if the geometry changed
-  const layer = context.layer;
+const updateGeometry = (geometry, bb, opt_flatCoords, opt_offset) => {
   const geomRevision = geometry.getRevision();
   if (!bb.geomRevision || bb.geomRevision != geomRevision) {
     const flats = opt_flatCoords || geometry.getFlatCoordinates();
@@ -92,81 +121,107 @@ const updateBillboard = (feature, geometry, style, context, bb, opt_flatCoords, 
     bb.position = olcsCore.ol4326CoordinateToCesiumCartesian(coord);
     bb.geomRevision = geomRevision;
   }
+};
 
-  let imageId;
-  let image;
-  let iconColor;
 
+/**
+ * @param {!Feature} feature
+ * @param {!(Point|MultiPoint)} geometry
+ * @param {!OLImageStyle} style
+ * @param {!VectorContext} context
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ * @param {number=} opt_index
+ */
+const updateImage = (feature, geometry, style, context, bb, opt_index) => {
   if (style instanceof OLIconStyle) {
-    //
-    // Cesium should load icons directly instead of reusing the canvas from Openlayers. if the canvas is reused, each
-    // variation (color, scale, rotation, etc) of the same icon will be added to Cesium's texture atlas. this uses
-    // more memory than necessary, and is far more likely to hit the size limit for the atlas.
-    //
-    imageId = style.getSrc() || undefined;
-
-    if (imageId != bb.imageId && imageId != bb._imageId) {
-      image = iconStyleToImagePromise(feature, geometry, style, context, bb, opt_index);
-    }
-
-    const styleColor = style.getColor();
-    if (styleColor) {
-      iconColor = olcsCore.convertColorToCesium(styleColor);
-    }
+    updateImageIcon(feature, geometry, style, context, bb, opt_index);
   } else {
-    image = style.getImage(1);
+    updateImageShape(feature, geometry, style, context, bb);
+  }
+};
 
-    // Cesium uses the imageId to identify a texture in the WebGL texture atlas. this *must* be unique to the texture
-    // being displayed, but we want as much reuse as possible. we'll try:
-    //  - The style id that we use to cache OL3 styles
-    //  - Fall back on the UID of the image/canvas
-    imageId = style['id'] || ol.getUid(image);
+
+/**
+ *  Cesium should load icons directly instead of reusing the canvas from Openlayers. if the canvas is reused, each
+ *  variation (color, scale, rotation, etc) of the same icon will be added to Cesium's texture atlas. this uses
+ *  more memory than necessary, and is far more likely to hit the size limit for the atlas.
+ *
+ * @param {!Feature} feature
+ * @param {!(Point|MultiPoint)} geometry
+ * @param {!OLIconStyle} style
+ * @param {!VectorContext} context
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ * @param {number=} opt_index
+ */
+const updateImageIcon = (feature, geometry, style, context, bb, opt_index) => {
+  const imageId = style.getSrc();
+
+  if (imageId && imageId != bb.imageId && imageId != bb._imageId) {
+    const image = iconStyleToImagePromise(feature, geometry, style, context, bb, opt_index);
+    updateBillboardImage(bb, imageId, image);
   }
 
-  if (imageId) {
-    if (image && imageId != bb.imageId && imageId != bb._imageId) {
-      if (bb instanceof Cesium.Billboard) {
-        bb.setImage(imageId, image);
-        bb.pixelOffset.x = 0;
-        bb.pixelOffset.y = 0;
-      } else {
-        bb.image = image;
-        bb.imageId = imageId;
-        bb.pixelOffset = bb.pixelOffset || new Cesium.Cartesian2(0, 0);
-      }
-    }
+  const styleColor = style.getColor();
+  if (styleColor) {
+    bb.color = olcsCore.convertColorToCesium(styleColor);
+  }
 
-    // use the icon color if available, otherwise default to white to use the original image color
-    const color = iconColor || new Cesium.Color(1.0, 1.0, 1.0, 1.0);
-    color.alpha = layer.getOpacity();
-    const opacity = style.getOpacity();
-    if (opacity != null) {
-      color.alpha = color.alpha * opacity;
-    }
+  updateSizeDynamicIconProperties(style, bb);
+};
 
-    bb.color = color;
 
-    const scale = style.getScale();
-    bb.scale = scale != null ? scale : 1.0;
-    bb.rotation = -style.getRotation() || 0;
+/**
+ * @param {!Feature} feature
+ * @param {!(Point|MultiPoint)} geometry
+ * @param {!OLImageStyle} style
+ * @param {!VectorContext} context
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ */
+const updateImageShape = (feature, geometry, style, context, bb) => {
+  const image = style.getImage(1);
 
-    // rotate on z-axis, so rotation references the cardinal direction.
-    // note: Cesium doesn't handle this well when the camera is rotated more than +/- 90 degrees from north.
-    bb.alignedAxis = Cesium.Cartesian3.UNIT_Z;
+  // Cesium uses the imageId to identify a texture in the WebGL texture atlas. this *must* be unique to the texture
+  // being displayed, but we want as much reuse as possible. we'll try:
+  //  - The style id that we use to cache OL3 styles
+  //  - Fall back on the UID of the image/canvas
+  const imageId = style['id'] || ol.getUid(image);
 
-    bb.horizontalOrigin = Cesium.HorizontalOrigin.CENTER;
-    bb.verticalOrigin = Cesium.VerticalOrigin.CENTER;
+  if (image && imageId != bb.imageId && imageId != bb._imageId) {
+    updateBillboardImage(bb, imageId, image);
+  }
 
-    if (style instanceof OLIconStyle) {
-      updateSizeDynamicIconProperties(style, bb);
-    }
+  bb.color = bb.color || new Cesium.Color(1.0, 1.0, 1.0, 1.0);
+};
 
-    bb.heightReference = getHeightReference(layer, feature, geometry, opt_index);
 
-    if (bb instanceof Cesium.Billboard) {
-      // mark as updated so it isn't deleted
-      bb.dirty = false;
-    }
+/**
+ * @param {!OLImageStyle} style
+ * @param {!VectorContext} context
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ */
+const updateColorAlpha = (style, context, bb) => {
+  bb.color.alpha = context.layer.getOpacity();
+  const opacity = style.getOpacity();
+  if (opacity != null) {
+    bb.color.alpha *= opacity;
+  }
+};
+
+
+/**
+ * @param {!(Cesium.Billboard|Cesium.optionsBillboardCollectionAdd)} bb
+ * @param {!string} imageId
+ * @param {Cesium.ImageLike|Promise<Cesium.ImageLike>} image
+ */
+const updateBillboardImage = (bb, imageId, image) => {
+  if (bb instanceof Cesium.Billboard) {
+    bb.setImage(imageId, image);
+    bb.pixelOffset.x = 0;
+    bb.pixelOffset.y = 0;
+  } else {
+    bb.image = image;
+    bb.imageId = imageId;
+    bb.pixelOffset = bb.pixelOffset || new Cesium.Cartesian2(0, 0);
   }
 };
 
