638677d8ec8e7aaa7fe0624acee29482eadf99b1
Mike Roth <Michael.Roth@caci.com>
2020-06-25 16:14:14 -0600

    feat(alerts): throttle duplicated alerts
    UUOM-1063

--- test/os/alert/alertmanager.test.js
+++ test/os/alert/alertmanager.test.js
@@ -5,111 +5,217 @@ goog.require('os.alert.AlertManager');
 goog.require('os.structs.EventType');
 
 describe('os.alert.AlertManager', function() {
-  var alertManager = new os.alert.AlertManager();
+  let alertManager = new os.alert.AlertManager();
+  let waitTime = os.alert.AlertManager.DEFAULT_THROTTLE_TIME + 250;
 
   it('should handle alerts and and add them to its buffer', function() {
-    var alert1 = 'test alert';
-    var alert2 = 'test alert 2';
+    const alert1 = 'test alert';
+    const alert2 = 'test alert 2';
+
     expect(alertManager.getAlerts().getCount()).toBe(0);
 
-    alertManager.sendAlert(alert1);
-    expect(alertManager.getAlerts().getCount()).toBe(1);
-    expect(alertManager.getAlerts().getLast().getMessage()).toBe('test alert');
-    alertManager.sendAlert(alert2);
-    expect(alertManager.getAlerts().getCount()).toBe(2);
-    expect(alertManager.getAlerts().getLast().getMessage()).toBe('test alert 2');
-  });
+    alertManager.sendAlert(alert1).then(() => {
+      expect(alertManager.getAlerts().getCount()).toBe(1);
+      expect(alertManager.getAlerts().getLast().getMessage()).toBe('test alert');
 
-  it('should dispatch alert messages', function() {
-    var msg = '';
-    alertManager.listenOnce(os.structs.EventType.ALERT, function(e) {
-      msg = e.getMessage();
-      expect(msg).toBe('alert3');
-    }, false, this);
-    var alert3 = 'alert3';
-    alertManager.sendAlert(alert3);
-  });
-
-  it('should attach severities and times to alerts', function() {
-    var alert4 = 'test alert 4';
-    alertManager.sendAlert(alert4, os.alert.AlertEventSeverity.WARNING);
-    expect(alertManager.getAlerts().getLast().getTime()).not.toBe(null);
-    expect(alertManager.getAlerts().getLast().getSeverity()).toBe(os.alert.AlertEventSeverity.WARNING);
+      alertManager.sendAlert(alert2).then(() => {
+        expect(alertManager.getAlerts().getCount()).toBe(2);
+        expect(alertManager.getAlerts().getLast().getMessage()).toBe('test alert 2');
+      });
+    });
   });
 
   it('should be able to reset the number of new alerts and elements in the buffer', function() {
-    expect(alertManager.getAlerts().getCount()).toBe(4);
-    alertManager.clearAlerts();
-    expect(alertManager.getAlerts().getCount()).toBe(0);
+    const am = new os.alert.AlertManager();
+    const alert1 = 'test alert';
+    am.sendAlert(alert1).then(() => {
+      expect(am.getAlerts().getCount()).toBe(1);
+      am.clearAlerts();
+      expect(am.getAlerts().getCount()).toBe(0);
+    });
   });
 
-  it('should process misssed alerts', function() {
-    var clientId = 'testalertclient';
-    var am = new os.alert.AlertManager();
+  it('should dispatch alert messages', function() {
+    const am = new os.alert.AlertManager();
+    const alert3 = 'alert3';
+    let msg = '';
+    am.listenOnce(os.structs.EventType.ALERT, function(e) {
+      msg = e.getMessage();
+      expect(msg).toBe(alert3);
+    }, false, this);
+    am.sendAlert(alert3);
+  });
 
-    var callback = {
-      handleAlert: function() {}
+  it('should attach severities and times to alerts', function() {
+    const am = new os.alert.AlertManager();
+    const alert4 = 'test alert 4';
+
+    am.sendAlert(alert4, os.alert.AlertEventSeverity.WARNING).then(() => {
+      expect(am.getAlerts().getLast().getTime()).not.toBe(null);
+      expect(am.getAlerts().getLast().getSeverity()).toBe(os.alert.AlertEventSeverity.WARNING);
+    });
+  });
+
+  it('should process missed alerts', function() {
+    const am = new os.alert.AlertManager();
+    const clientId = 'testalertclient';
+
+    const callback = {
+      handleAlert: function () {
+      }
     };
     spyOn(callback, 'handleAlert');
 
-    am.sendAlert('alert1');
-    am.sendAlert('alert2');
+    const promises = [];
+    promises.push(am.sendAlert('alert1'));
+    promises.push(am.sendAlert('alert2'));
+    Promise.all(promises).then((resolve, reject) => {
+      // handle missed
+      am.processMissedAlerts(clientId, callback.handleAlert);
+      expect(callback.handleAlert.calls.length).toBe(2);
 
-    // handle missed
-    am.processMissedAlerts(clientId, callback.handleAlert);
-    expect(callback.handleAlert.calls.length).toBe(2);
+      // try again and should have no more
+      am.processMissedAlerts(clientId, callback.handleAlert);
+      expect(callback.handleAlert.calls.length).toBe(2);
 
-    // try again and should have no more
-    am.processMissedAlerts(clientId, callback.handleAlert);
-    expect(callback.handleAlert.calls.length).toBe(2);
-
-    // one more alert
-    am.sendAlert('alert3');
-
-    // subsequent calls to processMissedAlerts should be ignored
-    am.processMissedAlerts(clientId, callback.handleAlert);
-    expect(callback.handleAlert.calls.length).toBe(2);
+      // one more alert
+      am.sendAlert('alert3').then(() => {
+        // subsequent calls to processMissedAlerts should be ignored
+        am.processMissedAlerts(clientId, callback.handleAlert);
+        expect(callback.handleAlert.calls.length).toBe(2);
+      });
+    });
   });
 
   it('should optionally log alerts', function() {
     // mock up a logger
-    var MockLogger = function() {
-      this.count = 0;
-      this.any = function(severity, message, opt_exception) {
-        expect(severity).toBe(this.severity);
-        expect(message).toBe(this.message);
-        this.count++;
+    const MockLogger = function () {
+      this.records = [];
+      this.any = function (severity, message, opt_exception) {
+        this.records.push({
+          level: severity,
+          msg: message,
+          exception: opt_exception
+        });
       };
 
-      this.info = function(message, e) {
+      this.info = function (message, e) {
         this.any(os.alert.AlertEventSeverity.INFO, message, e);
       };
 
-      this.warning = function(message, e) {
+      this.warning = function (message, e) {
         this.any(os.alert.AlertEventSeverity.WARNING, message, e);
       };
 
-      this.severe = function(message, e) {
+      this.severe = function (message, e) {
         this.any(os.alert.AlertEventSeverity.ERROR, message, e);
       };
-
-      message: 'This is a test';
-      severity: null;
     };
 
-    var am = new os.alert.AlertManager();
-    var logger = new MockLogger();
+    const am = new os.alert.AlertManager();
+    const logger = new MockLogger();
+    const promises = [];
 
-    for (var key in os.alert.AlertEventSeverity) {
-      logger.severity = os.alert.AlertEventSeverity[key];
-
-      if (logger.severity === os.alert.AlertEventSeverity.SUCCESS) {
-        logger.severity = os.alert.AlertEventSeverity.INFO;
-      }
-
-      am.sendAlert(logger.message, os.alert.AlertEventSeverity[key], logger);
+    for (let key in os.alert.AlertEventSeverity) {
+      promises.push(am.sendAlert('alert', os.alert.AlertEventSeverity[key], logger));
     }
 
-    expect(logger.count).toBeGreaterThan(0);
+    Promise.all(promises).then(() => {
+      // 4 unique alerts
+      expect(logger.records.length).toBe(4);
+      expect(logger.records.filter((r) => r.level.name  === os.alert.AlertEventSeverity.INFO.name).length).toBe(2);
+      expect(logger.records.filter((r) => r.level.name  === os.alert.AlertEventSeverity.WARNING.name).length).toBe(1);
+      expect(logger.records.filter((r) => r.level.name  === os.alert.AlertEventSeverity.ERROR.name).length).toBe(1);
+    });
+
+    const am2 = new os.alert.AlertManager();
+    const logger2 = new MockLogger();
+    const promises2 = [];
+
+    for (let key in os.alert.AlertEventSeverity) {
+      if (os.alert.AlertEventSeverity[key] === os.alert.AlertEventSeverity.WARNING) {
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+      } else if (os.alert.AlertEventSeverity[key] === os.alert.AlertEventSeverity.ERROR) {
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+      } else {
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+        promises2.push(am2.sendAlert(`${key} alert`, os.alert.AlertEventSeverity[key], logger2));
+      }
+    }
+
+    Promise.all(promises2).then(() => {
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.INFO.name).length).toBe(2);
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.INFO.name &&
+          r.msg.indexOf(' (6)')).length).toBeGreaterThan(-1);
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.WARNING.name).length).toBe(1);
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.WARNING.name &&
+          r.msg.indexOf(' (2)')).length).toBeGreaterThan(-1);
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.ERROR.name).length).toBe(1);
+      expect(logger2.records.filter((r) => r.level.name === os.alert.AlertEventSeverity.ERROR.name &&
+          r.msg.match(/\s+\(\d+\)$/)).length).toBe(0);
+    });
+  });
+
+  it('should throttle duplicate alerts', function() {
+    const am = new os.alert.AlertManager();
+    // Throttle duplicate alerts.
+    const alert4 = 'alert 4';
+    const promises = [];
+
+    promises.push(am.sendAlert(alert4));
+    promises.push(am.sendAlert(alert4));
+    Promise.all(promises).then(() => {
+      expect(am.getAlerts().getLast().getCount()).toBe(2);
+      expect(am.getAlerts().getCount()).toBe(1);
+    });
+  });
+
+  it('should not throttle different alerts', function() {
+    const am = new os.alert.AlertManager();
+    // Different alerts. No throttle.
+    const alert5 = 'alert 5';
+    const alert6 = 'alert 6';
+    const promises = [];
+
+    promises.push(am.sendAlert(alert5));
+    promises.push(am.sendAlert(alert6));
+    Promise.all(promises).then(() => {
+      const last = am.getAlerts().getLast();
+      expect(last.getMessage()).toBe(alert6);
+      expect(last.getCount()).toBe(1);
+      expect(am.getAlerts().getCount()).toBe(2);
+    });
+  });
+
+  it('should send a duplicate alert after the throttle interval', function() {
+    const am = new os.alert.AlertManager();
+    // Alert can be sent after throttle interval.
+    const alert7 = 'alert 7';
+    am.sendAlert(alert7).then(() => {
+      let last = am.getAlerts().getLast();
+      expect(last.getMessage()).toBe(alert7);
+      expect(last.getCount()).toBe(1);
+
+      am.sendAlert(alert7).then(() => {
+        last = am.getAlerts().getLast();
+        expect(last.getMessage()).toBe(alert7);
+        expect(last.getCount()).toBe(1);
+        expect(am.getAlerts().getCount()).toBe(2);
+      });
+    });
+  });
+
+  it('should allow unthrottled alerts', function() {
+    const am = new os.alert.AlertManager();
+    const alert8 = 'alert 8';
+
+    am.sendAlert(alert8, undefined, undefined, undefined, undefined, 0);
+    am.sendAlert(alert8, undefined, undefined, undefined, undefined, 0);
+    const last = am.getAlerts().getLast();
+    expect(last.getMessage()).toBe(alert8);
+    expect(last.getCount()).toBe(1);
+    expect(am.getAlerts().getCount()).toBe(2);
   });
 });
