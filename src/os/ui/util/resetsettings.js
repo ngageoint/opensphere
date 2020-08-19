ac66e8ce88f9b90d89f2b07dcb3b73c9a28b7fe9
Erica Ecklund <ecklunde@wudn-ecklunde.udev.six3>
2020-08-19 10:25:31 -0600

    feat(settings): don't allow reset settings with multiple apps opened

--- src/os/ui/util/resetsettings.js
+++ src/os/ui/util/resetsettings.js
@@ -14,34 +14,53 @@ goog.require('os.ui.window.ConfirmUI');
  * @param {string=} opt_parent Optional parent selector.
  */
 os.ui.util.resetSettings = function(opt_parent) {
-  var windowOptions = {
-    'label': 'Reset Settings',
-    'icon': 'fa fa-refresh',
-    'x': 'center',
-    'y': 300,
-    'width': 400,
-    'height': 'auto',
-    'modal': true,
-    'no-scroll': true,
-    'disable-drag': true,
-    'headerClass': 'bg-danger u-bg-danger-text'
-  };
-  if (opt_parent) {
-    windowOptions['window-container'] = opt_parent;
+  if (os.config.Settings.getInstance().getPeerInfo(os.config.appNs).length) {
+    os.ui.window.ConfirmUI.launchConfirm(/** @type {osx.window.ConfirmOptions} */ ({
+      'prompt': 'In order to Reset Settings, ' +
+      'you may not have the same application opened multiple times.  Please close all but one, and retry.',
+      'yesText': 'Ok',
+      'noText': '',
+      'windowOptions': {
+        'id': 'resetDisabled',
+        'label': 'Reset Disabled',
+        'icon': 'fa fa-refresh',
+        'width': '400',
+        'height': 'auto',
+        'no-scroll': true,
+        'modal': true,
+        'headerClass': 'bg-danger u-bg-danger-text'
+      }
+    }));
+  } else {
+    var windowOptions = {
+      'label': 'Reset Settings',
+      'icon': 'fa fa-refresh',
+      'x': 'center',
+      'y': 300,
+      'width': 400,
+      'height': 'auto',
+      'modal': true,
+      'no-scroll': true,
+      'disable-drag': true,
+      'headerClass': 'bg-danger u-bg-danger-text'
+    };
+    if (opt_parent) {
+      windowOptions['window-container'] = opt_parent;
+    }
+
+    var text = 'This action will clear all locally saved application settings and <b>reload the current page.</b> ';
+    text += 'The current state of your application will be lost, so be sure you\'re at a good stopping point.<br>';
+    text += '<br>Last reset: ' + os.config.Settings.getInstance().getLastReset();
+    text += '<br><br><b>Are you sure you want to clear your settings and reload?</b>';
+
+    os.ui.window.ConfirmUI.launchConfirm(/** @type {osx.window.ConfirmOptions} */ ({
+      confirm: os.storage.clearStorage,
+      prompt: text,
+      yesText: 'Clear and Reload',
+      yesButtonClass: 'btn-danger',
+      windowOptions: windowOptions
+    }));
   }
-
-  var text = 'This action will clear all locally saved application settings and <b>reload the current page.</b> ';
-  text += 'The current state of your application will be lost, so be sure you\'re at a good stopping point.<br>';
-  text += '<br>Last reset: ' + os.config.Settings.getInstance().getLastReset();
-  text += '<br><br><b>Are you sure you want to clear your settings and reload?</b>';
-
-  os.ui.window.ConfirmUI.launchConfirm(/** @type {osx.window.ConfirmOptions} */ ({
-    confirm: os.storage.clearStorage,
-    prompt: text,
-    yesText: 'Clear and Reload',
-    yesButtonClass: 'btn-danger',
-    windowOptions: windowOptions
-  }));
 };
 
 
