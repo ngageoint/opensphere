ef2adf09d9679fee3e5fedda5f95dad950115c7b
Kevin Schmidt <kschmidt@caci.com>
2020-08-27 14:50:38 -0600

    fix(vectortile): fix hit detection in 3D mode

--- src/plugin/vectortile/doubleclickinteraction.js
+++ src/plugin/vectortile/doubleclickinteraction.js
@@ -8,36 +8,121 @@ const OLMap = goog.require('ol.Map');
 const MapBrowserEventType = goog.require('ol.MapBrowserEventType');
 const ViewHint = goog.require('ol.ViewHint');
 const Interaction = goog.require('ol.interaction.Interaction');
-const Feature = goog.require('ol.render.Feature');
+const RenderFeature = goog.require('ol.render.Feature');
+
 const I3DSupport = goog.require('os.I3DSupport');
+const MapContainer = goog.require('os.MapContainer');
 const osImplements = goog.require('os.implements');
 const VectorTile = goog.require('os.layer.VectorTile');
 
+const Feature = goog.requireType('ol.Feature');
+const MapBrowserEvent = goog.requireType('ol.MapBrowserEvent');
+const PluggableMap = goog.requireType('ol.PluggableMap');
+const OLLayer = goog.requireType('ol.layer.Layer');
+const VectorTileSource = goog.requireType('ol.source.VectorTile');
 const OSMap = goog.requireType('os.Map');
 
 
 /**
+ * Array to hold hit features.
+ * @type {!Array<Feature|RenderFeature>}
+ */
+const features = [];
+
+
+/**
+ * Handle hit feature.
+ * @param {Feature|RenderFeature} feature The feature.
+ * @param {OLLayer} layer The layer.
+ */
+const forEachFeatureCallback = (feature, layer) => {
+  if (layer instanceof VectorTile && feature instanceof RenderFeature) {
+    if (feature['id'] == null) {
+      feature['id'] = getUid(feature);
+    }
+    features.push(feature);
+  }
+};
+
+
+/**
+ * When OpenLayers isn't the active renderer, vector tile layers are not being rendered fully. We render tiles in the
+ * VectorImageTile mixin, but this does not set appropriate fields for hit detection.
+ *
+ * This function locates the first rendered tile with a replay group at the current zoom level and event coordinate and
+ * uses that for hit detection.
+ *
+ * @param {PluggableMap} map The OL map.
+ * @param {VectorTile} layer The layer.
+ * @param {ol.Coordinate} coordinate The event coordinate.
+ *
+ * @suppress {accessControls} To allow access to frameState and renderedTiles.
+ */
+const glForEachFeatureAtCoordinate = (map, layer, coordinate) => {
+  const frameState = map.frameState_;
+  const source = /** @type {VectorTileSource} */ (layer.getSource());
+  const resolution = map.getView().getResolution();
+  if (frameState && source && resolution != null) {
+    const tileGrid = source.getTileGridForProjection(frameState.viewState.projection);
+    const minZoom = tileGrid.getMinZoom();
+
+    let z = tileGrid.getZForResolution(resolution);
+    let tile;
+
+    while (z >= minZoom) {
+      const tileCoord = tileGrid.getTileCoordForCoordAndZ(coordinate, z);
+      if (tileCoord) {
+        tile = source.getTile(...tileCoord, frameState.pixelRatio, frameState.viewState.projection);
+
+        if (tile) {
+          const tileCoordKey = tileCoord.toString();
+          const sourceTile = tile.getTile(tileCoordKey);
+          if (sourceTile && sourceTile.getReplayGroup(layer, tileCoordKey)) {
+            break;
+          }
+        }
+      }
+
+      tile = undefined;
+      z--;
+    }
+
+    if (tile) {
+      const renderer = layer.getRenderer();
+      renderer.renderedTiles.length = 0;
+      renderer.renderedTiles.push(tile);
+
+      renderer.forEachFeatureAtCoordinate(coordinate, frameState, 0, forEachFeatureCallback, this);
+    }
+  }
+};
+
+
+/**
  * Double click event handler.
- * @param {ol.MapBrowserEvent} mapBrowserEvent Map browser event.
+ * @param {MapBrowserEvent} mapBrowserEvent Map browser event.
  * @return {boolean} 'false' to stop event propagation.
  */
 const handleEvent = (mapBrowserEvent) => {
   const map = mapBrowserEvent.map;
-  const features = [];
+  features.length = 0;
 
   if (mapBrowserEvent.type == MapBrowserEventType.DBLCLICK && map.getView().getHints()[ViewHint.INTERACTING] == 0) {
-    // Do a 2D-only forEachFeatureAtPixel for items in a VectorTileLayer. 3D contexts
-    // only have the resulting raster tile.
-    const pixel2D = /** @type {OSMap} */ (map).get2DPixelFromCoordinate(mapBrowserEvent.coordinate);
-    OLMap.prototype.forEachFeatureAtPixel.call(map, pixel2D, function(feature, layer) {
-      if (layer instanceof VectorTile && feature instanceof Feature) {
-        feature['id'] = getUid(feature);
-        features.push(feature);
-      }
-    });
+    const mapContainer = MapContainer.getInstance();
+    if (map && mapContainer.is3DEnabled()) {
+      // forEachFeatureAtPixel does not work in 2D mode because layers are marked as not visible in the frame state
+      const vectorTileLayers = /** @type {Array<VectorTile>} */ (mapContainer.getLayers()
+          .filter(MapContainer.isVectorTileLayer));
+      vectorTileLayers.forEach((layer) => {
+        glForEachFeatureAtCoordinate(map, layer, mapBrowserEvent.coordinate);
+      });
+    } else {
+      const pixel2D = /** @type {OSMap} */ (map).get2DPixelFromCoordinate(mapBrowserEvent.coordinate);
+      OLMap.prototype.forEachFeatureAtPixel.call(map, pixel2D, forEachFeatureCallback);
+    }
 
     if (features.length > 0) {
-      os.ui.feature.launchMultiFeatureInfo(features);
+      os.ui.feature.launchMultiFeatureInfo(features.slice());
     }
   }
 
