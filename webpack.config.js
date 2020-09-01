fa9c276a68654d9a095c3d5926f97f324b6b19af
Kevin Schmidt <kschmidt@caci.com>
2020-09-01 08:03:22 -0600

    build(webpack): use webpack for the debug build
    BREAKING CHANGE: Plugins providing their own index must add the debugJs
    property pointing at the webpack development bundle (.build/opensphere.js).

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const ClosurePlugin = require('closure-webpack-plugin');
+const path = require('path');
+
+const buildDir = path.resolve(__dirname, '.build');
+const gccOptions = require(path.join(buildDir, 'gcc-webpack'));
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production' || !argv.mode;
+
+  return {
+    entry: [
+      path.join(buildDir, 'index.js')
+    ],
+    output: {
+      path: buildDir,
+      filename: isProduction ? 'opensphere.min.js' : 'opensphere.js'
+    },
+    devtool: 'source-map',
+    watch: !isProduction,
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new ClosurePlugin({mode: 'AGGRESSIVE_BUNDLE'}, gccOptions)
+      ],
+      concatenateModules: false,
+      splitChunks: {
+        minSize: 0
+      }
+    },
+    plugins: [
+      new ClosurePlugin.LibraryPlugin({
+        closureLibraryBase: require.resolve('google-closure-library/closure/goog/base'),
+        deps: [
+          require.resolve('google-closure-library/closure/goog/deps'),
+          require.resolve('opensphere/.build/deps')
+        ]
+      })
+    ]
+  };
+};
