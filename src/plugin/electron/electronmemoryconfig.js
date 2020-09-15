438032ae8cac10bb06a89b59bcfcc5b993f5b010
Brandon Cleveland <bcleveland@caci.com>
2020-09-15 15:40:58 -0600

    feat(memory): esSixify electronmemoryconfig.js

--- src/plugin/electron/electronmemoryconfig.js
+++ src/plugin/electron/electronmemoryconfig.js
@@ -1,64 +1,72 @@
-goog.provide('plugin.electron.ElectronMemoryConfigCtrl');
-goog.provide('plugin.electron.electronMemoryConfigDirective');
+goog.module('plugin.electron.ElectronMemoryConfigUI');
+goog.module.declareLegacyNamespace();
 
-goog.require('os.ui.Module');
+const Module = goog.require('os.ui.Module');
+
 
 /**
  * The electron configuration directive.
  * @return {angular.Directive}
  */
-plugin.electron.electronMemoryConfigDirective = function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    templateUrl: os.ROOT + 'views/plugin/electron/electronmemoryconfig.html',
-    controller: plugin.electron.ElectronMemoryConfigCtrl,
-    controllerAs: 'ctrl'
-  };
-};
+const directive = () => ({
+  restrict: 'E',
+  replace: true,
+  templateUrl: os.ROOT + 'views/plugin/electron/electronmemoryconfig.html',
+  controller: Controller,
+  controllerAs: 'ctrl'
+});
 
 
 /**
  * Add the directive to the module
  */
-os.ui.Module.directive('electronmemoryconfig', [plugin.electron.electronMemoryConfigDirective]);
+Module.directive('electronmemoryconfig', [directive]);
 
 
 
 /**
  * Controller function for the electron configuration.
- * @param {!angular.Scope} $scope The Angular scope
- * @param {!angular.JQLite} $element The root DOM element
- * @constructor
- * @ngInject
+ * @unrestricted
  */
-plugin.electron.ElectronMemoryConfigCtrl = function($scope, $element) {
-  this['maxMemory'] = ElectronOS.getMaxMemory();
-  this['minMemory'] = 512;
-  this['systemMemory'] = ElectronOS.getSystemMemory();
-  this['restartButtonActive'] = false;
-};
+class Controller {
+  /**
+   * Constructor.
+   * @param {!angular.Scope} $scope The Angular scope
+   * @param {!angular.JQLite} $element The root DOM element
+   * @ngInject
+   */
+  constructor($scope, $element) {
+    this['maxMemory'] = ElectronOS.getMaxMemory();
+    this['minMemory'] = 512;
+    this['systemMemory'] = ElectronOS.getSystemMemory();
+    this['restartButtonActive'] = false;
+  }
 
-
-/**
- * @export
- */
-plugin.electron.ElectronMemoryConfigCtrl.prototype.update = function() {
-  if (this['maxMemory']) {
-    const maxMem = this['maxMemory'];
-    if (maxMem >= this['minMemory'] && maxMem <= this['systemMemory']) {
-      this['restartButtonActive'] = true;
-    } else {
-      this['restartButtonActive'] = false;
+  /**
+   * @export
+   */
+  update() {
+    if (this['maxMemory']) {
+      const maxMem = this['maxMemory'];
+      if (maxMem >= this['minMemory'] && maxMem <= this['systemMemory']) {
+        this['restartButtonActive'] = true;
+      } else {
+        this['restartButtonActive'] = false;
+      }
     }
   }
-};
 
-/**
- * Restarts the application.
- * @export
- */
-plugin.electron.ElectronMemoryConfigCtrl.prototype.restart = function() {
-  ElectronOS.setMaxMemory(this['maxMemory']);
-  ElectronOS.restart();
+  /**
+   * Restarts the application.
+   * @export
+   */
+  restart() {
+    ElectronOS.setMaxMemory(this['maxMemory']);
+    ElectronOS.restart();
+  }
+}
+
+exports = {
+  Controller,
+  directive
 };
