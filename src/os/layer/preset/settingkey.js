94c4d4314730fe5fe3cb198195b3395934d5d112
Glenn Kitchell <kitchellg@wudn-kitchellg.udev.six3>
2020-10-06 15:34:58 -0600

    fix(es6): fix es6 transform
    small changes to fix es6 transform

--- src/os/layer/preset/settingkey.js
+++ src/os/layer/preset/settingkey.js
@@ -1,6 +1,9 @@
 goog.module('os.layer.preset.SettingKey');
 goog.module.declareLegacyNamespace();
 
+const {BASE_KEY} = goog.require('os.layer.preset');
+
+
 /**
  * Settings keys for layer presets.
  * @enum {string}
