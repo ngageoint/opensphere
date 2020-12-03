4bdf3b846f25a8a051d5c03f0be9d5558dcc2f61
Kevin Wilson <kevin.wilson1@caci.com>
2020-12-03 07:48:23 -0700

    feat(toastui): upgrade toast with better integration

--- externs/tui-editor.externs.js
+++ externs/tui-editor.externs.js
@@ -3,42 +3,42 @@
  *
  * @externs
  */
-var tui = {};
+var toastui = {};
 
 
 /**
  * @constructor
  */
-tui.EventManager = function() {};
+toastui.EventManager = function() {};
 
 
 /**
  * @param {string} eventName
  * @param {function()} cb
  */
-tui.EventManager.prototype.listen = function(eventName, cb) {};
+toastui.EventManager.prototype.listen = function(eventName, cb) {};
 
 
 /**
  * @param {string} eventName
  * @param {function()} cb
  */
-tui.EventManager.prototype.addEventType = function(eventName, cb) {};
+toastui.EventManager.prototype.addEventType = function(eventName, cb) {};
 
 
 /**
  * @param {string} eventName
  */
-tui.EventManager.prototype.emit = function(eventName) {};
+toastui.EventManager.prototype.emit = function(eventName) {};
 
 
 /**
  * @constructor
  * @param {Object} options
  */
-tui.Editor = function(options) {
+toastui.Editor = function(options) {
   /**
-   * @type {tui.EventManager}
+   * @type {toastui.EventManager}
    */
   this.eventManager;
 };
@@ -47,71 +47,78 @@ tui.Editor = function(options) {
 /**
  * @return {string}
  */
-tui.Editor.prototype.getHtml = function() {};
+toastui.Editor.prototype.getHtml = function() {};
 
 
 /**
  * @return {string}
  */
-tui.Editor.prototype.getValue = function() {};
+toastui.Editor.prototype.getValue = function() {};
 
 
 /**
  * @param {string} markdown
  */
-tui.Editor.prototype.setValue = function(markdown) {};
+toastui.Editor.prototype.setValue = function(markdown) {};
 
 
 /**
  * @return {boolean}
  */
-tui.Editor.prototype.isMarkdownMode = function() {};
+toastui.Editor.prototype.isMarkdownMode = function() {};
 
 
 /**
  * @param {string} text
  */
-tui.Editor.prototype.insertText = function(text) {};
+toastui.Editor.prototype.insertText = function(text) {};
+
+
+/**
+ * @param {string} code
+ * @param {Object} data
+ */
+toastui.Editor.setLanguage = function(code, data) {};
 
 
 /**
  * @type {Object}
  */
-tui.Editor.markdownit = {};
+toastui.Editor.markdownit = {};
 
 
 /**
  * @param {string} markdown
  * @return {string}
  */
-tui.Editor.markdownit.render = function(markdown) {};
+toastui.Editor.markdownit.render = function(markdown) {};
 
 
 /**
  * @param {Object} options
  * @return {Object}
  */
-tui.Editor.factory = function(options) {};
+toastui.Editor.factory = function(options) {};
 
 
 /**
  * @type {Object}
  */
-tui.Editor.mdEditor = {};
+toastui.Editor.mdEditor = {};
 
 /**
  * @type {Object}
  */
-tui.Editor.mdEditor.cm = {};
+toastui.Editor.mdEditor.cm = {};
 
 
 /**
  * kicks codemirror to update
  */
-tui.Editor.mdEditor.cm.refresh = function() {};
+toastui.Editor.mdEditor.cm.refresh = function() {};
 
 
 /**
  * @type {Object<string, Map>}
  */
-tui.Editor.i18n = {'_langs': {}};
+toastui.Editor.i18n = {'_langs': {}};
