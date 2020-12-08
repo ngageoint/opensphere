1efdb36c2a48683232834052fe080b83230e9b9f
Connor Gibson <connor.gibson@caci.com>
2020-12-08 16:22:38 -0700

    refactor(es6): run jscodeshift on opensphere/src/plugin/ogc/wms

--- src/plugin/ogc/wms/tilewmssource.js
+++ src/plugin/ogc/wms/tilewmssource.js
@@ -1,64 +1,71 @@
-goog.provide('plugin.ogc.wms.TileWMSSource');
-goog.require('ol.source.TileWMS');
-goog.require('os.events.PropertyChangeEvent');
-goog.require('os.implements');
-goog.require('os.ol.source.ILoadingSource');
+goog.module('plugin.ogc.wms.TileWMSSource');
+goog.module.declareLegacyNamespace();
+
+const TileWMS = goog.require('ol.source.TileWMS');
+const PropertyChangeEvent = goog.require('os.events.PropertyChangeEvent');
+const osImplements = goog.require('os.implements');
+const IStyle = goog.require('os.source.IStyle');
+const ILoadingSource = goog.requireType('os.ol.source.ILoadingSource');
+
+
 goog.require('os.source.IFilterableTileSource');
-goog.require('os.source.IStyle');
+
 goog.require('os.tile.ColorableTile');
-
-
 
 /**
  * Layer source for tile data from WMS servers. This source fires a property change event when its
  * loading state changes based on how many tiles are currently in a loading state.
  *
- * @param {olx.source.TileWMSOptions=} opt_options Tile WMS options.
- * @implements {os.ol.source.ILoadingSource}
- * @implements {os.source.IStyle}
- * @extends {ol.source.TileWMS}
- * @constructor
+ * @implements {ILoadingSource}
+ * @implements {IStyle}
  */
-plugin.ogc.wms.TileWMSSource = function(opt_options) {
-  plugin.ogc.wms.TileWMSSource.base(this, 'constructor', opt_options);
-  this.refreshEnabled = true;
-};
-goog.inherits(plugin.ogc.wms.TileWMSSource, ol.source.TileWMS);
-os.implements(plugin.ogc.wms.TileWMSSource, os.source.IStyle.ID);
-
-
-/**
- * @return {?(string|osx.ogc.TileStyle)}
- * @override
- */
-plugin.ogc.wms.TileWMSSource.prototype.getStyle = function() {
-  var params = this.getParams();
-
-  if (params) {
-    var style = params['STYLES'] || '';
+class TileWMSSource extends TileWMS {
+  /**
+   * Constructor.
+   * @param {olx.source.TileWMSOptions=} opt_options Tile WMS options.
+   */
+  constructor(opt_options) {
+    super(opt_options);
+    this.refreshEnabled = true;
   }
 
-  return style;
-};
-
-
-/**
- * @param {?(string|osx.ogc.TileStyle)} value
- * @override
- */
-plugin.ogc.wms.TileWMSSource.prototype.setStyle = function(value) {
-  var style = typeof value == 'string' ? value : value != null ? value.data : '';
-
-  if (style != this.getStyle()) {
+  /**
+   * @return {?(string|osx.ogc.TileStyle)}
+   * @override
+   */
+  getStyle() {
     var params = this.getParams();
-    params['STYLES'] = style;
 
-    // clear the tile cache or tiles from the old style may be temporarily displayed while the new tiles are loaded
-    this.tileCache.clear();
+    if (params) {
+      var style = params['STYLES'] || '';
+    }
 
-    // update params, which will trigger a tile refresh
-    this.updateParams(params);
-
-    this.dispatchEvent(new os.events.PropertyChangeEvent(os.source.PropertyChange.STYLE));
+    return style;
   }
-};
+
+  /**
+   * @param {?(string|osx.ogc.TileStyle)} value
+   * @override
+   */
+  setStyle(value) {
+    var style = typeof value == 'string' ? value : value != null ? value.data : '';
+
+    if (style != this.getStyle()) {
+      var params = this.getParams();
+      params['STYLES'] = style;
+
+      // clear the tile cache or tiles from the old style may be temporarily displayed while the new tiles are loaded
+      this.tileCache.clear();
+
+      // update params, which will trigger a tile refresh
+      this.updateParams(params);
+
+      this.dispatchEvent(new PropertyChangeEvent(os.source.PropertyChange.STYLE));
+    }
+  }
+}
+
+osImplements(TileWMSSource, IStyle.ID);
+
+
+exports = TileWMSSource;
