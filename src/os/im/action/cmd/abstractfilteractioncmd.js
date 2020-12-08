cc109a1ade330d148cd6da2ed98e3c54dd5b2a8e
Connor Gibson <connor.gibson@caci.com>
2020-12-08 15:56:47 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/im/action/cmd

--- src/os/im/action/cmd/abstractfilteractioncmd.js
+++ src/os/im/action/cmd/abstractfilteractioncmd.js
@@ -1,131 +1,133 @@
-goog.provide('os.im.action.cmd.AbstractFilterAction');
+goog.module('os.im.action.cmd.AbstractFilterAction');
+goog.module.declareLegacyNamespace();
 
-goog.require('os.command.ICommand');
-goog.require('os.command.State');
+const State = goog.require('os.command.State');
+const ICommand = goog.requireType('os.command.ICommand');
+
+
 goog.require('os.ui.query.cmd.QueryEntries');
-
-
 
 /**
  * Abstract command for adding/removing filter actions.
  *
  * @abstract
- * @param {!os.im.action.FilterActionEntry} entry The filter action.
- * @param {number=} opt_index The index in the entry list.
- * @param {string=} opt_parentId The parent node ID.
- * @implements {os.command.ICommand}
- * @constructor
+ * @implements {ICommand}
  */
-os.im.action.cmd.AbstractFilterAction = function(entry, opt_index, opt_parentId) {
+class AbstractFilterAction {
   /**
-   * @type {!os.im.action.FilterActionEntry}
+   * Constructor.
+   * @param {!os.im.action.FilterActionEntry} entry The filter action.
+   * @param {number=} opt_index The index in the entry list.
+   * @param {string=} opt_parentId The parent node ID.
+   */
+  constructor(entry, opt_index, opt_parentId) {
+    /**
+     * @type {!os.im.action.FilterActionEntry}
+     * @protected
+     */
+    this.entry = entry;
+
+    /**
+     * The index for add operations.
+     * @type {number|undefined}
+     */
+    this.index = opt_index;
+
+    /**
+     * The parent node ID to add to (as opposed to the manager itself).
+     * @type {string|undefined}
+     * @protected
+     */
+    this.parentId = opt_parentId;
+
+    /**
+     * @type {boolean}
+     */
+    this.isAsync = false;
+
+    /**
+     * @type {string}
+     */
+    this.title = 'Add/Remove ' + os.im.action.ImportActionManager.getInstance().entryTitle;
+
+    /**
+     * @type {?string}
+     */
+    this.details = null;
+
+    /**
+     * @type {State}
+     */
+    this.state = State.READY;
+  }
+
+  /**
+   * @abstract
+   * @inheritDoc
+   */
+  execute() {}
+
+  /**
+   * @abstract
+   * @inheritDoc
+   */
+  revert() {}
+
+  /**
+   * Tests whether or not the command is ready and able to execute.
+   *
+   * @return {boolean} True if ready, false otherwise.
    * @protected
    */
-  this.entry = entry;
+  canExecute() {
+    var entryTitle = os.im.action.ImportActionManager.getInstance().entryTitle || 'Entry';
+
+    if (this.state !== State.READY) {
+      this.details = 'Command not in ready state';
+      return false;
+    }
+
+    if (!this.entry) {
+      this.details = entryTitle + ' not provided';
+      return false;
+    }
+
+    if (!this.entry.type) {
+      this.details = entryTitle + ' type not set';
+      return false;
+    }
+
+    if (!this.entry.getFilter()) {
+      this.details = entryTitle + ' does not contain a filter';
+      return false;
+    }
+
+    return true;
+  }
 
   /**
-   * The index for add operations.
-   * @type {number|undefined}
-   */
-  this.index = opt_index;
-
-  /**
-   * The parent node ID to add to (as opposed to the manager itself).
-   * @type {string|undefined}
+   * Adds the filter action.
+   *
    * @protected
    */
-  this.parentId = opt_parentId;
+  add() {
+    if (this.entry) {
+      var iam = os.im.action.ImportActionManager.getInstance();
+      iam.addActionEntry(this.entry, this.index, this.parentId);
+      iam.processItems(this.entry.getType());
+    }
+  }
 
   /**
-   * @type {boolean}
+   * Removes the filter action.
+   *
+   * @protected
    */
-  this.isAsync = false;
-
-  /**
-   * @type {string}
-   */
-  this.title = 'Add/Remove ' + os.im.action.ImportActionManager.getInstance().entryTitle;
-
-  /**
-   * @type {?string}
-   */
-  this.details = null;
-
-  /**
-   * @type {os.command.State}
-   */
-  this.state = os.command.State.READY;
-};
-
-
-/**
- * @abstract
- * @inheritDoc
- */
-os.im.action.cmd.AbstractFilterAction.prototype.execute = function() {};
-
-
-/**
- * @abstract
- * @inheritDoc
- */
-os.im.action.cmd.AbstractFilterAction.prototype.revert = function() {};
-
-
-/**
- * Tests whether or not the command is ready and able to execute.
- *
- * @return {boolean} True if ready, false otherwise.
- * @protected
- */
-os.im.action.cmd.AbstractFilterAction.prototype.canExecute = function() {
-  var entryTitle = os.im.action.ImportActionManager.getInstance().entryTitle || 'Entry';
-
-  if (this.state !== os.command.State.READY) {
-    this.details = 'Command not in ready state';
-    return false;
+  remove() {
+    if (this.entry) {
+      os.im.action.ImportActionManager.getInstance().removeActionEntry(this.entry, this.parentId);
+    }
   }
+}
 
-  if (!this.entry) {
-    this.details = entryTitle + ' not provided';
-    return false;
-  }
-
-  if (!this.entry.type) {
-    this.details = entryTitle + ' type not set';
-    return false;
-  }
-
-  if (!this.entry.getFilter()) {
-    this.details = entryTitle + ' does not contain a filter';
-    return false;
-  }
-
-  return true;
-};
-
-
-/**
- * Adds the filter action.
- *
- * @protected
- */
-os.im.action.cmd.AbstractFilterAction.prototype.add = function() {
-  if (this.entry) {
-    var iam = os.im.action.ImportActionManager.getInstance();
-    iam.addActionEntry(this.entry, this.index, this.parentId);
-    iam.processItems(this.entry.getType());
-  }
-};
-
-
-/**
- * Removes the filter action.
- *
- * @protected
- */
-os.im.action.cmd.AbstractFilterAction.prototype.remove = function() {
-  if (this.entry) {
-    os.im.action.ImportActionManager.getInstance().removeActionEntry(this.entry, this.parentId);
-  }
-};
+exports = AbstractFilterAction;
--- src/os/im/action/cmd/filteractionaddcmd.js
+++ src/os/im/action/cmd/filteractionaddcmd.js
@@ -1,53 +1,54 @@
-goog.provide('os.im.action.cmd.FilterActionAdd');
+goog.module('os.im.action.cmd.FilterActionAdd');
+goog.module.declareLegacyNamespace();
 
-goog.require('os.command.State');
-goog.require('os.im.action.cmd.AbstractFilterAction');
-
+const State = goog.require('os.command.State');
+const AbstractFilterAction = goog.require('os.im.action.cmd.AbstractFilterAction');
 
 
 /**
  * Command for adding filter actions.
- *
- * @param {!os.im.action.FilterActionEntry} entry The filter action.
- * @param {number=} opt_index The index in the entry list.
- * @param {string=} opt_parentId The parent node ID.
- * @extends {os.im.action.cmd.AbstractFilterAction}
- * @constructor
  */
-os.im.action.cmd.FilterActionAdd = function(entry, opt_index, opt_parentId) {
-  os.im.action.cmd.FilterActionAdd.base(this, 'constructor', entry, opt_index, opt_parentId);
+class FilterActionAdd extends AbstractFilterAction {
+  /**
+   * Constructor.
+   * @param {!os.im.action.FilterActionEntry} entry The filter action.
+   * @param {number=} opt_index The index in the entry list.
+   * @param {string=} opt_parentId The parent node ID.
+   */
+  constructor(entry, opt_index, opt_parentId) {
+    super(entry, opt_index, opt_parentId);
 
-  if (entry) {
-    var appEntryTitle = os.im.action.ImportActionManager.getInstance().entryTitle;
-    this.title = 'Add ' + appEntryTitle + ' "' + entry.getTitle() + '"';
+    if (entry) {
+      var appEntryTitle = os.im.action.ImportActionManager.getInstance().entryTitle;
+      this.title = 'Add ' + appEntryTitle + ' "' + entry.getTitle() + '"';
+    }
   }
-};
-goog.inherits(os.im.action.cmd.FilterActionAdd, os.im.action.cmd.AbstractFilterAction);
 
+  /**
+   * @inheritDoc
+   */
+  execute() {
+    if (this.canExecute()) {
+      this.state = State.EXECUTING;
 
-/**
- * @inheritDoc
- */
-os.im.action.cmd.FilterActionAdd.prototype.execute = function() {
-  if (this.canExecute()) {
-    this.state = os.command.State.EXECUTING;
+      this.add();
+      this.state = State.SUCCESS;
+      return true;
+    }
 
-    this.add();
-    this.state = os.command.State.SUCCESS;
+    return false;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  revert() {
+    this.state = State.REVERTING;
+
+    this.remove();
+    this.state = State.READY;
     return true;
   }
+}
 
-  return false;
-};
-
-
-/**
- * @inheritDoc
- */
-os.im.action.cmd.FilterActionAdd.prototype.revert = function() {
-  this.state = os.command.State.REVERTING;
-
-  this.remove();
-  this.state = os.command.State.READY;
-  return true;
-};
+exports = FilterActionAdd;
