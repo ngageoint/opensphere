777ed4592bc033b8e1eb1b901d27d6a549b17ef1
Connor Gibson <connor.gibson@caci.com>
2020-12-08 16:26:36 -0700

    refactor(es6): run jscodeshift on opensphere/src/plugin/heatmap/cmd

--- src/plugin/heatmap/cmd/intensitycmd.js
+++ src/plugin/heatmap/cmd/intensitycmd.js
@@ -1,54 +1,55 @@
-goog.provide('plugin.heatmap.cmd.Intensity');
-goog.require('os.command.AbstractStyle');
+goog.module('plugin.heatmap.cmd.Intensity');
+goog.module.declareLegacyNamespace();
 
+const AbstractStyle = goog.require('os.command.AbstractStyle');
 
 
 /**
  * Changes the intensity of a heatmap.
- *
- * @extends {os.command.AbstractStyle}
- * @param {string} layerId
- * @param {number} value
- * @param {number=} opt_oldValue
- * @constructor
  */
-plugin.heatmap.cmd.Intensity = function(layerId, value, opt_oldValue) {
-  plugin.heatmap.cmd.Intensity.base(this, 'constructor', layerId, value, opt_oldValue);
-  this.title = 'Change heatmap intensity';
-};
-goog.inherits(plugin.heatmap.cmd.Intensity, os.command.AbstractStyle);
-
-
-/**
- * @inheritDoc
- */
-plugin.heatmap.cmd.Intensity.prototype.getOldValue = function() {
-  var layer = /** @type {plugin.heatmap.Heatmap} */ (this.getLayer());
-
-  if (layer) {
-    return layer.getIntensity();
+class Intensity extends AbstractStyle {
+  /**
+   * Constructor.
+   * @param {string} layerId
+   * @param {number} value
+   * @param {number=} opt_oldValue
+   */
+  constructor(layerId, value, opt_oldValue) {
+    super(layerId, value, opt_oldValue);
+    this.title = 'Change heatmap intensity';
   }
 
-  return null;
-};
+  /**
+   * @inheritDoc
+   */
+  getOldValue() {
+    var layer = /** @type {plugin.heatmap.Heatmap} */ (this.getLayer());
 
+    if (layer) {
+      return layer.getIntensity();
+    }
 
-/**
- * @inheritDoc
- */
-plugin.heatmap.cmd.Intensity.prototype.applyValue = function(config, value) {
-  var layer = /** @type {plugin.heatmap.Heatmap} */ (this.getLayer());
-  if (layer) {
-    layer.setIntensity(value);
+    return null;
   }
-};
 
+  /**
+   * @inheritDoc
+   */
+  applyValue(config, value) {
+    var layer = /** @type {plugin.heatmap.Heatmap} */ (this.getLayer());
+    if (layer) {
+      layer.setIntensity(value);
+    }
+  }
 
-/**
- * I'm just here so I don't throw an error.
- *
- * @inheritDoc
- */
-plugin.heatmap.cmd.Intensity.prototype.getLayerConfig = function() {
-  return {};
-};
+  /**
+   * I'm just here so I don't throw an error.
+   *
+   * @inheritDoc
+   */
+  getLayerConfig() {
+    return {};
+  }
+}
+
+exports = Intensity;
