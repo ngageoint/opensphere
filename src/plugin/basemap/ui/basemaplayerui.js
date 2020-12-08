ae6c92c0578e78d8d77b454f559acbcbdd0731d3
Connor Gibson <connor.gibson@caci.com>
2020-12-08 16:45:17 -0700

    refactor(es6): run jscodeshift on opensphere/src/plugin/basemap/ui

--- src/plugin/basemap/ui/basemaplayerui.js
+++ src/plugin/basemap/ui/basemaplayerui.js
@@ -1,12 +1,13 @@
-goog.provide('plugin.basemap.ui.BaseMapLayerUICtrl');
-goog.provide('plugin.basemap.ui.baseMapLayerUIDirective');
-goog.require('goog.async.Delay');
-goog.require('os.MapContainer');
+goog.module('plugin.basemap.ui.BaseMapLayerUIUI');
+goog.module.declareLegacyNamespace();
+
 goog.require('os.defines');
-goog.require('os.map');
-goog.require('os.ui.Module');
-goog.require('os.ui.layer.TileLayerUICtrl');
 goog.require('os.ui.spinnerDirective');
+const MapContainer = goog.require('os.MapContainer');
+const Delay = goog.require('goog.async.Delay');
+const osMap = goog.require('os.map');
+const Module = goog.require('os.ui.Module');
+const TileLayerUICtrl = goog.require('os.ui.layer.TileLayerUICtrl');
 
 
 /**
@@ -14,212 +15,209 @@ goog.require('os.ui.spinnerDirective');
  *
  * @return {!angular.Directive}
  */
-plugin.basemap.ui.baseMapLayerUIDirective = function() {
-  return {
-    restrict: 'AE',
-    replace: true,
-    templateUrl: os.ROOT + 'views/plugin/basemap/basemaplayerui.html',
-    controller: plugin.basemap.ui.BaseMapLayerUICtrl,
-    controllerAs: 'basemap'
-  };
-};
+const directive = () => ({
+  restrict: 'AE',
+  replace: true,
+  templateUrl: os.ROOT + 'views/plugin/basemap/basemaplayerui.html',
+  controller: Controller,
+  controllerAs: 'basemap'
+});
 
 
 /**
  * Add the directive to the module
  */
-os.ui.Module.directive('basemaplayerui', [plugin.basemap.ui.baseMapLayerUIDirective]);
+Module.directive('basemaplayerui', [directive]);
 
 
 
 /**
  * Controller for the base map layer UI
- *
- * @param {!angular.Scope} $scope
- * @param {!angular.JQLite} $element
- * @param {!angular.$timeout} $timeout
- * @constructor
- * @extends {os.ui.layer.TileLayerUICtrl}
- * @ngInject
+ * @unrestricted
  */
-plugin.basemap.ui.BaseMapLayerUICtrl = function($scope, $element, $timeout) {
-  plugin.basemap.ui.BaseMapLayerUICtrl.base(this, 'constructor', $scope, $element, $timeout);
+class Controller extends TileLayerUICtrl {
+  /**
+   * Constructor.
+   * @param {!angular.Scope} $scope
+   * @param {!angular.JQLite} $element
+   * @param {!angular.$timeout} $timeout
+   * @ngInject
+   */
+  constructor($scope, $element, $timeout) {
+    super($scope, $element, $timeout);
 
-  $scope.$on('minZoom.spin', this.onMinZoomChange.bind(this));
-  $scope.$on('minZoom.spinchange', this.onMinZoomChange.bind(this));
-  $scope.$on('maxZoom.spin', this.onMaxZoomChange.bind(this));
-  $scope.$on('maxZoom.spinchange', this.onMaxZoomChange.bind(this));
+    $scope.$on('minZoom.spin', this.onMinZoomChange.bind(this));
+    $scope.$on('minZoom.spinchange', this.onMinZoomChange.bind(this));
+    $scope.$on('maxZoom.spin', this.onMaxZoomChange.bind(this));
+    $scope.$on('maxZoom.spinchange', this.onMaxZoomChange.bind(this));
 
-  this['mapMinZoom'] = os.map.MIN_ZOOM;
-  this['mapMaxZoom'] = os.map.MAX_ZOOM;
+    this['mapMinZoom'] = osMap.MIN_ZOOM;
+    this['mapMaxZoom'] = osMap.MAX_ZOOM;
 
-  this.refreshDelay = new goog.async.Delay(this.onRefresh, 1000, this);
-};
-goog.inherits(plugin.basemap.ui.BaseMapLayerUICtrl, os.ui.layer.TileLayerUICtrl);
-
-
-/**
- * @inheritDoc
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.initUI = function() {
-  plugin.basemap.ui.BaseMapLayerUICtrl.base(this, 'initUI');
-
-  if (this.scope) {
-    this.scope['minZoom'] = this.getMinZoom_();
-    this.scope['maxZoom'] = this.getMaxZoom_();
+    this.refreshDelay = new Delay(this.onRefresh, 1000, this);
   }
-};
 
+  /**
+   * @inheritDoc
+   */
+  initUI() {
+    super.initUI();
 
-/**
- * Handles changes to min zoom
- *
- * @param {?angular.Scope.Event} event
- * @param {number} value
- * @protected
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.onMinZoomChange = function(event, value) {
-  var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
+    if (this.scope) {
+      this.scope['minZoom'] = this.getMinZoom_();
+      this.scope['maxZoom'] = this.getMaxZoom_();
+    }
+  }
 
-  if (items) {
-    var resolution = os.MapContainer.getInstance().zoomToResolution(value);
+  /**
+   * Handles changes to min zoom
+   *
+   * @param {?angular.Scope.Event} event
+   * @param {number} value
+   * @protected
+   */
+  onMinZoomChange(event, value) {
+    var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
 
-    for (var i = 0, n = items.length; i < n; i++) {
-      try {
-        /** @type {ol.layer.Layer} */ (items[i].getLayer()).setMaxResolution(resolution);
-        this.refreshDelay.start();
-      } catch (e) {
+    if (items) {
+      var resolution = MapContainer.getInstance().zoomToResolution(value);
+
+      for (var i = 0, n = items.length; i < n; i++) {
+        try {
+          /** @type {ol.layer.Layer} */ (items[i].getLayer()).setMaxResolution(resolution);
+          this.refreshDelay.start();
+        } catch (e) {
+        }
       }
     }
   }
-};
 
+  /**
+   * Handles changes to max zoom
+   *
+   * @param {?angular.Scope.Event} event
+   * @param {number} value
+   * @protected
+   */
+  onMaxZoomChange(event, value) {
+    var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
 
-/**
- * Handles changes to max zoom
- *
- * @param {?angular.Scope.Event} event
- * @param {number} value
- * @protected
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.onMaxZoomChange = function(event, value) {
-  var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
+    if (items) {
+      var resolution = MapContainer.getInstance().zoomToResolution(value);
 
-  if (items) {
-    var resolution = os.MapContainer.getInstance().zoomToResolution(value);
-
-    for (var i = 0, n = items.length; i < n; i++) {
-      try {
-        /** @type {ol.layer.Layer} */ (items[i].getLayer()).setMinResolution(resolution);
-        this.refreshDelay.start();
-      } catch (e) {
+      for (var i = 0, n = items.length; i < n; i++) {
+        try {
+          /** @type {ol.layer.Layer} */ (items[i].getLayer()).setMinResolution(resolution);
+          this.refreshDelay.start();
+        } catch (e) {
+        }
       }
     }
   }
-};
 
+  /**
+   * @protected
+   */
+  onRefresh() {
+    // Changing the min/max zoom in Cesium affects the tile cache, so refresh
+    if (MapContainer.getInstance().is3DEnabled()) {
+      var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
 
-/**
- * @protected
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.onRefresh = function() {
-  // Changing the min/max zoom in Cesium affects the tile cache, so refresh
-  if (os.MapContainer.getInstance().is3DEnabled()) {
+      if (items) {
+        for (var i = 0, n = items.length; i < n; i++) {
+          var layer = items[i].getLayer();
+
+          if (layer && /** @type {ol.layer.Layer} */ (layer).getSource()) {
+            /** @type {ol.layer.Layer} */ (layer).getSource().refresh();
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Gets the min zoom
+   *
+   * @return {number} zoom
+   * @private
+   */
+  getMinZoom_() {
     var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
 
     if (items) {
       for (var i = 0, n = items.length; i < n; i++) {
-        var layer = items[i].getLayer();
+        try {
+          var layer = /** @type {ol.layer.Layer} */ (items[i].getLayer());
 
-        if (layer && /** @type {ol.layer.Layer} */ (layer).getSource()) {
-          /** @type {ol.layer.Layer} */ (layer).getSource().refresh();
+          if (layer) {
+            return Math.round(MapContainer.getInstance().resolutionToZoom(layer.getMaxResolution()));
+          }
+        } catch (e) {
         }
       }
     }
+
+    return osMap.MIN_ZOOM;
   }
-};
 
+  /**
+   * Gets the max zoom
+   *
+   * @return {number}
+   * @private
+   */
+  getMaxZoom_() {
+    var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
 
-/**
- * Gets the min zoom
- *
- * @return {number} zoom
- * @private
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.getMinZoom_ = function() {
-  var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
+    if (items) {
+      for (var i = 0, n = items.length; i < n; i++) {
+        try {
+          var layer = /** @type {ol.layer.Layer} */ (items[i].getLayer());
 
-  if (items) {
-    for (var i = 0, n = items.length; i < n; i++) {
-      try {
-        var layer = /** @type {ol.layer.Layer} */ (items[i].getLayer());
-
-        if (layer) {
-          return Math.round(os.MapContainer.getInstance().resolutionToZoom(layer.getMaxResolution()));
+          if (layer) {
+            return Math.round(MapContainer.getInstance().resolutionToZoom(layer.getMinResolution()));
+          }
+        } catch (e) {
         }
-      } catch (e) {
       }
     }
+
+    return osMap.MAX_ZOOM;
   }
 
-  return os.map.MIN_ZOOM;
-};
+  /**
+   * @param {string} key The scope value to set
+   * @export
+   */
+  setCurrent(key) {
+    var map = MapContainer.getInstance();
+    var resolution;
 
-
-/**
- * Gets the max zoom
- *
- * @return {number}
- * @private
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.getMaxZoom_ = function() {
-  var items = /** @type {Array.<!os.data.LayerNode>} */ (this.scope['items']);
-
-  if (items) {
-    for (var i = 0, n = items.length; i < n; i++) {
-      try {
-        var layer = /** @type {ol.layer.Layer} */ (items[i].getLayer());
-
-        if (layer) {
-          return Math.round(os.MapContainer.getInstance().resolutionToZoom(layer.getMinResolution()));
-        }
-      } catch (e) {
+    if (map.is3DEnabled()) {
+      // in 3D mode, the resolution should be determined based on the camera height adjusted to the equator. this will
+      // prevent the 2D projection from interfering with the relative zoom
+      var camera = map.getWebGLCamera();
+      if (camera) {
+        resolution = camera.calcResolutionForDistance(camera.getAltitude(), 0);
       }
-    }
-  }
-
-  return os.map.MAX_ZOOM;
-};
-
-
-/**
- * @param {string} key The scope value to set
- * @export
- */
-plugin.basemap.ui.BaseMapLayerUICtrl.prototype.setCurrent = function(key) {
-  var map = os.MapContainer.getInstance();
-  var resolution;
-
-  if (map.is3DEnabled()) {
-    // in 3D mode, the resolution should be determined based on the camera height adjusted to the equator. this will
-    // prevent the 2D projection from interfering with the relative zoom
-    var camera = map.getWebGLCamera();
-    if (camera) {
-      resolution = camera.calcResolutionForDistance(camera.getAltitude(), 0);
-    }
-  } else {
-    resolution = map.getMap().getView().getResolution();
-  }
-
-  if (resolution != null) {
-    // zoom level should be an integer inclusive of the current level
-    this.scope[key] = Math.floor(map.resolutionToZoom(resolution));
-
-    if (key == 'minZoom') {
-      this.onMinZoomChange(null, this.scope[key]);
     } else {
-      this.onMaxZoomChange(null, this.scope[key]);
+      resolution = map.getMap().getView().getResolution();
+    }
+
+    if (resolution != null) {
+      // zoom level should be an integer inclusive of the current level
+      this.scope[key] = Math.floor(map.resolutionToZoom(resolution));
+
+      if (key == 'minZoom') {
+        this.onMinZoomChange(null, this.scope[key]);
+      } else {
+        this.onMaxZoomChange(null, this.scope[key]);
+      }
     }
   }
-};
+}
 
+exports = {
+  Controller,
+  directive
+};
---
+++ src/plugin/basemap/ui/basemaplayerui_shim.js
@@ -0,0 +1,13 @@
+goog.provide('plugin.basemap.ui.BaseMapLayerUICtrl');
+goog.provide('plugin.basemap.ui.baseMapLayerUIDirective');
+goog.require('plugin.basemap.ui.BaseMapLayerUIUI');
+
+/**
+ * @deprecated Please use goog.require('plugin.basemap.ui.BaseMapLayerUIUI').Controller instead.
+ */
+plugin.basemap.ui.BaseMapLayerUICtrl = plugin.basemap.ui.BaseMapLayerUIUI.Controller;
+
+/**
+ * @deprecated Please use goog.require('plugin.basemap.ui.BaseMapLayerUIUI').directive instead.
+ */
+plugin.basemap.ui.baseMapLayerUIDirective = plugin.basemap.ui.BaseMapLayerUIUI.directive;
