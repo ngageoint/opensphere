f05311fb1a63d6da462bf8783ba8d818cc5f9024
Connor Gibson <connor.gibson@caci.com>
2020-12-08 16:50:47 -0700

    refactor(es6): run jscodeshift on opensphere/src/plugin/arc/node

--- src/plugin/arc/node/arcfoldernode.js
+++ src/plugin/arc/node/arcfoldernode.js
@@ -1,76 +1,77 @@
-goog.provide('plugin.arc.node.ArcFolderNode');
-goog.require('os.ui.slick.LoadingNode');
-goog.require('os.ui.slick.SlickTreeNode');
+goog.module('plugin.arc.node.ArcFolderNode');
+goog.module.declareLegacyNamespace();
 
+const LoadingNode = goog.require('os.ui.slick.LoadingNode');
+const SlickTreeNode = goog.require('os.ui.slick.SlickTreeNode');
 
 
 /**
  * Loads the capabilities from an Arc server and constructs the tree.
- *
- * @param {plugin.arc.ArcServer} server
- * @extends {os.ui.slick.LoadingNode}
- * @constructor
  */
-plugin.arc.node.ArcFolderNode = function(server) {
-  plugin.arc.node.ArcFolderNode.base(this, 'constructor');
+class ArcFolderNode extends LoadingNode {
+  /**
+   * Constructor.
+   * @param {plugin.arc.ArcServer} server
+   */
+  constructor(server) {
+    super();
+
+    /**
+     * @type {plugin.arc.ArcServer}
+     * @private
+     */
+    this.server_ = server;
+
+    /**
+     * @type {?plugin.arc.ArcLoader}
+     * @private
+     */
+    this.loader_ = null;
+  }
 
   /**
-   * @type {plugin.arc.ArcServer}
-   * @private
+   * Loads Arc node capabilities.
+   *
+   * @param {string} url
    */
-  this.server_ = server;
+  load(url) {
+    this.setLoading(true);
+    this.loader_ = plugin.arc.getArcLoader(new SlickTreeNode(), url, this.server_);
+    this.loader_.listen(goog.net.EventType.SUCCESS, this.onLoad, false, this);
+    this.loader_.listen(goog.net.EventType.ERROR, this.onError, false, this);
+    this.loader_.load();
+  }
 
   /**
-   * @type {?plugin.arc.ArcLoader}
-   * @private
+   * Handler for Arc folder load success.
+   *
+   * @param {goog.events.Event} event
+   * @protected
    */
-  this.loader_ = null;
-};
-goog.inherits(plugin.arc.node.ArcFolderNode, os.ui.slick.LoadingNode);
+  onLoad(event) {
+    this.setLoading(false);
+    this.loader_.unlisten(goog.net.EventType.SUCCESS, this.onLoad, false, this);
+    this.loader_.unlisten(goog.net.EventType.ERROR, this.onError, false, this);
+    this.setChildren(this.loader_.getNode().getChildren());
+    goog.dispose(this.loader_);
+    this.loader_ = null;
+    this.dispatchEvent(goog.net.EventType.SUCCESS);
+  }
 
+  /**
+   * Handler for Arc folder load errors.
+   *
+   * @param {goog.events.Event} event
+   * @protected
+   */
+  onError(event) {
+    this.setLoading(false);
+    this.loader_.unlisten(goog.net.EventType.SUCCESS, this.onLoad, false, this);
+    this.loader_.unlisten(goog.net.EventType.ERROR, this.onError, false, this);
+    goog.dispose(this.loader_);
+    this.loader_ = null;
+    this.dispatchEvent(goog.net.EventType.ERROR);
+  }
+}
 
-/**
- * Loads Arc node capabilities.
- *
- * @param {string} url
- */
-plugin.arc.node.ArcFolderNode.prototype.load = function(url) {
-  this.setLoading(true);
-  this.loader_ = plugin.arc.getArcLoader(new os.ui.slick.SlickTreeNode(), url, this.server_);
-  this.loader_.listen(goog.net.EventType.SUCCESS, this.onLoad, false, this);
-  this.loader_.listen(goog.net.EventType.ERROR, this.onError, false, this);
-  this.loader_.load();
-};
-
-
-/**
- * Handler for Arc folder load success.
- *
- * @param {goog.events.Event} event
- * @protected
- */
-plugin.arc.node.ArcFolderNode.prototype.onLoad = function(event) {
-  this.setLoading(false);
-  this.loader_.unlisten(goog.net.EventType.SUCCESS, this.onLoad, false, this);
-  this.loader_.unlisten(goog.net.EventType.ERROR, this.onError, false, this);
-  this.setChildren(this.loader_.getNode().getChildren());
-  goog.dispose(this.loader_);
-  this.loader_ = null;
-  this.dispatchEvent(goog.net.EventType.SUCCESS);
-};
-
-
-/**
- * Handler for Arc folder load errors.
- *
- * @param {goog.events.Event} event
- * @protected
- */
-plugin.arc.node.ArcFolderNode.prototype.onError = function(event) {
-  this.setLoading(false);
-  this.loader_.unlisten(goog.net.EventType.SUCCESS, this.onLoad, false, this);
-  this.loader_.unlisten(goog.net.EventType.ERROR, this.onError, false, this);
-  goog.dispose(this.loader_);
-  this.loader_ = null;
-  this.dispatchEvent(goog.net.EventType.ERROR);
-};
+exports = ArcFolderNode;
--- src/plugin/arc/node/arcservicenode.js
+++ src/plugin/arc/node/arcservicenode.js
@@ -1,43 +1,175 @@
-goog.provide('plugin.arc.node.ArcServiceNode');
-goog.require('goog.log');
+goog.module('plugin.arc.node.ArcServiceNode');
+goog.module.declareLegacyNamespace();
+
+const ui = goog.require('os.ui');
+const log = goog.require('goog.log');
+const Request = goog.require('os.net.Request');
+const DescriptorNode = goog.require('os.ui.data.DescriptorNode');
+const LoadingNode = goog.require('os.ui.slick.LoadingNode');
+const ArcLayerDescriptor = goog.require('plugin.arc.layer.ArcLayerDescriptor');
+
+
 goog.require('goog.log.Logger');
-goog.require('os.net.Request');
-goog.require('os.ui.data.DescriptorNode');
-goog.require('os.ui.slick.LoadingNode');
-goog.require('plugin.arc.layer.ArcLayerDescriptor');
-
-
 
 /**
  * Loads the capabilities from an Arc server and constructs the tree.
- *
- * @param {plugin.arc.ArcServer} server
- * @extends {os.ui.slick.LoadingNode}
- * @constructor
  */
-plugin.arc.node.ArcServiceNode = function(server) {
-  plugin.arc.node.ArcServiceNode.base(this, 'constructor');
-  this.log = plugin.arc.node.ArcServiceNode.LOGGER_;
+class ArcServiceNode extends LoadingNode {
+  /**
+   * Constructor.
+   * @param {plugin.arc.ArcServer} server
+   */
+  constructor(server) {
+    super();
+    this.log = ArcServiceNode.LOGGER_;
+
+    /**
+     * @type {?string}
+     * @private
+     */
+    this.url_ = null;
+
+    /**
+     * @type {plugin.arc.ArcServer}
+     * @private
+     */
+    this.server_ = server;
+
+    /**
+     * @type {?Request}
+     * @private
+     */
+    this.request_ = null;
+  }
 
   /**
-   * @type {?string}
-   * @private
+   * Sets the URL to load. This keeps the URL only to the folder/service level and strips off anything after that
+   * as these URLs are often provided out further than we expect.
+   *
+   * @param {string} url
    */
-  this.url_ = null;
+  setUrl(url) {
+    var featureServerIdx = url.indexOf('/FeatureServer');
+    var mapServerIdx = url.indexOf('/MapServer');
+    if (featureServerIdx > -1) {
+      url = url.substring(0, featureServerIdx);
+    } else if (mapServerIdx > -1) {
+      url = url.substring(0, mapServerIdx);
+    }
+
+    this.url_ = url;
+  }
 
   /**
-   * @type {plugin.arc.ArcServer}
-   * @private
+   * Loads Arc node capabilities.
+   *
+   * @param {string} url
    */
-  this.server_ = server;
+  load(url) {
+    if (this.request_) {
+      goog.dispose(this.request_);
+    }
+
+    this.setLoading(true);
+    this.setUrl(url);
+
+    // request the folder plus the MapServer info in order to get the full layer metadata
+    url = this.url_ + '/MapServer/layers?f=json';
+    this.request_ = new Request(url);
+    this.request_.setHeader('Accept', '*/*');
+    this.request_.listen(goog.net.EventType.SUCCESS, this.onLoad_, false, this);
+    this.request_.listen(goog.net.EventType.ERROR, this.onError_, false, this);
+    this.request_.load();
+  }
 
   /**
-   * @type {?os.net.Request}
+   * Success callback for loading the Arc service data.
+   *
+   * @param {goog.events.Event} event
    * @private
    */
-  this.request_ = null;
-};
-goog.inherits(plugin.arc.node.ArcServiceNode, os.ui.slick.LoadingNode);
+  onLoad_(event) {
+    this.setLoading(false);
+
+    var response = /** @type {string} */ (this.request_.getResponse());
+    goog.dispose(this.request_);
+    this.request_ = null;
+
+    var json = null;
+    try {
+      json = JSON.parse(response);
+    } catch (e) {
+      // wah wah
+    }
+
+    if (json && this.server_) {
+      var layers = /** @type {Array<Object>} */ (json['layers']);
+      if (layers && layers.length > 0) {
+        for (var i = 0, ii = layers.length; i < ii; i++) {
+          var layer = layers[i];
+          this.addLayer_(layer, this.server_);
+        }
+      }
+    }
+
+    this.dispatchEvent(goog.net.EventType.SUCCESS);
+  }
+
+  /**
+   * Creates and adds layer descriptors for each Arc service.
+   *
+   * @param {Object} layer
+   * @param {plugin.arc.ArcServer} server
+   * @private
+   */
+  addLayer_(layer, server) {
+    var dm = os.dataManager;
+
+    var uniquePath = '';
+    if (this.url_) {
+      uniquePath = this.url_.replace(server.getUrl(), '');
+    }
+
+    var layerId = goog.string.hashCode(uniquePath + '|' + layer['id']);
+    var id = server.getId() + ui.data.BaseProvider.ID_DELIMITER + layerId;
+    var d = /** @type {ArcLayerDescriptor} */ (dm.getDescriptor(id));
+
+    if (!d) {
+      d = new ArcLayerDescriptor();
+    }
+
+    d.setProvider(server.getLabel());
+    d.configureDescriptor(layer, id, this.url_ + '/MapServer');
+    d.setDataProvider(server);
+
+    dm.addDescriptor(d);
+
+    var node = new DescriptorNode();
+    node.setDescriptor(d);
+
+    this.addChild(node);
+  }
+
+  /**
+   * Handler for Arc load errors.
+   *
+   * @param {goog.events.Event} event
+   * @private
+   */
+  onError_(event) {
+    this.setLoading(false);
+
+    var uri = this.request_.getUri();
+    goog.dispose(this.request_);
+    this.request_ = null;
+
+    var href = uri ? uri.toString() : 'no URL available!';
+    var msg = 'Arc loading failed for URL: ' + href;
+    log.error(this.log, msg);
+
+    this.dispatchEvent(goog.net.EventType.ERROR);
+  }
+}
 
 
 /**
@@ -46,138 +178,8 @@ goog.inherits(plugin.arc.node.ArcServiceNode, os.ui.slick.LoadingNode);
  * @type {goog.debug.Logger}
  * @private
  */
-plugin.arc.node.ArcServiceNode.LOGGER_ = goog.log.getLogger('plugin.arc.node.ArcServiceNode');
+ArcServiceNode.LOGGER_ = log.getLogger('plugin.arc.node.ArcServiceNode');
 
 
 
-/**
- * Sets the URL to load. This keeps the URL only to the folder/service level and strips off anything after that
- * as these URLs are often provided out further than we expect.
- *
- * @param {string} url
- */
-plugin.arc.node.ArcServiceNode.prototype.setUrl = function(url) {
-  var featureServerIdx = url.indexOf('/FeatureServer');
-  var mapServerIdx = url.indexOf('/MapServer');
-  if (featureServerIdx > -1) {
-    url = url.substring(0, featureServerIdx);
-  } else if (mapServerIdx > -1) {
-    url = url.substring(0, mapServerIdx);
-  }
-
-  this.url_ = url;
-};
-
-
-/**
- * Loads Arc node capabilities.
- *
- * @param {string} url
- */
-plugin.arc.node.ArcServiceNode.prototype.load = function(url) {
-  if (this.request_) {
-    goog.dispose(this.request_);
-  }
-
-  this.setLoading(true);
-  this.setUrl(url);
-
-  // request the folder plus the MapServer info in order to get the full layer metadata
-  url = this.url_ + '/MapServer/layers?f=json';
-  this.request_ = new os.net.Request(url);
-  this.request_.setHeader('Accept', '*/*');
-  this.request_.listen(goog.net.EventType.SUCCESS, this.onLoad_, false, this);
-  this.request_.listen(goog.net.EventType.ERROR, this.onError_, false, this);
-  this.request_.load();
-};
-
-
-/**
- * Success callback for loading the Arc service data.
- *
- * @param {goog.events.Event} event
- * @private
- */
-plugin.arc.node.ArcServiceNode.prototype.onLoad_ = function(event) {
-  this.setLoading(false);
-
-  var response = /** @type {string} */ (this.request_.getResponse());
-  goog.dispose(this.request_);
-  this.request_ = null;
-
-  var json = null;
-  try {
-    json = JSON.parse(response);
-  } catch (e) {
-    // wah wah
-  }
-
-  if (json && this.server_) {
-    var layers = /** @type {Array<Object>} */ (json['layers']);
-    if (layers && layers.length > 0) {
-      for (var i = 0, ii = layers.length; i < ii; i++) {
-        var layer = layers[i];
-        this.addLayer_(layer, this.server_);
-      }
-    }
-  }
-
-  this.dispatchEvent(goog.net.EventType.SUCCESS);
-};
-
-
-/**
- * Creates and adds layer descriptors for each Arc service.
- *
- * @param {Object} layer
- * @param {plugin.arc.ArcServer} server
- * @private
- */
-plugin.arc.node.ArcServiceNode.prototype.addLayer_ = function(layer, server) {
-  var dm = os.dataManager;
-
-  var uniquePath = '';
-  if (this.url_) {
-    uniquePath = this.url_.replace(server.getUrl(), '');
-  }
-
-  var layerId = goog.string.hashCode(uniquePath + '|' + layer['id']);
-  var id = server.getId() + os.ui.data.BaseProvider.ID_DELIMITER + layerId;
-  var d = /** @type {plugin.arc.layer.ArcLayerDescriptor} */ (dm.getDescriptor(id));
-
-  if (!d) {
-    d = new plugin.arc.layer.ArcLayerDescriptor();
-  }
-
-  d.setProvider(server.getLabel());
-  d.configureDescriptor(layer, id, this.url_ + '/MapServer');
-  d.setDataProvider(server);
-
-  dm.addDescriptor(d);
-
-  var node = new os.ui.data.DescriptorNode();
-  node.setDescriptor(d);
-
-  this.addChild(node);
-};
-
-
-/**
- * Handler for Arc load errors.
- *
- * @param {goog.events.Event} event
- * @private
- */
-plugin.arc.node.ArcServiceNode.prototype.onError_ = function(event) {
-  this.setLoading(false);
-
-  var uri = this.request_.getUri();
-  goog.dispose(this.request_);
-  this.request_ = null;
-
-  var href = uri ? uri.toString() : 'no URL available!';
-  var msg = 'Arc loading failed for URL: ' + href;
-  goog.log.error(this.log, msg);
-
-  this.dispatchEvent(goog.net.EventType.ERROR);
-};
+exports = ArcServiceNode;
