4a6928f2df9e1b6fe5f28229b0423baffa15b173
Connor Gibson <connor.gibson@caci.com>
2020-12-08 15:39:16 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/ui/ogc/query

--- src/os/ui/ogc/query/ogcqueryhandler.js
+++ src/os/ui/ogc/query/ogcqueryhandler.js
@@ -1,25 +1,29 @@
-goog.provide('os.ui.ogc.query.OGCQueryHandler');
-goog.require('os.net.ParamModifier');
-goog.require('os.ogc.filter.ModifierConstants');
-goog.require('os.ogc.filter.OGCExclusionFormatter');
-goog.require('os.ogc.filter.OGCFilterFormatter');
-goog.require('os.ogc.filter.OGCSpatialFormatter');
-goog.require('os.ui.query.QueryHandler');
+goog.module('os.ui.ogc.query.OGCQueryHandler');
+goog.module.declareLegacyNamespace();
 
+const ParamModifier = goog.require('os.net.ParamModifier');
+const ModifierConstants = goog.require('os.ogc.filter.ModifierConstants');
+const OGCExclusionFormatter = goog.require('os.ogc.filter.OGCExclusionFormatter');
+const OGCFilterFormatter = goog.require('os.ogc.filter.OGCFilterFormatter');
+const OGCSpatialFormatter = goog.require('os.ogc.filter.OGCSpatialFormatter');
+const QueryHandler = goog.require('os.ui.query.QueryHandler');
 
 
 /**
- * @param {string=} opt_geomColumn
- * @constructor
- * @extends {os.ui.query.QueryHandler}
  */
-os.ui.ogc.query.OGCQueryHandler = function(opt_geomColumn) {
-  os.ui.ogc.query.OGCQueryHandler.base(this, 'constructor');
-  this.setModifier(new os.net.ParamModifier('filter', 'filter', os.ogc.filter.ModifierConstants.FILTER, ''));
-  this.setAreaFormatter(new os.ogc.filter.OGCSpatialFormatter(opt_geomColumn));
-  this.setExclusionFormatter(new os.ogc.filter.OGCExclusionFormatter(opt_geomColumn));
-  this.setFilterFormatter(new os.ogc.filter.OGCFilterFormatter());
-  this.spatialRequired = true;
-};
-goog.inherits(os.ui.ogc.query.OGCQueryHandler, os.ui.query.QueryHandler);
+class OGCQueryHandler extends QueryHandler {
+  /**
+   * Constructor.
+   * @param {string=} opt_geomColumn
+   */
+  constructor(opt_geomColumn) {
+    super();
+    this.setModifier(new ParamModifier('filter', 'filter', ModifierConstants.FILTER, ''));
+    this.setAreaFormatter(new OGCSpatialFormatter(opt_geomColumn));
+    this.setExclusionFormatter(new OGCExclusionFormatter(opt_geomColumn));
+    this.setFilterFormatter(new OGCFilterFormatter());
+    this.spatialRequired = true;
+  }
+}
 
+exports = OGCQueryHandler;
