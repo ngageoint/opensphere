55e0fda482db9e2a7a7d6be32cc3a02c4d9b0b71
Connor Gibson <connor.gibson@caci.com>
2020-12-08 17:20:56 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/ui/header

--- src/os/ui/header/scrollheader.js
+++ src/os/ui/header/scrollheader.js
@@ -1,8 +1,9 @@
-goog.provide('os.ui.header.ScrollHeaderCtrl');
-goog.provide('os.ui.header.ScrollHeaderEvents');
-goog.provide('os.ui.header.scrollHeaderDirective');
-goog.require('goog.async.Throttle');
-goog.require('os.ui.Module');
+goog.module('os.ui.header.ScrollHeaderUI');
+goog.module.declareLegacyNamespace();
+
+const Throttle = goog.require('goog.async.Throttle');
+const Module = goog.require('os.ui.Module');
+const ScrollHeaderEvents = goog.require('os.ui.header.ScrollHeaderEvents');
 
 
 /**
@@ -10,189 +11,184 @@ goog.require('os.ui.Module');
  *
  * @return {angular.Directive}
  */
-os.ui.header.scrollHeaderDirective = function() {
-  return {
-    restrict: 'A',
-    scope: {
-      'parents': '@?',
-      'eventId': '@?'
-    },
-    controller: os.ui.header.ScrollHeaderCtrl
-  };
-};
+const directive = () => ({
+  restrict: 'A',
+
+  scope: {
+    'parents': '@?',
+    'eventId': '@?'
+  },
+
+  controller: Controller
+});
 
 
 /**
  * Add the directive to the module.
  */
-os.ui.Module.directive('scrollHeader', [os.ui.header.scrollHeaderDirective]);
-
-
-/**
- * @enum {string}
- */
-os.ui.header.ScrollHeaderEvents = {
-  RESET: 'scrollheader.resetHeight',
-  STICK: 'scrollheader.stick',
-  UNSTICK: 'scrollheader.unstick'
-};
-
+Module.directive('scrollHeader', [directive]);
 
 
 /**
  * Controller function for the scrollHeader directive
- *
- * @param {!angular.Scope} $scope
- * @param {!jQuery} $element
- * @param {!angular.$timeout} $timeout
- * @param {!Object.<string, string>} $attrs
- * @constructor
- * @ngInject
+ * @unrestricted
  */
-os.ui.header.ScrollHeaderCtrl = function($scope, $element, $timeout, $attrs) {
+class Controller {
   /**
-   * @type {?angular.Scope}
-   * @private
+   * Constructor.
+   * @param {!angular.Scope} $scope
+   * @param {!jQuery} $element
+   * @param {!angular.$timeout} $timeout
+   * @param {!Object.<string, string>} $attrs
+   * @ngInject
    */
-  this.scope_ = $scope;
+  constructor($scope, $element, $timeout, $attrs) {
+    /**
+     * @type {?angular.Scope}
+     * @private
+     */
+    this.scope_ = $scope;
 
-  /**
-   * @type {?jQuery}
-   * @private
-   */
-  this.element_ = $element;
+    /**
+     * @type {?jQuery}
+     * @private
+     */
+    this.element_ = $element;
 
-  /**
-   * @type {?angular.$timeout}
-   * @private
-   */
-  this.timeout_ = $timeout;
+    /**
+     * @type {?angular.$timeout}
+     * @private
+     */
+    this.timeout_ = $timeout;
 
-  /**
-   * @type {?jQuery}
-   * @private
-   */
-  this.scrollEl_ = $($attrs['selector'] || window);
+    /**
+     * @type {?jQuery}
+     * @private
+     */
+    this.scrollEl_ = $($attrs['selector'] || window);
 
-  /**
-   * The element height to offset from
-   * @type {number}
-   * @private
-   */
-  this.resetHeight_ = 0;
+    /**
+     * The element height to offset from
+     * @type {number}
+     * @private
+     */
+    this.resetHeight_ = 0;
 
-  /**
-   * @type {boolean}
-   * @private
-   */
-  this.isFixed_ = false;
-
-  /**
-   * @type {jQuery}
-   * @private
-   */
-  this.filler_ = null;
-
-  /**
-   * If we support position sticky, use it!!!
-   * @type {boolean}
-   * @private
-   */
-  this.supportsSticky_ = Modernizr.csspositionsticky || false;
-  if (this.supportsSticky_) {
-    this.timeout_(function() {
-      this.element_.addClass('sticky-top position-sticky');
-    }.bind(this));
-  }
-
-  /**
-   * @type {goog.async.Throttle}
-   * @private
-   */
-  this.processScroll_ = new goog.async.Throttle(this.updatePositions_, 50, this);
-  this.processScrollHandler_ = this.processScroll_.fire.bind(this.processScroll_);
-
-  this.updatePositions_();
-  $scope.$on(os.ui.header.ScrollHeaderEvents.RESET, this.processScrollHandler_.bind(this));
-  $element.bind('DOMNodeInserted', this.updateHeight_.bind(this));
-  $(window).scroll(this.processScrollHandler_.bind(this));
-  this.element_.on('$destroy', this.destroyElement_.bind(this));
-};
-
-
-/**
- * Update this element's based on window scroll position.
- *
- * @private
- */
-os.ui.header.ScrollHeaderCtrl.prototype.updatePositions_ = function() {
-  var headerHeight = Math.floor($('.js-navtop').outerHeight());
-  if (this.scope_['parents']) {
-    headerHeight = 0;
-    $(this.scope_['parents']).each(function() {
-      headerHeight += Math.floor($(this).outerHeight());
-    });
-  }
-
-  var navTop = this.element_.offset().top - $(window).scrollTop() - headerHeight;
-
-  if (navTop < 0) {
-    navTop = 0;
-  }
-
-  if (!this.isFixed_ && navTop <= 1) {
-    this.isFixed_ = true;
-    var eleHeight = this.element_.height();
-    this.resetHeight_ = /** @type {number} */ (this.scrollEl_.scrollTop()) - (eleHeight ? eleHeight : 0);
-    if (!this.supportsSticky_) {
-      this.element_.addClass('position-fixed');
-    }
-    this.element_.css('top', headerHeight + 'px');
-
-    this.scope_.$emit(os.ui.header.ScrollHeaderEvents.STICK, this.scope_['eventId']);
-  } else if (this.isFixed_ && this.scrollEl_.scrollTop() <= this.resetHeight_) {
+    /**
+     * @type {boolean}
+     * @private
+     */
     this.isFixed_ = false;
-    if (!this.supportsSticky_) {
-      if (this.filler_) {
-        this.filler_.remove();
-        this.filler_ = null;
-      }
-      this.element_.removeClass('position-fixed');
+
+    /**
+     * @type {jQuery}
+     * @private
+     */
+    this.filler_ = null;
+
+    /**
+     * If we support position sticky, use it!!!
+     * @type {boolean}
+     * @private
+     */
+    this.supportsSticky_ = Modernizr.csspositionsticky || false;
+    if (this.supportsSticky_) {
+      this.timeout_(function() {
+        this.element_.addClass('sticky-top position-sticky');
+      }.bind(this));
     }
-    this.element_.css('top', '');
-    this.scope_.$emit(os.ui.header.ScrollHeaderEvents.UNSTICK, this.scope_['eventId']);
-  } else if (this.isFixed_ && headerHeight != this.element_.position().top) {
-    // We are in a fixed state but the header changed sizes, update our offset
-    this.element_.css('top', headerHeight + 'px');
+
+    /**
+     * @type {Throttle}
+     * @private
+     */
+    this.processScroll_ = new Throttle(this.updatePositions_, 50, this);
+    this.processScrollHandler_ = this.processScroll_.fire.bind(this.processScroll_);
+
+    this.updatePositions_();
+    $scope.$on(ScrollHeaderEvents.RESET, this.processScrollHandler_.bind(this));
+    $element.bind('DOMNodeInserted', this.updateHeight_.bind(this));
+    $(window).scroll(this.processScrollHandler_.bind(this));
+    this.element_.on('$destroy', this.destroyElement_.bind(this));
   }
-};
 
+  /**
+   * Update this element's based on window scroll position.
+   *
+   * @private
+   */
+  updatePositions_() {
+    var headerHeight = Math.floor($('.js-navtop').outerHeight());
+    if (this.scope_['parents']) {
+      headerHeight = 0;
+      $(this.scope_['parents']).each(function() {
+        headerHeight += Math.floor($(this).outerHeight());
+      });
+    }
 
-/**
- * Update the height of the filler.
- *
- * @private
- */
-os.ui.header.ScrollHeaderCtrl.prototype.updateHeight_ = function() {
-  if (this.filler_) {
-    this.filler_.css('height', this.element_.outerHeight());
+    var navTop = this.element_.offset().top - $(window).scrollTop() - headerHeight;
+
+    if (navTop < 0) {
+      navTop = 0;
+    }
+
+    if (!this.isFixed_ && navTop <= 1) {
+      this.isFixed_ = true;
+      var eleHeight = this.element_.height();
+      this.resetHeight_ = /** @type {number} */ (this.scrollEl_.scrollTop()) - (eleHeight ? eleHeight : 0);
+      if (!this.supportsSticky_) {
+        this.element_.addClass('position-fixed');
+      }
+      this.element_.css('top', headerHeight + 'px');
+
+      this.scope_.$emit(ScrollHeaderEvents.STICK, this.scope_['eventId']);
+    } else if (this.isFixed_ && this.scrollEl_.scrollTop() <= this.resetHeight_) {
+      this.isFixed_ = false;
+      if (!this.supportsSticky_) {
+        if (this.filler_) {
+          this.filler_.remove();
+          this.filler_ = null;
+        }
+        this.element_.removeClass('position-fixed');
+      }
+      this.element_.css('top', '');
+      this.scope_.$emit(ScrollHeaderEvents.UNSTICK, this.scope_['eventId']);
+    } else if (this.isFixed_ && headerHeight != this.element_.position().top) {
+      // We are in a fixed state but the header changed sizes, update our offset
+      this.element_.css('top', headerHeight + 'px');
+    }
   }
-};
 
+  /**
+   * Update the height of the filler.
+   *
+   * @private
+   */
+  updateHeight_() {
+    if (this.filler_) {
+      this.filler_.css('height', this.element_.outerHeight());
+    }
+  }
 
-/**
- * Clean up element
- *
- * @private
- */
-os.ui.header.ScrollHeaderCtrl.prototype.destroyElement_ = function() {
-  this.element_.unbind('DOMNodeInserted');
-  this.scrollEl_.off('scroll', this.processScrollHandler_);
-  this.processScroll_.stop();
-  this.processScroll_.dispose();
-  this.processScroll_ = null;
-  this.scope_ = null;
-  this.element_ = null;
-  this.timeout_ = null;
-  $(window).off('scroll');
+  /**
+   * Clean up element
+   *
+   * @private
+   */
+  destroyElement_() {
+    this.element_.unbind('DOMNodeInserted');
+    this.scrollEl_.off('scroll', this.processScrollHandler_);
+    this.processScroll_.stop();
+    this.processScroll_.dispose();
+    this.processScroll_ = null;
+    this.scope_ = null;
+    this.element_ = null;
+    this.timeout_ = null;
+    $(window).off('scroll');
+  }
+}
+
+exports = {
+  Controller,
+  directive
 };
---
+++ src/os/ui/header/scrollheader_shim.js
@@ -0,0 +1,13 @@
+goog.provide('os.ui.header.ScrollHeaderCtrl');
+goog.provide('os.ui.header.scrollHeaderDirective');
+goog.require('os.ui.header.ScrollHeaderUI');
+
+/**
+ * @deprecated Please use goog.require('os.ui.header.ScrollHeaderUI').Controller instead.
+ */
+os.ui.header.ScrollHeaderCtrl = os.ui.header.ScrollHeaderUI.Controller;
+
+/**
+ * @deprecated Please use goog.require('os.ui.header.ScrollHeaderUI').directive instead.
+ */
+os.ui.header.scrollHeaderDirective = os.ui.header.ScrollHeaderUI.directive;
