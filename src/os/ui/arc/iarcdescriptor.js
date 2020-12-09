fddb226643192dc8c099e1f88ba391e2e60ca4b9
Connor Gibson <connor.gibson@caci.com>
2020-12-08 17:39:05 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/ui/arc

--- src/os/ui/arc/iarcdescriptor.js
+++ src/os/ui/arc/iarcdescriptor.js
@@ -1,48 +1,51 @@
-goog.provide('os.ui.arc.IARCDescriptor');
+goog.module('os.ui.arc.IARCDescriptor');
+goog.module.declareLegacyNamespace();
 
-goog.require('os.data.IDataDescriptor');
-goog.require('os.data.IServerDescriptor');
-goog.require('os.filter.IFilterable');
-goog.require('os.ui.ogc.IFeatureTypeDescriptor');
+const IDataDescriptor = goog.requireType('os.data.IDataDescriptor');
+const IServerDescriptor = goog.requireType('os.data.IServerDescriptor');
+const IFilterable = goog.requireType('os.filter.IFilterable');
+const IFeatureTypeDescriptor = goog.requireType('os.ui.ogc.IFeatureTypeDescriptor');
+
 
 /**
  * Interface for ARC data descriptors
  *
  * @interface
- * @extends {os.data.IDataDescriptor}
- * @extends {os.data.IServerDescriptor}
- * @extends {os.filter.IFilterable}
- * @extends {os.ui.ogc.IFeatureTypeDescriptor}
+ * @extends {IDataDescriptor}
+ * @extends {IServerDescriptor}
+ * @extends {IFilterable}
+ * @extends {IFeatureTypeDescriptor}
  */
-os.ui.arc.IARCDescriptor = function() {};
+class IARCDescriptor {
+  /**
+   * Set the url
+   * @param {?string} value
+   */
+  setUrl(value) {}
+
+  /**
+   * Get the url
+   * @return {?string}
+   */
+  getUrl() {}
+
+  /**
+   * Set deprecated
+   * @param {boolean} value
+   */
+  setDeprecated(value) {}
+
+  /**
+   * Get deprecated
+   * @return {boolean}
+   */
+  getDeprecated() {}
+}
 
 /**
  * @type {string}
  * @const
  */
-os.ui.arc.IARCDescriptor.ID = 'os.ui.arc.IARCDescriptor';
+IARCDescriptor.ID = 'os.ui.arc.IARCDescriptor';
 
-/**
- * Set the url
- * @param {?string} value
- */
-os.ui.arc.IARCDescriptor.prototype.setUrl;
-
-/**
- * Get the url
- * @return {?string}
- */
-os.ui.arc.IARCDescriptor.prototype.getUrl;
-
-/**
- * Set deprecated
- * @param {boolean} value
- */
-os.ui.arc.IARCDescriptor.prototype.setDeprecated;
-
-/**
- * Get deprecated
- * @return {boolean}
- */
-os.ui.arc.IARCDescriptor.prototype.getDeprecated;
-
+exports = IARCDescriptor;
