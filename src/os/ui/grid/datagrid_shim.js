3d94727185037bf6ac71740bb969cf32953e6d02
Connor Gibson <connor.gibson@caci.com>
2020-12-08 17:22:20 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/ui/grid

---
+++ src/os/ui/grid/datagrid_shim.js
@@ -0,0 +1,13 @@
+goog.provide('os.ui.grid.DataGridCtrl');
+goog.provide('os.ui.grid.dataGridDirective');
+goog.require('os.ui.grid.DataGridUI');
+
+/**
+ * @deprecated Please use goog.require('os.ui.grid.DataGridUI').Controller instead.
+ */
+os.ui.grid.DataGridCtrl = os.ui.grid.DataGridUI.Controller;
+
+/**
+ * @deprecated Please use goog.require('os.ui.grid.DataGridUI').directive instead.
+ */
+os.ui.grid.dataGridDirective = os.ui.grid.DataGridUI.directive;
