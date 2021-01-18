cd2bb5f28f500e999b20f590483397d63af20b87
Kevin Schmidt <kschmidt@caci.com>
2021-01-18 15:45:16 -0700

    refactor(karma): use webpack to build karma test dependencies
    By bundling all test dependencies with webpack, both goog.module and ES6 modules
    can be supported without specific loaders or Karma config. The test webpack
    config uses istanbul-instrumenter-loader to instrument the webpack output with
    coverage code, which is supplemented by karma-coverage-istanbul-reporter to
    generate reports.

--- test/plugin/cesium/primitive.mock.js
+++ test/plugin/cesium/primitive.mock.js
@@ -1,15 +1,16 @@
-goog.module('test.plugin.cesium.primitive');
+goog.declareModuleId('test.plugin.cesium.primitive');
 
 goog.require('test.plugin.cesium');
 
 const olcsCore = goog.require('olcs.core');
+
 
 /**
  * @param {ol.Coordinate} coord
  * @param {string=} opt_imageSrc
  * @return {!Cesium.Billboard}
  */
-const createBillboard = (coord, opt_imageSrc) => {
+export const createBillboard = (coord, opt_imageSrc) => {
   opt_imageSrc = opt_imageSrc || '/base/images/icons/pushpin/wht-pushpin.png';
   const collection = new Cesium.BillboardCollection();
   const item = collection.add({
@@ -25,7 +26,7 @@ const createBillboard = (coord, opt_imageSrc) => {
  * @param {Cesium.Color=} opt_color
  * @return {!Cesium.PointPrimitive}
  */
-const createPointPrimitive = (coord, opt_color) => {
+export const createPointPrimitive = (coord, opt_color) => {
   const collection = new Cesium.PointPrimitiveCollection();
   const item = collection.add({
     position: olcsCore.ol4326CoordinateToCesiumCartesian(coord),
@@ -35,19 +36,17 @@ const createPointPrimitive = (coord, opt_color) => {
   return item;
 };
 
-
 /**
  * @param {!ol.Extent} extent
  * @return {!Cesium.Primitive}
  */
-const createPrimitive = (extent) => new Cesium.Primitive(createPrimitiveOptions(extent));
-
+export const createPrimitive = (extent) => new Cesium.Primitive(createPrimitiveOptions(extent));
 
 /**
  * @param {!ol.Extent} extent
  * @return {!Cesium.GroundPrimitive}
  */
-const createGroundPrimitive = (extent) => new Cesium.GroundPrimitive(createPrimitiveOptions(extent));
+export const createGroundPrimitive = (extent) => new Cesium.GroundPrimitive(createPrimitiveOptions(extent));
 
 
 /**
@@ -74,7 +73,7 @@ const createPrimitiveOptions = (extent) => ({
  * @param {!Array<!ol.Coordinate>} coords
  * @return {!Cesium.Polyline}
  */
-const createPolyline = (coords) => {
+export const createPolyline = (coords) => {
   const collection = new Cesium.PolylineCollection();
   const item = collection.add(createPolylineOptions(coords));
   collection.remove(item);
@@ -91,7 +90,7 @@ const createPolylineOptions = (coords) => ({
  * @param {!Array<!ol.Coordinate>} coords
  * @return {!Cesium.GroundPolylinePrimitive}
  */
-const createGroundPolylinePrimitive = (coords) => {
+export const createGroundPolylinePrimitive = (coords) => {
   return new Cesium.GroundPolylinePrimitive({
     geometryInstances: new Cesium.GeometryInstance({
       geometry: new Cesium.GroundPolylineGeometry({
@@ -120,17 +119,7 @@ const getLineWidth = (width) => Math.min(Math.max(2, Cesium.ContextLimits.minimu
  * @param {string=} opt_text
  * @return {!Cesium.optionsLabelCollection}
  */
-const createLabelOptions = (opt_text) => {
+export const createLabelOptions = (opt_text) => {
   opt_text = opt_text || 'Test Label';
   return {text: opt_text};
-};
-
-exports = {
-  createBillboard,
-  createGroundPrimitive,
-  createGroundPolylinePrimitive,
-  createLabelOptions,
-  createPointPrimitive,
-  createPolyline,
-  createPrimitive
 };
