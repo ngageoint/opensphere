c95780dae11b762c4475b1eae86cd18d7db83800
ameletyan <artm95@gmail.com>
2021-03-04 12:21:40 -0700

    fix: converted some this variables to bracket notation

--- src/os/ui/uiswitch.js
+++ src/os/ui/uiswitch.js
@@ -70,46 +70,46 @@ os.ui.UISwitchCtrl = function($scope, $element, $compile) {
    * @type {?angular.Scope}
    * @protected
    */
-  this.scope = $scope;
+  this['scope'] = $scope;
 
   /**
    * @type {?angular.JQLite}
    * @private
    */
-  this.element_ = $element;
+  this['element_'] = $element;
 
   /**
    * If the UI should be switched even when the directive doesn't change.
    * @type {boolean}
    * @private
    */
-  this.alwaysSwitch_ = this.scope['alwaysSwitch'] === 'true';
+  this['alwaysSwitch_'] = this['scope']['alwaysSwitch'] === 'true';
 
   /**
    * @type {?angular.JQLite}
    * @private
    */
-  this.currUI_ = null;
+  this['currUI_'] = null;
 
   /**
    * @type {?string}
    * @private
    */
-  this.lastUI_ = null;
+  this['lastUI_'] = null;
 
   /**
    * @type {?angular.$compile}
    * @private
    */
-  this.compile_ = $compile;
+  this['compile_'] = $compile;
 
   /**
    * @type {Array.<function()>}
    * @private
    */
-  this.destroyers_ = [];
+  this['destroyers_'] = [];
 
-  this.destroyers_.push($scope.$watch('items', this.onDataChange_.bind(this)));
+  this['destroyers_'].push($scope.$watch('items', this.onDataChange_.bind(this)));
   $scope.$on(os.ui.UISwitchEventType.UPDATE, this.update_.bind(this));
   $scope.$on('$destroy', this.onDestroy_.bind(this));
 
@@ -117,21 +117,21 @@ os.ui.UISwitchCtrl = function($scope, $element, $compile) {
    * @type {?MutationObserver}
    * @private
    */
-  this.observer_ = null;
+  this['observer_'] = null;
 
   /**
    * @type {?goog.async.Delay}
    * @private
    */
-  this.delay_ = null;
+  this['delay_'] = null;
 
   if ('MutationObserver' in window && MutationObserver) {
-    this.observer_ = new MutationObserver(this.onChange_.bind(this));
-    this.observer_.observe(this.element_[0], /** @type {MutationObserverInit} */ ({'childList': true}));
+    this['observer_'] = new MutationObserver(this.onChange_.bind(this));
+    this['observer_'].observe(this['element_'][0], /** @type {MutationObserverInit} */ ({'childList': true}));
   } else {
     // This should take care of older browsers, but can cause a race condition. Especially the first time that
     // a templateUrl is loaded.
-    this.delay_ = new goog.async.Delay(this.onChange_, 25, this);
+    this['delay_'] = new goog.async.Delay(this.onChange_, 25, this);
   }
 
   this.update_();
@@ -165,11 +165,11 @@ os.ui.UISwitchCtrl.prototype.onDataChange_ = function(newVal, oldVal) {
  * @private
  */
 os.ui.UISwitchCtrl.prototype.update_ = function() {
-  var x = this.scope['items'];
+  var x = this['scope']['items'];
 
   var items = Array.isArray(x) ? /** @type {Array} */ (x) : [x];
-  var ui = /** @type {function(*):string} */ (this.scope['directiveFunction']);
-  var generic = /** @type {string} */ (this.scope['generic']) || null;
+  var ui = /** @type {function(*):string} */ (this['scope']['directiveFunction']);
+  var generic = /** @type {string} */ (this['scope']['generic']) || null;
 
   /** @type {?string} */
   var newUI = null;
@@ -194,14 +194,14 @@ os.ui.UISwitchCtrl.prototype.update_ = function() {
 
   goog.log.fine(os.ui.UISwitchCtrl.LOGGER_, 'The new UI is ' + newUI);
 
-  if (this.alwaysSwitch_ || newUI != this.lastUI_) {
+  if (this['alwaysSwitch_'] || newUI != this['lastUI_']) {
     this.remove_();
 
     if (newUI) {
       var html = '<' + newUI + '></' + newUI + '>';
 
       // make a new scope
-      var s = this.scope.$new();
+      var s = this['scope'].$new();
 
       // put the items on the scope
       s['items'] = items;
@@ -210,22 +210,22 @@ os.ui.UISwitchCtrl.prototype.update_ = function() {
 
       // compile
       // This cast should not be necessary, are we missing something in the externs?
-      this.currUI_ = /** @type {?angular.JQLite} */ (this.compile_(html)(s));
-      this.element_.append(this.currUI_);
+      this['currUI_'] = /** @type {?angular.JQLite} */ (this['compile_'](html)(s));
+      this['element_'].append(this['currUI_']);
 
-      if (this.delay_) {
-        this.delay_.start();
+      if (this['delay_']) {
+        this['delay_'].start();
       }
     }
-  } else if (this.currUI_) {
+  } else if (this['currUI_']) {
     // update the items on the scope
-    s = this.currUI_.scope();
+    s = this['currUI_'].scope();
     s['items'] = items;
 
     os.ui.apply(s);
   }
 
-  this.lastUI_ = newUI;
+  this['lastUI_'] = newUI;
 };
 
 
@@ -236,7 +236,7 @@ os.ui.UISwitchCtrl.prototype.update_ = function() {
  * @protected
  */
 os.ui.UISwitchCtrl.prototype.addToScope = function(scope) {
-  var scopeUpdateFunction = /** @type {function(angular.Scope)} */ (this.scope['scopeUpdateFunction']);
+  var scopeUpdateFunction = /** @type {function(angular.Scope)} */ (this['scope']['scopeUpdateFunction']);
   if (scopeUpdateFunction) {
     scopeUpdateFunction(scope);
   }
@@ -249,7 +249,7 @@ os.ui.UISwitchCtrl.prototype.addToScope = function(scope) {
  * @private
  */
 os.ui.UISwitchCtrl.prototype.onChange_ = function() {
-  this.scope.$emit('controls.changed', this.element_.outerWidth(), this.element_.outerHeight());
+  this['scope'].$emit('controls.changed', this['element_'].outerWidth(), this['element_'].outerHeight());
 };
 
 
@@ -259,17 +259,17 @@ os.ui.UISwitchCtrl.prototype.onChange_ = function() {
  * @private
  */
 os.ui.UISwitchCtrl.prototype.remove_ = function() {
-  if (this.currUI_) {
-    this.scope.$emit('controls.changed', -this.element_.outerWidth(), -this.element_.outerHeight());
-    var s = this.currUI_.scope();
-    this.currUI_.remove();
+  if (this['currUI_']) {
+    this['scope'].$emit('controls.changed', -this['element_'].outerWidth(), -this['element_'].outerHeight());
+    var s = this['currUI_'].scope();
+    this['currUI_'].remove();
 
     if (s) {
       s.$destroy();
     }
 
-    this.currUI_ = null;
-    this.lastUI_ = null;
+    this['currUI_'] = null;
+    this['lastUI_'] = null;
   }
 };
 
@@ -280,23 +280,23 @@ os.ui.UISwitchCtrl.prototype.remove_ = function() {
  * @private
  */
 os.ui.UISwitchCtrl.prototype.onDestroy_ = function() {
-  for (var i = 0, n = this.destroyers_.length; i < n; i++) {
-    this.destroyers_[i]();
+  for (var i = 0, n = this['destroyers_'].length; i < n; i++) {
+    this['destroyers_'][i]();
   }
 
-  if (this.observer_) {
-    this.observer_.disconnect();
-    this.observer_ = null;
+  if (this['observer_']) {
+    this['observer_'].disconnect();
+    this['observer_'] = null;
   }
 
-  if (this.delay_) {
-    this.delay_.stop();
-    this.delay_.dispose();
-    this.delay_ = null;
+  if (this['delay_']) {
+    this['delay_'].stop();
+    this['delay_'].dispose();
+    this['delay_'] = null;
   }
 
   this.remove_();
-  this.scope = null;
-  this.element_ = null;
-  this.compile_ = null;
+  this['scope'] = null;
+  this['element_'] = null;
+  this['compile_'] = null;
 };
