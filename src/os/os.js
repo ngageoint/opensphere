acd13cbd1e3d2fe978328fd5ec40c0738f0f3497
Kevin Schmidt <kschmidt@caci.com>
2021-03-31 05:05:41 -0600

    refactor(os): move core goog.defines to correct namespace

--- src/os/os.js
+++ src/os/os.js
@@ -13,6 +13,42 @@ goog.requireType('os.debug.FancierWindow');
 
 
 /**
+ * @define {string} The root namespace for DB and storage keys
+ */
+os.NAMESPACE = goog.define('os.NAMESPACE', 'opensphere');
+
+
+/**
+ * @define {string} The path to this project
+ */
+os.ROOT = goog.define('os.ROOT', '../opensphere/');
+
+
+/**
+ * @define {string} The path to this project
+ */
+os.APP_ROOT = goog.define('os.APP_ROOT', './');
+
+
+/**
+ * @define {string} The settings file
+ */
+os.SETTINGS = goog.define('os.SETTINGS', '.build/settings-debug.json');
+
+
+/**
+ * @define {string} The DB name for settings storage
+ */
+os.SETTINGS_DB_NAME = goog.define('os.SETTINGS_DB_NAME', os.NAMESPACE + '.settings');
+
+
+/**
+ * @define {string} The database name used to transfer between apps
+ */
+os.SHARED_FILE_DB_NAME = goog.define('os.SHARED_FILE_DB_NAME', 'com.bitsys.db');
+
+
+/**
  * TODO after running the ES6 conversion on a lot of the files, bring this deprecated to life; it'd add
  * over a thousand warnings right now.
  *
