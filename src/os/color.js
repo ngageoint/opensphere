c340e78f39e01f17f055285fafd5812071c20e80
ameletyan <artm95@gmail.com>
2021-03-31 11:05:24 -0600

    feat: resolved performance issues when changing layer styles

--- src/os/color.js
+++ src/os/color.js
@@ -791,12 +791,9 @@ const adjustColor = function(data, brightness, contrast, saturation) {
     const g = data[i + 1];
     const b = data[i + 2];
     const a = data[i + 3];
-    data[i] = Math.round((r * m[0] + g * m[5] + b * m[10] + a * m[15] + m[20]));
-    data[i + 1] = Math.round((r * m[1] + g * m[6] + b * m[11] + a * m[16] + m[21]));
-    data[i + 2] = Math.round((r * m[2] + g * m[7] + b * m[12] + a * m[17] + m[22]));
-    data[i] = goog.math.clamp(data[i], 0, 255);
-    data[i + 1] = goog.math.clamp(data[i + 1], 0, 255);
-    data[i + 2] = goog.math.clamp(data[i + 2], 0, 255);
+    data[i] = r * m[0] + g * m[5] + b * m[10] + a * m[15] + m[20];
+    data[i + 1] = r * m[1] + g * m[6] + b * m[11] + a * m[16] + m[21];
+    data[i + 2] = r * m[2] + g * m[7] + b * m[12] + a * m[17] + m[22];
   }
 };
 
