753a572496e5d1f989a0a45ca76dcf14fe2d020d
Kevin Schmidt <kschmidt@caci.com>
2021-04-15 12:50:32 -0600

    refactor(xyz): transform xyz plugin to goog.module

--- src/plugin/xyz/xyzplugin.js
+++ src/plugin/xyz/xyzplugin.js
@@ -1,27 +1,30 @@
-goog.provide('plugin.xyz.XYZPlugin');
+goog.module('plugin.xyz.XYZPlugin');
+goog.module.declareLegacyNamespace();
 
-goog.require('os.plugin.AbstractPlugin');
-goog.require('plugin.xyz.XYZLayerConfig');
-
+const LayerConfigManager = goog.require('os.layer.config.LayerConfigManager');
+const AbstractPlugin = goog.require('os.plugin.AbstractPlugin');
+const XYZLayerConfig = goog.require('plugin.xyz.XYZLayerConfig');
 
 
 /**
  * Provides map layer support
- *
- * @extends {os.plugin.AbstractPlugin}
- * @constructor
  */
-plugin.xyz.XYZPlugin = function() {
-  plugin.xyz.XYZPlugin.base(this, 'constructor');
-  this.id = 'xyz';
-};
-goog.inherits(plugin.xyz.XYZPlugin, os.plugin.AbstractPlugin);
+class XYZPlugin extends AbstractPlugin {
+  /**
+   * Constructor.
+   */
+  constructor() {
+    super();
+    this.id = 'xyz';
+  }
 
+  /**
+   * @inheritDoc
+   */
+  init() {
+    var lcm = LayerConfigManager.getInstance();
+    lcm.registerLayerConfig('XYZ', XYZLayerConfig);
+  }
+}
 
-/**
- * @inheritDoc
- */
-plugin.xyz.XYZPlugin.prototype.init = function() {
-  var lcm = os.layer.config.LayerConfigManager.getInstance();
-  lcm.registerLayerConfig('XYZ', plugin.xyz.XYZLayerConfig);
-};
+exports = XYZPlugin;
