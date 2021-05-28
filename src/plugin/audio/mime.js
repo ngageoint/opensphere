98f198bc9d181e8e741c5e0654afa01b3084ddfd
Kevin Schmidt <kschmidt@caci.com>
2021-05-28 07:10:43 -0600

    fix(audio): fix audio files being incorrectly detected as text

--- src/plugin/audio/mime.js
+++ src/plugin/audio/mime.js
@@ -3,10 +3,18 @@ goog.module.declareLegacyNamespace();
 
 const Promise = goog.require('goog.Promise');
 const mime = goog.require('os.file.mime');
+const mimeText = goog.require('os.file.mime.text');
 
 const OSFile = goog.requireType('os.file.File');
 
 /**
+ * Priority for audio file detection. Some audio formats (like .wav) may be detected as text, so this has to run first.
+ * @type {number}
+ */
+const PRIORITY = mimeText.PRIORITY - 1;
+
+/**
+ * MIME type for audio files.
  * @type {string}
  */
 const TYPE = 'application/audio';
@@ -21,9 +29,11 @@ const detect = function(buffer, opt_file) {
     Promise.resolve(opt_file && /\.(mp3|wav|ogg|m4a)$/i.test(opt_file.getFileName())));
 };
 
-mime.register(TYPE, detect, 10000);
+// Some audio formats (like .wav) may be detected as text, so this has to run first.
+mime.register(TYPE, detect, PRIORITY);
 
 exports = {
+  PRIORITY,
   TYPE,
   detect
 };
