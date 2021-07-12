4f7bef95649b69f8823bf6898ff7b3bdc432f1ea
Kevin Schmidt <kschmidt@caci.com>
2021-07-12 06:39:33 -0600

    test(route): replace globals in route tests

--- test/os/ui/route/routemanager.test.js
+++ test/os/ui/route/routemanager.test.js
@@ -1,27 +1,35 @@
+goog.require('goog.object');
 goog.require('os.file.FileUrlHandler');
 goog.require('os.mock');
+goog.require('os.ui');
 goog.require('os.ui.route.RouteManager');
 
 
 describe('os.ui.route.RouteManager', function() {
-  var rm, handler;
+  const googObject = goog.module.get('goog.object');
+  const FileUrlHandler = goog.module.get('os.file.FileUrlHandler');
+  const ui = goog.module.get('os.ui');
+  const RouteManager = goog.module.get('os.ui.route.RouteManager');
+
+  let rm;
+  let handler;
 
   beforeEach(function() {
     inject(function($injector) {
-      os.ui.injector = $injector;
+      ui.injector = $injector;
     });
 
-    rm = new os.ui.route.RouteManager();
-    handler = new os.file.FileUrlHandler();
+    rm = new RouteManager();
+    handler = new FileUrlHandler();
   });
 
   it('should register URL handlers', function() {
     rm.registerUrlHandler(handler);
 
-    expect(goog.object.getCount(rm.routeHandlers_)).toBe(1);
-    expect(rm.routeHandlers_[os.file.FileUrlHandler.KEY]).not.toBe(null);
-    expect(rm.routeHandlers_[os.file.FileUrlHandler.KEY].length).toBe(1);
-    expect(rm.routeHandlers_[os.file.FileUrlHandler.KEY][0]).toBe(handler);
+    expect(googObject.getCount(rm.routeHandlers_)).toBe(1);
+    expect(rm.routeHandlers_[FileUrlHandler.KEY]).not.toBe(null);
+    expect(rm.routeHandlers_[FileUrlHandler.KEY].length).toBe(1);
+    expect(rm.routeHandlers_[FileUrlHandler.KEY][0]).toBe(handler);
   });
 
   it('should do nothing until its initialized', function() {
@@ -39,7 +47,7 @@ describe('os.ui.route.RouteManager', function() {
   });
 
   it('should unhandle handlers once their keys are gone', function() {
-    var obj = {'file': 'https://fake.com/#/?file=file.csv'};
+    let obj = {'file': 'https://fake.com/#/?file=file.csv'};
     rm.registerUrlHandler(handler);
     spyOn(rm, 'getSearch').andCallFake(function() {
       return obj;
@@ -49,13 +57,13 @@ describe('os.ui.route.RouteManager', function() {
 
     rm.initialize();
     expect(handler.handle.calls.length).toBe(1);
-    expect(handler.handlesCache[os.file.FileUrlHandler.KEY]).toEqual(['https://fake.com/#/?file=file.csv']);
+    expect(handler.handlesCache[FileUrlHandler.KEY]).toEqual(['https://fake.com/#/?file=file.csv']);
 
     // set the search object to be empty
     obj = {};
     rm.onRouteUpdate();
 
     expect(handler.unhandleAll.calls.length).toBe(1);
-    expect(handler.handlesCache[os.file.FileUrlHandler.KEY]).toBe(undefined);
+    expect(handler.handlesCache[FileUrlHandler.KEY]).toBe(undefined);
   });
 });
