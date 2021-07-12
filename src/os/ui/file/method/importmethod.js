1d59e15741aa2b6423ebda60a952295cbe4673cf
Kevin Schmidt <kschmidt@caci.com>
2021-07-12 12:34:56 -0600

    refactor(file): transform file methods to goog modules

--- src/os/ui/file/method/importmethod.js
+++ src/os/ui/file/method/importmethod.js
@@ -1,116 +1,111 @@
-goog.provide('os.ui.file.method.ImportMethod');
-goog.require('goog.async.Delay');
-goog.require('goog.dom');
-goog.require('goog.dom.TagName');
-goog.require('goog.log');
-goog.require('goog.log.Logger');
-goog.require('os.file');
-goog.require('os.file.IFileMethod');
-goog.require('os.ui.file.importDialogDirective');
-goog.require('os.ui.file.method.UrlMethod');
-goog.require('os.ui.window');
+goog.module('os.ui.file.method.ImportMethod');
+goog.module.declareLegacyNamespace();
 
+goog.require('os.ui.file.importDialogDirective');
+
+const log = goog.require('goog.log');
+const EventType = goog.require('os.events.EventType');
+const UrlMethod = goog.require('os.ui.file.method.UrlMethod');
+const ImportManager = goog.require('os.ui.im.ImportManager');
+const osWindow = goog.require('os.ui.window');
+
+const Logger = goog.requireType('goog.log.Logger');
 
 
 /**
  * Import method handling both local files and URL's.
- *
- * @param {boolean=} opt_keepFile Whether the file method will keep the original File reference around
- * @extends {os.ui.file.method.UrlMethod}
- * @constructor
  */
-os.ui.file.method.ImportMethod = function(opt_keepFile) {
-  os.ui.file.method.ImportMethod.base(this, 'constructor');
-  this.log = os.ui.file.method.ImportMethod.LOGGER_;
+class ImportMethod extends UrlMethod {
+  /**
+   * Constructor.
+   * @param {boolean=} opt_keepFile Whether the file method will keep the original File reference around
+   */
+  constructor(opt_keepFile) {
+    super();
+    this.log = logger;
+
+    /**
+     * If the File reference should be saved on the {@link os.file.File} instance.
+     * @type {boolean}
+     * @private
+     */
+    this.keepFile_ = Boolean(opt_keepFile);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  getPriority() {
+    return 100;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  clone() {
+    var other = new this.constructor();
+    other.setKeepFile(this.getKeepFile());
+    return other;
+  }
 
   /**
    * If the File reference should be saved on the {@link os.file.File} instance.
-   * @type {boolean}
-   * @private
+   *
+   * @return {boolean}
    */
-  this.keepFile_ = Boolean(opt_keepFile);
-};
-goog.inherits(os.ui.file.method.ImportMethod, os.ui.file.method.UrlMethod);
+  getKeepFile() {
+    return this.keepFile_;
+  }
 
+  /**
+   * Set if the File reference should be saved on the {@link os.file.File} instance.
+   *
+   * @param {boolean} val
+   */
+  setKeepFile(val) {
+    this.keepFile_ = val;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  loadFile(opt_options) {
+    var file = this.getFile();
+    var url = this.getUrl();
+    if (!file && !url) {
+      var scopeOptions = {
+        'manager': opt_options && opt_options['manager'] || ImportManager.getInstance(),
+        'method': this
+      };
+
+      var windowOptions = {
+        'id': UrlMethod.ID,
+        'label': 'Import Data',
+        'icon': 'fa fa-cloud-download',
+        'x': 'center',
+        'y': 'center',
+        'width': '400',
+        'min-width': '400',
+        'max-width': '400',
+        'height': 'auto',
+        'modal': 'true',
+        'show-close': 'true'
+      };
+
+      var template = '<importdialog manager="manager" method="method"></importdialog>';
+      osWindow.create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
+    } else if (url) {
+      this.loadUrl();
+    } else if (file) {
+      this.dispatchEvent(EventType.COMPLETE);
+    }
+  }
+}
 
 /**
  * Logger
- * @type {goog.log.Logger}
- * @private
- * @const
+ * @type {Logger}
  */
-os.ui.file.method.ImportMethod.LOGGER_ = goog.log.getLogger('os.ui.file.method.ImportMethod');
+const logger = log.getLogger('os.ui.file.method.ImportMethod');
 
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.ImportMethod.prototype.getPriority = function() {
-  return 100;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.ImportMethod.prototype.clone = function() {
-  var other = new this.constructor();
-  other.setKeepFile(this.getKeepFile());
-  return other;
-};
-
-
-/**
- * If the File reference should be saved on the {@link os.file.File} instance.
- *
- * @return {boolean}
- */
-os.ui.file.method.ImportMethod.prototype.getKeepFile = function() {
-  return this.keepFile_;
-};
-
-
-/**
- * Set if the File reference should be saved on the {@link os.file.File} instance.
- *
- * @param {boolean} val
- */
-os.ui.file.method.ImportMethod.prototype.setKeepFile = function(val) {
-  this.keepFile_ = val;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.ImportMethod.prototype.loadFile = function(opt_options) {
-  var file = this.getFile();
-  var url = this.getUrl();
-  if (!file && !url) {
-    var scopeOptions = {
-      'manager': opt_options && opt_options['manager'] || os.ui.im.ImportManager.getInstance(),
-      'method': this
-    };
-
-    var windowOptions = {
-      'id': os.ui.file.method.UrlMethod.ID,
-      'label': 'Import Data',
-      'icon': 'fa fa-cloud-download',
-      'x': 'center',
-      'y': 'center',
-      'width': '400',
-      'min-width': '400',
-      'max-width': '400',
-      'height': 'auto',
-      'modal': 'true',
-      'show-close': 'true'
-    };
-
-    var template = '<importdialog manager="manager" method="method"></importdialog>';
-    os.ui.window.create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
-  } else if (url) {
-    this.loadUrl();
-  } else if (file) {
-    this.dispatchEvent(os.events.EventType.COMPLETE);
-  }
-};
+exports = ImportMethod;
--- src/os/ui/file/method/urlmethod.js
+++ src/os/ui/file/method/urlmethod.js
@@ -1,287 +1,279 @@
-goog.provide('os.ui.file.method.UrlMethod');
+goog.module('os.ui.file.method.UrlMethod');
+goog.module.declareLegacyNamespace();
 
-goog.require('goog.events.EventTarget');
-goog.require('goog.log');
-goog.require('goog.log.Logger');
-goog.require('goog.net.XhrIo');
-goog.require('goog.userAgent');
-goog.require('os.alert.AlertEventSeverity');
-goog.require('os.alert.AlertManager');
-goog.require('os.file');
-goog.require('os.file.File');
-goog.require('os.file.IFileMethod');
-goog.require('os.net.Request');
-goog.require('os.net.RequestEvent');
-goog.require('os.net.RequestEventType');
 goog.require('os.ui.file.urlImportDirective');
-goog.require('os.ui.help.EventType');
-goog.require('os.ui.window');
 
+const dispose = goog.require('goog.dispose');
+const EventTarget = goog.require('goog.events.EventTarget');
+const log = goog.require('goog.log');
+const NetEventType = goog.require('goog.net.EventType');
+const XhrIo = goog.require('goog.net.XhrIo');
+const {IE, isVersionOrHigher} = goog.require('goog.userAgent');
+const dispatcher = goog.require('os.Dispatcher');
+const AlertEventSeverity = goog.require('os.alert.AlertEventSeverity');
+const AlertManager = goog.require('os.alert.AlertManager');
+const EventType = goog.require('os.events.EventType');
+const {createFromContent} = goog.require('os.file');
+const IFileMethod = goog.require('os.file.IFileMethod'); // eslint-disable-line
+const Request = goog.require('os.net.Request');
+const RequestEvent = goog.require('os.net.RequestEvent');
+const RequestEventType = goog.require('os.net.RequestEventType');
+const osWindow = goog.require('os.ui.window');
+
+const Logger = goog.requireType('goog.log.Logger');
+const OSFile = goog.requireType('os.file.File');
 
 
 /**
- * @implements {os.file.IFileMethod}
- * @extends {goog.events.EventTarget}
- * @constructor
+ * @implements {IFileMethod}
  */
-os.ui.file.method.UrlMethod = function() {
-  os.ui.file.method.UrlMethod.base(this, 'constructor');
+class UrlMethod extends EventTarget {
+  /**
+   * Constructor.
+   */
+  constructor() {
+    super();
+
+    /**
+     * The logger
+     * @type {Logger}
+     * @protected
+     */
+    this.log = logger;
+
+    /**
+     * @type {OSFile}
+     * @private
+     */
+    this.file_ = null;
+
+    /**
+     * @type {Request}
+     * @private
+     */
+    this.request_ = null;
+
+    /**
+     * @type {?string}
+     * @private
+     */
+    this.url_ = null;
+  }
 
   /**
-   * The logger
-   * @type {goog.log.Logger}
+   * @inheritDoc
+   */
+  disposeInternal() {
+    super.disposeInternal();
+
+    this.clearFile();
+
+    dispose(this.request_);
+    this.request_ = null;
+  }
+
+  /**
+   * @return {?string}
+   */
+  getUrl() {
+    return this.url_;
+  }
+
+  /**
+   * @param {?string} value
+   */
+  setUrl(value) {
+    this.url_ = value;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  getPriority() {
+    return 0;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  isSupported() {
+    return true;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  getFile() {
+    return this.file_;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  setFile(file) {
+    this.file_ = file;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  clearFile() {
+    this.file_ = null;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  clone() {
+    return new this.constructor();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  loadFile(opt_options) {
+    this.clearFile();
+
+    if (!this.url_) {
+      var scopeOptions = {
+        'method': this
+      };
+      var windowOptions = {
+        'id': UrlMethod.ID,
+        'label': 'Import URL',
+        'icon': 'fa fa-cloud-download',
+        'x': 'center',
+        'y': 'center',
+        'width': '400',
+        'min-width': '400',
+        'max-width': '400',
+        'height': 'auto',
+        'modal': true,
+        'show-close': true
+      };
+      var template = '<urlimport></urlimport>';
+      osWindow.create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
+    } else {
+      this.loadUrl();
+    }
+  }
+
+  /**
+   * Loads a file from the provided URL.
+   */
+  loadUrl() {
+    if (!this.url_) {
+      log.error(logger, 'URL cannot be null!');
+      this.dispatchEvent(EventType.CANCEL);
+      return;
+    }
+
+    if (this.request_) {
+      this.request_.dispose();
+      this.request_ = null;
+    }
+
+    // if this url contains our app, strip the app portion
+    var appIndex = this.url_.lastIndexOf('?file=');
+    if (appIndex != -1) {
+      this.url_ = this.url_.substring(appIndex + 6);
+    }
+
+    this.request_ = new Request(this.url_);
+    this.request_.setHeader('Accept', '*/*');
+
+    // use default validators to try to auto-detect specific errors
+    this.request_.setUseDefaultValidators(true);
+
+    if (!IE || isVersionOrHigher(10)) {
+      // IE9 doesn't support arraybuffer as a response type
+      this.request_.setResponseType(XhrIo.ResponseType.ARRAY_BUFFER);
+    }
+
+    this.request_.listen(NetEventType.SUCCESS, this.onLoad, false, this);
+    this.request_.listen(NetEventType.ERROR, this.onError, false, this);
+    this.request_.load();
+  }
+
+  /**
+   * @param {goog.events.Event} event
    * @protected
    */
-  this.log = os.ui.file.method.UrlMethod.LOGGER_;
+  onLoad(event) {
+    var url = this.request_.getUri().toString();
+    var q = url.indexOf('?');
+    var i = url.lastIndexOf('/') + 1;
+    var fileName = decodeURI(url.substring(i == -1 ? 0 : i, q == -1 ? url.length : q));
+    var headers = this.request_.getResponseHeaders();
+    var response = /** @type {!(ArrayBuffer|string)} */ (this.request_.getResponse());
+
+    // There is a header that can be used if it exists instead of the filename
+    if (headers) {
+      var contentDisposition = /** @type {string|undefined} */ (headers['content-disposition']);
+      if (contentDisposition) {
+        // Use the value in the content-disposition ex: attachment; filename="Super Awesome Dataz.kmz"
+        var re = /filename="(.*?)"/;
+        var match = re.exec(contentDisposition);
+        if (match && match[1]) {
+          fileName = match[1];
+        } else { // see if the filename was not in quotes
+          var rematch = contentDisposition.split('filename=');
+          if (rematch.length > 0 && rematch[1]) {
+            fileName = rematch[1];
+          }
+        }
+      }
+    }
+
+    this.request_.dispose();
+    this.request_ = null;
+
+    this.file_ = createFromContent(fileName, url, undefined, response);
+
+    if (!this.file_.getContentType() && headers) {
+      // extract the content-type header if possible
+      var contentType = /** @type {string|undefined} */ (headers['content-type']);
+      if (contentType) {
+        this.file_.setContentType(contentType);
+      }
+    }
+
+    this.dispatchEvent(EventType.COMPLETE);
+    dispatcher.getInstance().dispatchEvent(new RequestEvent(RequestEventType.USER_URL, url));
+  }
 
   /**
-   * @type {os.file.File}
-   * @private
+   * @param {goog.events.Event} event
+   * @protected
    */
-  this.file_ = null;
+  onError(event) {
+    const errors = this.request_.getErrors();
+    this.request_.dispose();
+    this.request_ = null;
 
-  /**
-   * @type {os.net.Request}
-   * @private
-   */
-  this.request_ = null;
+    let msg = 'Unable to load the provided URL, please check that it was entered correctly.';
+    if (errors.length) {
+      // Add server error(s) to the message if available.
+      msg += ` The request failed with: ${errors.join(', ')}`;
+    }
 
-  /**
-   * @type {?string}
-   * @private
-   */
-  this.url_ = null;
-};
-goog.inherits(os.ui.file.method.UrlMethod, goog.events.EventTarget);
+    // sendAlert is async and we want the log messages in order, so log the error separately.
+    AlertManager.getInstance().sendAlert(msg, AlertEventSeverity.ERROR, logger);
 
+    // notify listeners that the load failed
+    this.dispatchEvent(EventType.ERROR);
+  }
+}
 
 /**
  * Logger
- * @type {goog.log.Logger}
- * @private
- * @const
+ * @type {Logger}
  */
-os.ui.file.method.UrlMethod.LOGGER_ = goog.log.getLogger('os.ui.file.method.UrlMethod');
-
+const logger = log.getLogger('os.ui.file.method.UrlMethod');
 
 /**
  * @type {string}
  */
-os.ui.file.method.UrlMethod.ID = 'urlimport';
+UrlMethod.ID = 'urlimport';
 
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.disposeInternal = function() {
-  os.ui.file.method.UrlMethod.base(this, 'disposeInternal');
-
-  this.clearFile();
-
-  goog.dispose(this.request_);
-  this.request_ = null;
-};
-
-
-/**
- * @return {?string}
- */
-os.ui.file.method.UrlMethod.prototype.getUrl = function() {
-  return this.url_;
-};
-
-
-/**
- * @param {?string} value
- */
-os.ui.file.method.UrlMethod.prototype.setUrl = function(value) {
-  this.url_ = value;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.getPriority = function() {
-  return 0;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.isSupported = function() {
-  return true;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.getFile = function() {
-  return this.file_;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.setFile = function(file) {
-  this.file_ = file;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.clearFile = function() {
-  this.file_ = null;
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.clone = function() {
-  return new this.constructor();
-};
-
-
-/**
- * @inheritDoc
- */
-os.ui.file.method.UrlMethod.prototype.loadFile = function(opt_options) {
-  this.clearFile();
-
-  if (!this.url_) {
-    var scopeOptions = {
-      'method': this
-    };
-    var windowOptions = {
-      'id': os.ui.file.method.UrlMethod.ID,
-      'label': 'Import URL',
-      'icon': 'fa fa-cloud-download',
-      'x': 'center',
-      'y': 'center',
-      'width': '400',
-      'min-width': '400',
-      'max-width': '400',
-      'height': 'auto',
-      'modal': true,
-      'show-close': true
-    };
-    var template = '<urlimport></urlimport>';
-    os.ui.window.create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
-  } else {
-    this.loadUrl();
-  }
-};
-
-
-/**
- * Loads a file from the provided URL.
- */
-os.ui.file.method.UrlMethod.prototype.loadUrl = function() {
-  if (!this.url_) {
-    goog.log.error(os.ui.file.method.UrlMethod.LOGGER_, 'URL cannot be null!');
-    this.dispatchEvent(os.events.EventType.CANCEL);
-    return;
-  }
-
-  if (this.request_) {
-    this.request_.dispose();
-    this.request_ = null;
-  }
-
-  // if this url contains our app, strip the app portion
-  var appIndex = this.url_.lastIndexOf('?file=');
-  if (appIndex != -1) {
-    this.url_ = this.url_.substring(appIndex + 6);
-  }
-
-  this.request_ = new os.net.Request(this.url_);
-  this.request_.setHeader('Accept', '*/*');
-
-  // use default validators to try to auto-detect specific errors
-  this.request_.setUseDefaultValidators(true);
-
-  if (!goog.userAgent.IE || goog.userAgent.isVersionOrHigher(10)) {
-    // IE9 doesn't support arraybuffer as a response type
-    this.request_.setResponseType(goog.net.XhrIo.ResponseType.ARRAY_BUFFER);
-  }
-
-  this.request_.listen(goog.net.EventType.SUCCESS, this.onLoad, false, this);
-  this.request_.listen(goog.net.EventType.ERROR, this.onError, false, this);
-  this.request_.load();
-};
-
-
-/**
- * @param {goog.events.Event} event
- * @protected
- */
-os.ui.file.method.UrlMethod.prototype.onLoad = function(event) {
-  var url = this.request_.getUri().toString();
-  var q = url.indexOf('?');
-  var i = url.lastIndexOf('/') + 1;
-  var fileName = decodeURI(url.substring(i == -1 ? 0 : i, q == -1 ? url.length : q));
-  var headers = this.request_.getResponseHeaders();
-  var response = /** @type {!(ArrayBuffer|string)} */ (this.request_.getResponse());
-
-  // There is a header that can be used if it exists instead of the filename
-  if (headers) {
-    var contentDisposition = /** @type {string|undefined} */ (headers['content-disposition']);
-    if (contentDisposition) {
-      // Use the value in the content-disposition ex: attachment; filename="Super Awesome Dataz.kmz"
-      var re = /filename="(.*?)"/;
-      var match = re.exec(contentDisposition);
-      if (match && match[1]) {
-        fileName = match[1];
-      } else { // see if the filename was not in quotes
-        var rematch = contentDisposition.split('filename=');
-        if (rematch.length > 0 && rematch[1]) {
-          fileName = rematch[1];
-        }
-      }
-    }
-  }
-
-  this.request_.dispose();
-  this.request_ = null;
-
-  this.file_ = os.file.createFromContent(fileName, url, undefined, response);
-
-  if (!this.file_.getContentType() && headers) {
-    // extract the content-type header if possible
-    var contentType = /** @type {string|undefined} */ (headers['content-type']);
-    if (contentType) {
-      this.file_.setContentType(contentType);
-    }
-  }
-
-  this.dispatchEvent(os.events.EventType.COMPLETE);
-  os.dispatcher.dispatchEvent(new os.net.RequestEvent(os.net.RequestEventType.USER_URL, url));
-};
-
-
-/**
- * @param {goog.events.Event} event
- * @protected
- */
-os.ui.file.method.UrlMethod.prototype.onError = function(event) {
-  const errors = this.request_.getErrors();
-  this.request_.dispose();
-  this.request_ = null;
-
-  let msg = 'Unable to load the provided URL, please check that it was entered correctly.';
-  if (errors.length) {
-    // Add server error(s) to the message if available.
-    msg += ` The request failed with: ${errors.join(', ')}`;
-  }
-
-  // sendAlert is async and we want the log messages in order, so log the error separately.
-  os.alertManager.sendAlert(msg, os.alert.AlertEventSeverity.ERROR, os.ui.file.method.UrlMethod.LOGGER_);
-
-  // notify listeners that the load failed
-  this.dispatchEvent(os.events.EventType.ERROR);
-};
+exports = UrlMethod;
