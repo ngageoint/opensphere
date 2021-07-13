1f2c122f7e0d1df08be579aa0d07d67a2d1258c5
Kevin Schmidt <kschmidt@caci.com>
2021-07-13 07:27:51 -0600

    test(draw): replace globals in draw ui tests

--- test/os/ui/draw/draw.test.js
+++ test/os/ui/draw/draw.test.js
@@ -2,27 +2,32 @@ goog.require('ol.Feature');
 goog.require('ol.geom.Polygon');
 goog.require('ol.proj');
 goog.require('os.map');
+goog.require('os.proj');
+goog.require('os.style.area');
 goog.require('os.ui.draw');
 
 describe('os.ui.draw', function() {
+  const Feature = goog.module.get('ol.Feature');
+  const Polygon = goog.module.get('ol.geom.Polygon');
+  const {transformExtent} = goog.module.get('ol.proj');
+  const osMap = goog.module.get('os.map');
+  const {EPSG4326} = goog.module.get('os.proj');
+  const {GRID_STYLE} = goog.module.get('os.style.area');
+  const {getGridFromFeature} = goog.module.get('os.ui.draw');
+
   // reusable geometry / feature for testing
-  var g = ol.geom.Polygon
-    .fromExtent(
-      ol.proj.transformExtent(
-        [0.05, 0.05, 0.15, 0.15],
-        os.proj.EPSG4326,
-        os.map.PROJECTION));
-  var feature = new ol.Feature({geometry: g});
+  var g = Polygon.fromExtent(transformExtent([0.05, 0.05, 0.15, 0.15], EPSG4326, osMap.PROJECTION));
+  var feature = new Feature({geometry: g});
 
   it('should create a "detail x detail" grid around a feature, snapped to the world Lat/Lon coodinates', function() {
     runs(function() {
       var options = /** @type {!osx.ui.draw.GridOptions} */ ({
         detail: 0.1,
         max: 20.0,
-        style: os.style.area.GRID_STYLE
+        style: GRID_STYLE
       });
 
-      var grid = os.ui.draw.getGridFromFeature(feature, options);
+      var grid = getGridFromFeature(feature, options);
 
       expect(grid).toBeDefined();
       expect(grid.length).toBe(4); // since detail is 0.1, the 0.05 to 0.15 extent will get a 2x2 grid from 0.0 to 0.2 drawn around it
@@ -32,12 +37,12 @@ describe('os.ui.draw', function() {
   it('should not create a grid without valid GridOptions', function() {
     runs(function() {
       var options = /** @type {!osx.ui.draw.GridOptions} */ ({
-        detail: "1.0",
+        detail: 1.0,
         max: -3.0,
         style: null
       });
 
-      var grid = os.ui.draw.getGridFromFeature(feature, options);
+      var grid = getGridFromFeature(feature, options);
 
       expect(grid).toBeNull(); // does not calculate, since GridOptions are bad
     });
@@ -48,10 +53,10 @@ describe('os.ui.draw', function() {
       var options = /** @type {!osx.ui.draw.GridOptions} */ ({
         detail: 0.1,
         max: 1.0,
-        style: os.style.area.GRID_STYLE
+        style: GRID_STYLE
       });
 
-      var grid = os.ui.draw.getGridFromFeature(feature, options);
+      var grid = getGridFromFeature(feature, options);
 
       expect(grid).toBeNull(); // does not calculate the 2x2 grid, since 4 > max
     });
