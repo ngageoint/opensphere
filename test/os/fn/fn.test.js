c5d7c4ed9ae70672aaeb131f901c01890681cd97
Kevin Schmidt <kschmidt@caci.com>
2021-07-20 07:20:41 -0600

    test(fn): replace globals in fn tests

--- test/os/fn/fn.test.js
+++ test/os/fn/fn.test.js
@@ -2,15 +2,34 @@ goog.require('ol.Feature');
 goog.require('ol.extent');
 goog.require('ol.geom.Point');
 goog.require('ol.source.Image');
+goog.require('os.data.LayerNode');
 goog.require('os.fn');
+goog.require('os.layer.Image');
+goog.require('os.layer.Tile');
 goog.require('os.layer.Vector');
+goog.require('os.proj');
 goog.require('os.source.Vector');
+goog.require('os.structs.TreeNode');
 goog.require('plugin.xyz.XYZLayerConfig');
 
 describe('os.fn', function() {
+  const Feature = goog.module.get('ol.Feature');
+  const olExtent = goog.module.get('ol.extent');
+  const Point = goog.module.get('ol.geom.Point');
+  const olSourceImage = goog.module.get('ol.source.Image');
+  const LayerNode = goog.module.get('os.data.LayerNode');
+  const fn = goog.module.get('os.fn');
+  const Image = goog.module.get('os.layer.Image');
+  const Tile = goog.module.get('os.layer.Tile');
+  const VectorLayer = goog.module.get('os.layer.Vector');
+  const osProj = goog.module.get('os.proj');
+  const VectorSource = goog.module.get('os.source.Vector');
+  const TreeNode = goog.module.get('os.structs.TreeNode');
+  const XYZLayerConfig = goog.module.get('plugin.xyz.XYZLayerConfig');
+
   it('should filter falsey', function() {
-    var list = [1, true, 'thing', {label: 'Test'}, /matchy/i, [0], 0, false, null, undefined, ''].
-      filter(os.fn.filterFalsey);
+    var list = [1, true, 'thing', {label: 'Test'}, /matchy/i, [0], 0, false, null, undefined, '']
+        .filter(fn.filterFalsey);
 
     expect(list.length).toBe(6);
     expect(list).toContain(1);
@@ -20,30 +39,30 @@ describe('os.fn', function() {
 
   it('should reduce a combined extent from layers', function() {
     var layers = [];
-    var source = new os.source.Vector();
+    var source = new VectorSource();
     source.setId('a');
-    var layer = new os.layer.Vector();
+    var layer = new VectorLayer();
     layer.setSource(source);
-    source.addFeature(new ol.Feature(new ol.geom.Point([0, 0])));
-    source.addFeature(new ol.Feature(new ol.geom.Point([1, 1])));
+    source.addFeature(new Feature(new Point([0, 0])));
+    source.addFeature(new Feature(new Point([1, 1])));
     layers.push(layer);
 
-    var config = new plugin.xyz.XYZLayerConfig();
+    var config = new XYZLayerConfig();
     layers.push(config.createLayer({
       url: '/{z}/{y}/{x}.png',
       extent: [2, 2, 3, 3],
-      extentProjection: os.proj.EPSG4326
+      extentProjection: osProj.EPSG4326
     }));
 
-    source = new os.source.Vector();
+    source = new VectorSource();
     source.setId('b');
-    layer = new os.layer.Vector();
+    layer = new VectorLayer();
     layer.setSource(source);
-    source.addFeature(new ol.Feature(new ol.geom.Point([-1, -1])));
-    source.addFeature(new ol.Feature(new ol.geom.Point([-2, -2])));
+    source.addFeature(new Feature(new Point([-1, -1])));
+    source.addFeature(new Feature(new Point([-2, -2])));
     layers.push(layer);
 
-    var extent = layers.reduce(os.fn.reduceExtentFromLayers, ol.extent.createEmpty());
+    var extent = layers.reduce(fn.reduceExtentFromLayers, olExtent.createEmpty());
     expect(extent[0]).toBe(-2);
     expect(extent[1]).toBe(-2);
     expect(extent[2]).toBe(3);
@@ -52,8 +71,8 @@ describe('os.fn', function() {
 
   it('should reduce a combined extent from geometries', function() {
     var extent = [
-      new ol.geom.Point([-1, -1]),
-      new ol.geom.Point([-2, -2])].reduce(os.fn.reduceExtentFromGeometries, ol.extent.createEmpty());
+      new Point([-1, -1]),
+      new Point([-2, -2])].reduce(fn.reduceExtentFromGeometries, olExtent.createEmpty());
 
     expect(extent[0]).toBe(-2);
     expect(extent[1]).toBe(-2);
@@ -62,17 +81,17 @@ describe('os.fn', function() {
   });
 
   it('should map layers to sources', function() {
-    var source1 = new os.source.Vector({});
-    var layer1 = new os.layer.Vector({
+    var source1 = new VectorSource({});
+    var layer1 = new VectorLayer({
       source: source1
     });
 
-    var source2 = new ol.source.Image({});
-    var layer2 = new os.layer.Image({
+    var source2 = new olSourceImage({});
+    var layer2 = new Image({
       source: source2
     });
 
-    var sources = [layer1, null, layer2, undefined].map(os.fn.mapLayerToSource);
+    var sources = [layer1, null, layer2, undefined].map(fn.mapLayerToSource);
 
     expect(sources[0]).toBe(source1);
     expect(sources[1]).toBe(undefined);
@@ -81,15 +100,15 @@ describe('os.fn', function() {
   });
 
   it('should map a features to geometries', function() {
-    var f1 = new ol.Feature();
-    var g1 = new ol.geom.Point([1, 1]);
+    var f1 = new Feature();
+    var g1 = new Point([1, 1]);
     f1.setGeometry(g1);
 
-    var f2 = new ol.Feature();
-    var g2 = new ol.geom.Point([2, 2]);
+    var f2 = new Feature();
+    var g2 = new Point([2, 2]);
     f2.setGeometry(g2);
 
-    var geoms = [f1, null, f2, undefined].map(os.fn.mapFeatureToGeometry);
+    var geoms = [f1, null, f2, undefined].map(fn.mapFeatureToGeometry);
 
     expect(geoms[0]).toBe(g1);
     expect(geoms[1]).toBe(undefined);
@@ -98,16 +117,16 @@ describe('os.fn', function() {
   });
 
   it('should map nodes to layers', function() {
-    var node1 = new os.data.LayerNode();
-    var layer1 = new os.layer.Tile();
+    var node1 = new LayerNode();
+    var layer1 = new Tile();
     node1.setLayer(layer1);
 
-    var node2 = new os.data.LayerNode();
-    var layer2 = new os.layer.Vector();
+    var node2 = new LayerNode();
+    var layer2 = new VectorLayer();
     node2.setLayer(layer2);
 
-    var node3 = new os.structs.TreeNode();
-    var layers = [node1, null, node2, undefined, node3].map(os.fn.mapNodeToLayer);
+    var node3 = new TreeNode();
+    var layers = [node1, null, node2, undefined, node3].map(fn.mapNodeToLayer);
 
     expect(layers[0]).toBe(layer1);
     expect(layers[1]).toBe(undefined);
