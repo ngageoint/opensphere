1ae96aa1c0ca982f6c1bda2871dcecd16ff97cab
Kevin Schmidt <kschmidt@caci.com>
2021-07-21 17:05:43 -0600

    refactor(im): transform provider import ui to goog module

--- src/os/ui/providerimportui.js
+++ src/os/ui/providerimportui.js
@@ -1,77 +1,81 @@
-goog.provide('os.ui.ProviderImportUI');
-goog.require('os.data.DataManager');
-goog.require('os.parse.FileParserConfig');
-goog.require('os.ui.im.FileImportUI');
-goog.require('os.ui.window');
+goog.module('os.ui.ProviderImportUI');
+goog.module.declareLegacyNamespace();
 
+const DataManager = goog.require('os.data.DataManager');
+const FileParserConfig = goog.require('os.parse.FileParserConfig');
+const FileImportUI = goog.require('os.ui.im.FileImportUI');
+const osWindow = goog.require('os.ui.window');
 
 
 /**
- * @param {string} ui The directive to launch
- * @extends {os.ui.im.FileImportUI}
- * @constructor
  */
-os.ui.ProviderImportUI = function(ui) {
-  os.ui.ProviderImportUI.base(this, 'constructor');
+class ProviderImportUI extends FileImportUI {
+  /**
+   * Constructor.
+   * @param {string} ui The directive to launch
+   */
+  constructor(ui) {
+    super();
 
-  // file contents are only used in memory, not loaded from storage
-  this.requiresStorage = false;
+    // file contents are only used in memory, not loaded from storage
+    this.requiresStorage = false;
+
+    /**
+     * @type {string}
+     */
+    this.ui = ui;
+  }
 
   /**
-   * @type {string}
+   * @inheritDoc
    */
-  this.ui = ui;
-};
-goog.inherits(os.ui.ProviderImportUI, os.ui.im.FileImportUI);
+  launchUI(file, opt_config) {
+    super.launchUI(file, opt_config);
 
+    var config = new FileParserConfig();
 
-/**
- * @inheritDoc
- */
-os.ui.ProviderImportUI.prototype.launchUI = function(file, opt_config) {
-  os.ui.ProviderImportUI.base(this, 'launchUI', file, opt_config);
-
-  var config = new os.parse.FileParserConfig();
-
-  // if an existing config was provided, merge it in
-  if (opt_config) {
-    this.mergeConfig(opt_config, config);
-  }
-
-  var type = opt_config ? opt_config['type'] : file ? file.getType() : null;
-  var entry = os.dataManager.getProviderEntry(type);
-
-  if (this.ui) {
-    var provider = opt_config ? opt_config.provider : null;
-
-    if (provider) {
-      config['provider'] = provider;
-      config['enabled'] = provider.getEnabled();
-    } else {
-      config['file'] = file;
-      config['label'] = file.getFileName();
-      config['enabled'] = true;
+    // if an existing config was provided, merge it in
+    if (opt_config) {
+      this.mergeConfig(opt_config, config);
     }
 
-    var scopeOptions = {
-      'config': config
-    };
-    var windowOptions = {
-      'label': (provider ? provider.getEditable() ? 'Edit ' : 'View ' : 'Add ') + entry.title,
-      'icon': 'fa fa-database',
-      'x': 'center',
-      'y': 'center',
-      'width': '500',
-      'min-width': '350',
-      'max-width': '600',
-      'height': 'auto',
-      'min-height': '250',
-      'max-height': '500',
-      'modal': 'true',
-      'show-close': 'true'
-    };
-    os.ui.window.create(windowOptions, this.ui, undefined, undefined, undefined, scopeOptions);
-  } else {
-    // todo log some stuff, man
+    var type = opt_config ? opt_config['type'] : file ? file.getType() : null;
+    var entry = DataManager.getInstance().getProviderEntry(type);
+
+    if (this.ui) {
+      var provider = opt_config ? opt_config.provider : null;
+
+      if (provider) {
+        config['provider'] = provider;
+        config['enabled'] = provider.getEnabled();
+      } else {
+        config['file'] = file;
+        config['label'] = file.getFileName();
+        config['enabled'] = true;
+      }
+
+      var scopeOptions = {
+        'config': config
+      };
+      var windowOptions = {
+        'label': (provider ? provider.getEditable() ? 'Edit ' : 'View ' : 'Add ') + entry.title,
+        'icon': 'fa fa-database',
+        'x': 'center',
+        'y': 'center',
+        'width': '500',
+        'min-width': '350',
+        'max-width': '600',
+        'height': 'auto',
+        'min-height': '250',
+        'max-height': '500',
+        'modal': 'true',
+        'show-close': 'true'
+      };
+      osWindow.create(windowOptions, this.ui, undefined, undefined, undefined, scopeOptions);
+    } else {
+      // todo log some stuff, man
+    }
   }
-};
+}
+
+exports = ProviderImportUI;
