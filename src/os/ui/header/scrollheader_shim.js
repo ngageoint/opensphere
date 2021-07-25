e8528851335999671e3ff81475d161132d37d275
Kevin Schmidt <kschmidt@caci.com>
2021-07-25 16:03:17 -0600

    refactor(header): transform os.ui.header to goog modules

---
+++ src/os/ui/header/scrollheader_shim.js
@@ -0,0 +1,7 @@
+goog.provide('os.ui.header.scrollHeaderDirective');
+goog.require('os.ui.header.ScrollHeaderUI');
+
+/**
+ * @deprecated Please use goog.require('os.ui.header.ScrollHeaderUI').directive instead.
+ */
+os.ui.header.scrollHeaderDirective = os.ui.header.ScrollHeaderUI.directive;
---
+++ src/os/ui/header/scrollheaderevents.js
@@ -0,0 +1,11 @@
+goog.module('os.ui.header.ScrollHeaderEvents');
+goog.module.declareLegacyNamespace();
+
+/**
+ * @enum {string}
+ */
+exports = {
+  RESET: 'scrollheader.resetHeight',
+  STICK: 'scrollheader.stick',
+  UNSTICK: 'scrollheader.unstick'
+};
