e4e5b7eee8f21d43168393f701d131d97cc326a3
Kevin Schmidt <kschmidt@caci.com>
2021-08-10 17:14:35 -0600

    refactor(favorite): move global def and replace addSingletonGetter

---
+++ src/os/user/settings/favorite.js
@@ -0,0 +1,17 @@
+goog.module('os.user.settings.favorite');
+goog.module.declareLegacyNamespace();
+
+
+/**
+ * @typedef {{
+ *   children: Array<favorite>,
+ *   key: string,
+ *   time: number,
+ *   type: string,
+ *   value: string,
+ *   key2: string
+ *   }}
+ */
+let favorite;
+
+exports = favorite;
