b7645c598cdda2a10e357b528a41a25ae2ccf613
Kevin Schmidt <kschmidt@caci.com>
2021-08-10 15:10:56 -0600

    test(plugin): replace globals in os.plugin tests

--- test/os/plugin/plugin.mock.js
+++ test/os/plugin/plugin.mock.js
@@ -1,68 +1,41 @@
-goog.provide('os.plugin.MockErrorPlugin');
-goog.provide('os.plugin.MockPlugin');
+goog.module('os.plugin.MockPlugin');
+goog.module.declareLegacyNamespace();
 
-goog.require('goog.Promise');
-goog.require('os.plugin.AbstractPlugin');
-
+const Promise = goog.require('goog.Promise');
+const AbstractPlugin = goog.require('os.plugin.AbstractPlugin');
 
 
 /**
  * Mock plugin that loads after a timeout.
- * @param {Object=} opt_options Mock plugin options
- * @extends {os.plugin.AbstractPlugin}
- * @constructor
  */
-os.plugin.MockPlugin = function(opt_options) {
-  os.plugin.MockPlugin.base(this, 'constructor');
+class MockPlugin extends AbstractPlugin {
+  /**
+   * Constructor.
+   * @param {Object=} opt_options Mock plugin options
+   */
+  constructor(opt_options) {
+    super();
 
-  var options = opt_options || {};
+    var options = opt_options || {};
 
-  this.id = options.id || 'mock';
-  this.init_ = false;
-  this.errorMessage = null;
-  this.options = options;
-};
-goog.inherits(os.plugin.MockPlugin, os.plugin.AbstractPlugin);
-
-
-/**
- * @inheritDoc
- */
-os.plugin.MockPlugin.prototype.init = function() {
-  var initTimeout = this.options.initTimeout != null ? this.options.initTimeout : 10;
-
-  if (initTimeout > 0) {
-    return new goog.Promise(function(resolve, reject) {
-      setTimeout(resolve, initTimeout);
-    });
-  }
-};
-
-
-
-/**
- * Mock plugin the dispatches/throws errors.
- * @param {Object=} opt_options Mock plugin options
- * @constructor
- */
-os.plugin.MockErrorPlugin = function(opt_options) {
-  var options = opt_options || {};
-  options.id = options.id || 'error';
-
-  os.plugin.MockErrorPlugin.base(this, 'constructor', options);
-};
-goog.inherits(os.plugin.MockErrorPlugin, os.plugin.MockPlugin);
-
-
-/**
- * @inheritDoc
- */
-os.plugin.MockErrorPlugin.prototype.init = function() {
-  if (this.options.shouldThrow) {
-    throw new Error(this.options.errorMessage || 'fail');
+    this.id = options.id || 'mock';
+    this.init_ = false;
+    this.errorMessage = null;
+    this.options = options;
   }
 
-  return new goog.Promise(function(resolve, reject) {
-    setTimeout(reject, this.options.initTimeout || 10);
-  });
-};
+  /**
+   * @inheritDoc
+   */
+  init() {
+    var initTimeout = this.options.initTimeout != null ? this.options.initTimeout : 10;
+
+    if (initTimeout > 0) {
+      return new Promise(function(resolve, reject) {
+        setTimeout(resolve, initTimeout);
+      });
+    }
+  }
+}
+
+exports = MockPlugin;
