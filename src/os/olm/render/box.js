18ba000eb487fb33aa7000ee7342f6707d72d21b
Kevin Schmidt <kschmidt@caci.com>
2021-08-10 14:24:41 -0600

    refactor(olm): transform os.olm to goog modules

--- src/os/olm/render/box.js
+++ src/os/olm/render/box.js
@@ -1,73 +1,74 @@
-// FIXME add rotation
+/* FIXME add rotation */
+goog.module('os.olm.render.Box');
+goog.module.declareLegacyNamespace();
 
-goog.provide('os.olm.render.Box');
+const {beginTempInterpolation, endTempInterpolation, interpolateGeom} = goog.require('os.interpolate');
+const Method = goog.require('os.interpolate.Method');
+const BaseShape = goog.require('os.olm.render.BaseShape');
 
-goog.require('goog.Disposable');
-goog.require('ol.geom.Polygon');
-goog.require('ol.render.EventType');
-goog.require('os.interpolate');
-goog.require('os.olm.render.BaseShape');
+const Polygon = goog.requireType('ol.geom.Polygon');
+const Style = goog.requireType('ol.style.Style');
 
 
 /**
- * OL3 changed their box renderer to use the DOM, but that doesn't jive well with how we're using it. This class is a
- * modified copy of the pre-DOM ol.render.Box circa OL3 commit 31a68e21a51ee378c3698354523d185eef52f543.
- *
- * @param {ol.style.Style} style Style.
- * @extends {os.olm.render.BaseShape}
- * @constructor
+ * OL changed their box renderer to use the DOM, but that doesn't jive well with how we're using it. This class is a
+ * modified copy of the pre-DOM ol.render.Box circa OL commit 31a68e21a51ee378c3698354523d185eef52f543.
  */
-os.olm.render.Box = function(style) {
-  os.olm.render.Box.base(this, 'constructor', style);
+class Box extends BaseShape {
+  /**
+   * Constructor.
+   * @param {Style} style Style.
+   */
+  constructor(style) {
+    super(style);
+
+    /**
+     * @private
+     * @type {Polygon}
+     */
+    this.geometry_ = null;
+
+    /**
+     * @private
+     * @type {Polygon}
+     */
+    this.originalGeometry_ = null;
+  }
 
   /**
-   * @private
-   * @type {ol.geom.Polygon}
+   * @inheritDoc
    */
-  this.geometry_ = null;
+  getGeometry() {
+    return this.geometry_;
+  }
 
   /**
-   * @private
-   * @type {ol.geom.Polygon}
+   * @return {Polygon} Geometry
    */
-  this.originalGeometry_ = null;
-};
-goog.inherits(os.olm.render.Box, os.olm.render.BaseShape);
+  getOriginalGeometry() {
+    return this.originalGeometry_;
+  }
 
+  /**
+   * @param {!Polygon} geometry The geometry
+   */
+  updateGeometry(geometry) {
+    this.originalGeometry_ = geometry;
+    this.originalGeometry_.osTransform();
+    this.geometry_ = this.originalGeometry_.clone();
 
-/**
- * @inheritDoc
- */
-os.olm.render.Box.prototype.getGeometry = function() {
-  return this.geometry_;
-};
+    // "boxes" are really rhumb boxes, so ensure that we interpolate with the proper method.
+    // The interpolation ensures that the box segments are still rendered as rhumb lines even in
+    // projections in which rhumb lines are complex curves.
+    //
+    // Vertical and horizontal rhumbs in EPSG:4326 and EPSG:3857 are both straight lines, so the
+    // interpolation will be a no-op in those projections.
 
+    beginTempInterpolation(undefined, Method.RHUMB);
+    interpolateGeom(this.geometry_);
+    endTempInterpolation();
+    this.render();
+  }
+}
 
-/**
- * @return {ol.geom.Polygon} Geometry
- */
-os.olm.render.Box.prototype.getOriginalGeometry = function() {
-  return this.originalGeometry_;
-};
-
-
-/**
- * @param {!ol.geom.Polygon} geometry The geometry
- */
-os.olm.render.Box.prototype.updateGeometry = function(geometry) {
-  this.originalGeometry_ = geometry;
-  this.originalGeometry_.osTransform();
-  this.geometry_ = this.originalGeometry_.clone();
-
-  // "boxes" are really rhumb boxes, so ensure that we interpolate with the proper method.
-  // The interpolation ensures that the box segments are still rendered as rhumb lines even in
-  // projections in which rhumb lines are complex curves.
-  //
-  // Vertical and horizontal rhumbs in EPSG:4326 and EPSG:3857 are both straight lines, so the
-  // interpolation will be a no-op in those projections.
-
-  os.interpolate.beginTempInterpolation(undefined, os.interpolate.Method.RHUMB);
-  os.interpolate.interpolateGeom(this.geometry_);
-  os.interpolate.endTempInterpolation();
-  this.render();
-};
+exports = Box;
