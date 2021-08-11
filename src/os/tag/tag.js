c6c1a0d2700c3315281e13385a5b0e566944534d
Kevin Schmidt <kschmidt@caci.com>
2021-08-11 14:00:14 -0600

    refactor(tag): transform os.tag to goog modules

--- src/os/tag/tag.js
+++ src/os/tag/tag.js
@@ -1,47 +1,42 @@
-goog.provide('os.tag');
+goog.module('os.tag');
+goog.module.declareLegacyNamespace();
 
-goog.require('goog.dom');
-goog.require('goog.string');
-goog.require('ol.xml');
-goog.require('os.xml');
+const {getChildren} = goog.require('goog.dom');
+const {getAllTextContent} = goog.require('ol.xml');
+const {appendElement, createElement} = goog.require('os.xml');
 
 
 /**
  * The default root node to use when converting to XML.
  * @type {string}
- * @const
  */
-os.tag.DEFAULT_XML_ROOT = 'tags';
-
+const DEFAULT_XML_ROOT = 'tags';
 
 /**
  * The default tag node to use when converting to XML.
  * @type {string}
- * @const
  */
-os.tag.DEFAULT_XML_TAG = 'tag';
-
+const DEFAULT_XML_TAG = 'tag';
 
 /**
  * Converts a string to an array of tags.
  *
  * @param {?string} str The string
- * @return {Array.<string>} The tag array
+ * @return {Array<string>} The tag array
  */
-os.tag.tagsFromString = function(str) {
+const tagsFromString = function(str) {
   // trim the string first to remove leading/trailing whitespace from the tags. the regex will remove intermediate
   // whitespace around the commas.
-  return str ? goog.string.trim(str).split(/\s*,\s*/) : null;
+  return str ? str.trim().split(/\s*,\s*/) : null;
 };
-
 
 /**
  * Converts an array of tags to a string.
  *
- * @param {Array.<string>} tags The tag array
+ * @param {Array<string>} tags The tag array
  * @return {string} The tag string
  */
-os.tag.stringFromTags = function(tags) {
+const stringFromTags = function(tags) {
   if (tags) {
     return tags.join(', ');
   }
@@ -49,65 +44,72 @@ os.tag.stringFromTags = function(tags) {
   return '';
 };
 
-
 /**
  * Converts a DOM element to a tag string
  *
  * @param {Element} node The element
  * @return {string} The tag string
  */
-os.tag.stringFromXML = function(node) {
-  return os.tag.stringFromTags(os.tag.tagsFromXML(node));
+const stringFromXML = function(node) {
+  return stringFromTags(tagsFromXML(node));
 };
-
 
 /**
  * Converts a DOM element to an array of tags.
  *
  * @param {Element} node The element
- * @return {Array.<string>} The tag array
+ * @return {Array<string>} The tag array
  */
-os.tag.tagsFromXML = function(node) {
+const tagsFromXML = function(node) {
   var tags = null;
 
   if (node) {
-    var children = goog.dom.getChildren(node);
+    var children = getChildren(node);
     tags = [];
 
     for (var i = 0, n = children.length; i < n; i++) {
-      tags.push(ol.xml.getAllTextContent(children[i], true).trim());
+      tags.push(getAllTextContent(children[i], true).trim());
     }
   }
 
   return tags;
 };
 
-
 /**
  * Converts an array of tags to a DOM element.
  *
- * @param {Array.<string>|string} tags The tag array
+ * @param {Array<string>|string} tags The tag array
  * @param {string=} opt_tagName The tag name (defaults to 'tags')
  * @param {Document=} opt_doc The root document
  * @return {Element} The element
  */
-os.tag.xmlFromTags = function(tags, opt_tagName, opt_doc) {
+const xmlFromTags = function(tags, opt_tagName, opt_doc) {
   var t;
   if (typeof tags === 'string') {
-    t = os.tag.tagsFromString(tags);
+    t = tagsFromString(tags);
   } else if (tags) {
     t = tags;
   }
 
   if (t) {
-    var name = opt_tagName || os.tag.DEFAULT_XML_ROOT;
-    var tagEl = os.xml.createElement(name, opt_doc);
+    var name = opt_tagName || DEFAULT_XML_ROOT;
+    var tagEl = createElement(name, opt_doc);
     for (var i = 0, n = t.length; i < n; i++) {
-      os.xml.appendElement(os.tag.DEFAULT_XML_TAG, tagEl, t[i]);
+      appendElement(DEFAULT_XML_TAG, tagEl, t[i]);
     }
 
     return tagEl;
   }
 
   return null;
+};
+
+exports = {
+  DEFAULT_XML_ROOT,
+  DEFAULT_XML_TAG,
+  tagsFromString,
+  stringFromTags,
+  stringFromXML,
+  tagsFromXML,
+  xmlFromTags
 };
