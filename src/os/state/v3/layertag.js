8da696ce0fea4b5eff00119d9e451dee8de67bac
Kevin Schmidt <kschmidt@caci.com>
2021-08-11 08:23:53 -0600

    refactor(state): transform os.state.v3 to goog modules

---
+++ src/os/state/v3/layertag.js
@@ -0,0 +1,42 @@
+goog.module('os.state.v3.LayerTag');
+goog.module.declareLegacyNamespace();
+
+
+/**
+ * XML tags for layer state
+ * @enum {string}
+ */
+exports = {
+  ANIMATE: 'animate',
+  BFS: 'basicFeatureStyle',
+  COLOR: 'color',
+  CONTRAST: 'contrast',
+  CSV_COMMENT_CHAR: 'commentChar',
+  CSV_HEADER_ROW: 'headerRow',
+  CSV_DATA_ROW: 'dataRow',
+  CSV_USE_HEADER: 'useHeader',
+  DATA_PROVIDER: 'dataProvider',
+  IS_BASE_LAYER: 'isBaseLayer',
+  LABEL_COLUMN: 'labelColumn',
+  LABEL_COLUMNS: 'labelColumns',
+  LABEL: 'label',
+  LABEL_COLOR: 'labelColor',
+  LABEL_SIZE: 'labelSize',
+  MAP_URL: 'getMapUrl',
+  MAPPINGS: 'mappings',
+  PARAMS: 'params',
+  PARSER_CONFIG: 'parserConfig',
+  PROVIDER_TYPE: 'provider',
+  PT_COLOR: 'pointColor',
+  PT_OPACITY: 'pointOpacity',
+  PT_SIZE: 'pointSize',
+  TITLE: 'title',
+  SHOW_LABELS: 'showLabels',
+  STYLES: 'styles',
+  TAGS: 'tags',
+  TIME_ENABLED: 'timeEnabled',
+  USE_HEADER: 'useHeader',
+  REFRESH_RATE: 'refreshRate',
+  OPACITY: 'opacity',
+  ALPHA: 'alpha'
+};
