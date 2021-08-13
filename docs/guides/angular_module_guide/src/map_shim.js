0f9cae07f88a3cbeba5d71d876f0299f6c7577f2
Kevin Schmidt <kschmidt@caci.com>
2021-08-13 09:08:59 -0600

    refactor(map): remove map ui shim

--- docs/guides/angular_module_guide/src/map_shim.js
+++ docs/guides/angular_module_guide/src/map_shim.js
@@ -1,14 +1,14 @@
 goog.provide('os.ui.MapCtrl');
 goog.provide('os.ui.mapDirective');
 
-goog.require('os.ui.MapUI');
+goog.require('os.ui.Map');
 
 /**
- * @deprecated Please use goog.require('os.ui.MapUI').Controller instead.
+ * @deprecated Please use goog.require('os.ui.Map').Controller instead.
  */
-os.ui.MapCtrl = os.ui.MapUI.Controller;
+os.ui.MapCtrl = os.ui.Map.Controller;
 
 /**
- * @deprecated Please use goog.require('os.ui.MapUI').directive instead.
+ * @deprecated Please use goog.require('os.ui.Map').directive instead.
  */
-os.ui.mapDirective = os.ui.MapUI.directive;
+os.ui.mapDirective = os.ui.Map.directive;
