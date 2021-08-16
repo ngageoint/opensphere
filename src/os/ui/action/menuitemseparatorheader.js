d22dbe7e5ffdb45d7f5fd63c7b1577a74a00ed16
Kevin Schmidt <kschmidt@caci.com>
2021-08-16 07:46:23 -0600

    fix(action): deprecate separator and suppress internal warning

--- src/os/ui/action/menuitemseparatorheader.js
+++ src/os/ui/action/menuitemseparatorheader.js
@@ -8,11 +8,13 @@ const IMenuItem = goog.requireType('os.ui.action.IMenuItem');
  * Represents a menu divider with a name
  *
  * @implements {IMenuItem}
+ * @deprecated Please use os.ui.menu.MenuItemType.SEPARATOR instead.
  */
 class MenuItemSeparatorHeader extends MenuItem {
   /**
    * Constructor.
    * @param {string} name The name of the header
+   * @suppress {deprecated}
    */
   constructor(name) {
     super(name, null);
