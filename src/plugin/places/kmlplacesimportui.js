29eeb80d24c2ddaa36bb584dea456f729b748d04
Kevin Schmidt <kschmidt@caci.com>
2021-11-12 13:03:05 -0700

    feat(places): directly import kml into places

--- src/plugin/places/kmlplacesimportui.js
+++ src/plugin/places/kmlplacesimportui.js
@@ -1,19 +1,13 @@
 goog.declareModuleId('plugin.places.KMLPlacesImportUI');
 
 import EventType from '../../os/events/eventtype.js';
-import {ROOT} from '../../os/os.js';
-import FileParserConfig from '../../os/parse/fileparserconfig.js';
-import {Controller as FileImportCtrl} from '../../os/ui/file/fileimport.js';
 import FileImportUI from '../../os/ui/im/fileimportui.js';
-import Module from '../../os/ui/module.js';
-import {create} from '../../os/ui/window.js';
 import KMLImporter from '../file/kml/kmlimporter.js';
 import KMLParser from '../file/kml/kmlparser.js';
 import {getPlacesManager} from './places.js';
 import {saveKMLToPlaces} from './placessave.js';
 
-const dispose = goog.require('goog.dispose');
-
+const GoogEvent = goog.requireType('goog.events.Event');
 const {default: KMLNode} = goog.requireType('plugin.file.kml.ui.KMLNode');
 
 
@@ -28,19 +22,6 @@ export default class KMLPlacesImportUI extends FileImportUI {
 
     // file contents are only used in memory, not loaded from storage
     this.requiresStorage = false;
-
-    /**
-     * @type {?string}
-     * @protected
-     */
-    this.fileName = '';
-
-    /**
-     * The parser config.
-     * @type {FileParserConfig}
-     * @protected
-     */
-    this.config = null;
   }
 
   /**
@@ -54,121 +35,28 @@ export default class KMLPlacesImportUI extends FileImportUI {
    * @inheritDoc
    */
   launchUI(file, opt_config) {
-    super.launchUI(file, opt_config);
-
-    var config = new FileParserConfig();
-    config['file'] = file;
-    config['title'] = file.getFileName() || '';
-
-    var callback = goog.partial(this.onPreviewReady_, config);
-    var parser = new KMLParser({});
-    parser.listenOnce(EventType.COMPLETE, callback, false, this);
-    parser.listenOnce(EventType.ERROR, callback, false, this);
-    parser.setSource(file.getContent());
-  }
-
-  /**
-   * @param {FileParserConfig} config
-   * @param {goog.events.Event} event
-   * @private
-   */
-  onPreviewReady_(config, event) {
-    var parser = /** @type {KMLParser} */ (event.target);
-    var preview = parser.parseNext();
-    var columns = parser.getColumns() || [];
-    dispose(parser);
-
-    this.config = config;
-    config['columns'] = columns;
-    config['preview'] = preview;
-
-    var scopeOptions = {
-      'config': config
-    };
-    var windowOptions = {
-      'label': 'KML Places Import',
-      'icon': 'fa fa-sign-in',
-      'x': 'center',
-      'y': 'center',
-      'width': '450',
-      'min-width': '300',
-      'max-width': '800',
-      'height': 'auto',
-      'modal': 'true',
-      'show-close': 'true'
-    };
-    var template = '<kmlplaces></kmlplaces>';
-    create(windowOptions, template, undefined, undefined, undefined, scopeOptions);
-  }
-}
-
-
-/**
- * The KML places import directive
- *
- * @return {angular.Directive}
- */
-const kmlPlacesDirective = function() {
-  return {
-    restrict: 'E',
-    templateUrl: ROOT + 'views/plugin/kml/kmlplaces.html',
-    controller: Controller,
-    controllerAs: 'placesFiles'
-  };
-};
-
-
-/**
- * Add the directive to the module
- */
-Module.directive('kmlplaces', [kmlPlacesDirective]);
-
-
-
-/**
- * Controller for the SHP import file selection step
- * @unrestricted
- */
-class Controller extends FileImportCtrl {
-  /**
-   * Constructor.
-   * @param {!angular.Scope} $scope
-   * @param {!angular.JQLite} $element
-   * @param {!angular.$timeout} $timeout The Angular $timeout service.
-   * @ngInject
-   */
-  constructor($scope, $element, $timeout) {
-    super($scope, $element, $timeout);
-    this.config = $scope.config;
-  }
-
-  /**
-   * @export
-   */
-  finish() {
+    // A KML tree can be dropped into Saved Places without user intervention, so parse the file and add it.
     const parser = new KMLParser({});
     const importer = new KMLImporter(parser);
-    importer.listenOnce(EventType.COMPLETE, this.onImportComplete_, false, this);
-    importer.startImport(this.config['file'].getContent());
+    importer.listenOnce(EventType.COMPLETE, this.onImportComplete, false, this);
+    importer.startImport(file.getContent());
   }
 
   /**
-   * Success callback for importing data.
-   *
-   * @param {goog.events.Event} event
-   * @private
+   * Handle KML import success.
+   * @param {GoogEvent} event The event.
+   * @protected
    */
-  onImportComplete_(event) {
+  onImportComplete(event) {
     const importer = /** @type {KMLImporter} */ (event.target);
-    const rootNode = getImportRoot(importer.getRootNode());
+    const importNodes = getImportRoot(importer.getRootNode());
     const placesManager = getPlacesManager();
     const placesRoot = placesManager ? placesManager.getPlacesRoot() : null;
-    if (rootNode && placesRoot) {
-      saveKMLToPlaces(rootNode);
+    if (importNodes && placesRoot) {
+      saveKMLToPlaces(importNodes);
     }
 
     importer.dispose();
-    this.close();
   }
 }
 
@@ -178,6 +66,7 @@ class Controller extends FileImportCtrl {
  * @return {KMLNode}
  */
 const getImportRoot = (node) => {
+  // Ignore the root 'kml' node and the Saved Places root node, as long as the node only has a single child.
   if (node && node.getLabel() === 'kmlroot' || node.getLabel() === 'Saved Places') {
     const children = node.getChildren();
     if (children && children.length === 1) {
