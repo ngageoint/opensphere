8aa8a75a657c0dfdaa682e8f63fdec25516257b1
Brandon Cleveland <bcleveland1022@gmail.com>
2022-02-07 10:39:52 -0700

    fix: switch to import statements for new OL

--- src/os/ui/ol/interaction/dragcircleinteraction.js
+++ src/os/ui/ol/interaction/dragcircleinteraction.js
@@ -1,5 +1,6 @@
 goog.declareModuleId('os.ui.ol.interaction.DragCircle');
 
+import {toLonLat} from 'ol/proj';
 import RecordField from '../../../data/recordfield.js';
 import {normalizeGeometryCoordinates} from '../../../geo/geo2.js';
 import {METHOD_FIELD, getMethod} from '../../../interpolate.js';
@@ -7,9 +8,6 @@ import Units from '../../../math/units.js';
 import Circle from '../../../olm/render/circle.js';
 import AltitudeMode from '../../../webgl/altitudemode.js';
 import AbstractDrag from './abstractdraginteraction.js';
-
-const {toLonLat} = goog.require('ol.proj');
-
 
 /**
  * Draws a circular query area on the map.
