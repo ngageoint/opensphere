d106e9074d1601c36a2f1665648ffb68b98cf114
Brandon Cleveland <bcleveland1022@gmail.com>
2022-02-07 10:23:35 -0700

    fix: switch to import statements for new OL

--- src/os/ui/ol/interaction/interaction.js
+++ src/os/ui/ol/interaction/interaction.js
@@ -1,9 +1,9 @@
 goog.declareModuleId('os.ui.ol.interaction');
 
+import EventType from 'ol/events/EventType';
 import {isGeometryPolygonal} from '../../../geo/geo.js';
 
 const userAgent = goog.require('goog.userAgent');
-const EventType = goog.require('ol.events.EventType');
 
 const Feature = goog.requireType('ol.Feature');
 const MapBrowserEvent = goog.requireType('ol.MapBrowserEvent');
