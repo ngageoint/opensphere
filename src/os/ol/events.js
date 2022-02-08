13b69959e1ab74745dabcad85de14bfa5430a178
Kevin Gilland <kgilland@caci.com>
2022-02-08 06:00:44 -0700

    fix: switching to ol, and removing unused method

--- src/os/ol/events.js
+++ src/os/ol/events.js
@@ -1,6 +1,6 @@
 goog.declareModuleId('os.ol.events');
 
-const {listen, unlisten} = goog.require('ol.events');
+import {listen} from 'ol/events';
 
 
 /**
@@ -19,18 +19,4 @@ export const listenEach = function(target, types, listener, opt_this, opt_once) 
     keys.push(listen(target, types[i], listener, opt_this, opt_once));
   }
   return keys;
-};
-
-/**
- * Unregisters event listeners on an event target for a list of events.
- *
- * @param {ol.EventTargetLike} target Event target.
- * @param {!Array<string>} types Event types.
- * @param {ol.EventsListenerFunctionType} listener Listener.
- * @param {Object=} opt_this Object referenced by the `this` keyword in the listener. Default is the `target`.
- */
-export const unlistenEach = function(target, types, listener, opt_this) {
-  for (var i = 0; i < types.length; i++) {
-    unlisten(target, types[i], listener, opt_this);
-  }
 };
