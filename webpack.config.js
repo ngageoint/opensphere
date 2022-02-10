9f073441a6d2ae8460d009208cd8853855850816
Brandon Cleveland <bcleveland1022@gmail.com>
2022-02-10 06:32:34 -0700

    fix: add back the closure library plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const ClosurePlugin = require('@ngageoint/closure-webpack-plugin');
 const path = require('path');
 
 const buildDir = path.resolve(__dirname, '.build');
+const gccOptions = require(path.join(buildDir, 'gcc-webpack'));
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production' || !argv.mode;
+  const depsFile = path.join(buildDir, 'deps.js');
 
   return {
     entry: [
@@ -27,6 +30,15 @@ module.exports = (env, argv) => {
       hints: isProduction ? 'warning' : false,
       maxAssetSize: 5000000,
       maxEntrypointSize: 5000000
-    }
+    },
+    plugins: [
+      new ClosurePlugin.LibraryPlugin({
+        closureLibraryBase: require.resolve('google-closure-library/closure/goog/base'),
+        deps: [
+          require.resolve('google-closure-library/closure/goog/deps'),
+          depsFile
+        ]
+      })
+    ]
   };
 };
