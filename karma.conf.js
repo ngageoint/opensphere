35c32b88449284a48fbcc168c57da1514d34fe1e
Brandon Cleveland <bcleveland1022@gmail.com>
2022-03-07 10:29:59 -0700

    fix: add requirejs to the tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
     basePath: '',
 
     // frameworks to use
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'requirejs'],
 
     // list of files / patterns to load in the browser
     files: [
