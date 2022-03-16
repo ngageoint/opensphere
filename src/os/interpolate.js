af7b18d64dbff41bbb4ebe3c39e46645b1bdf1f4
Kevin Gilland <kgilland@caci.com>
2022-03-16 07:21:40 -0600

    fix: adding back in commented out code

--- src/os/interpolate.js
+++ src/os/interpolate.js
@@ -5,16 +5,13 @@
  */
 goog.declareModuleId('os.interpolate');
 
+import GeometryType from 'ol/src/geom/GeometryType';
+import {get, getTransform} from 'ol/src/proj';
+
 import * as geo2 from './geo/geo2.js';
 import Method from './interpolatemethod.js';
-// import * as osMap from './map/map.js';
+import * as osMap from './map/map.js';
 import {EPSG4326} from './proj/proj.js';
-
-/* import GeometryType from 'ol.geom.GeometryType';
-import olProj from 'ol.proj'); */
-
-const {default: Config} = goog.requireType('os.interpolate.Config');
-
 
 /**
  * Interpolation settings keys.
@@ -129,15 +126,15 @@ export const beginTempInterpolation = function(opt_projection, opt_method, opt_d
     distance: opt_distance || config.distance
   };
 
-  /* if (opt_projection) {
-     var projection = olProj.get(opt_projection || osMap.PROJECTION);
+  if (opt_projection) {
+    var projection = get(opt_projection || osMap.PROJECTION);
 
     tmpTransforms = ({
-      coordToLonLat: olProj.getTransform(projection, EPSG4326),
-      lonLatToCoord: olProj.getTransform(EPSG4326, projection),
+      coordToLonLat: getTransform(projection, EPSG4326),
+      lonLatToCoord: getTransform(EPSG4326, projection),
       lastProj: projection.getCode()
     });
-  } */
+  }
 };
 
 /**
@@ -185,10 +182,10 @@ export const interpolateFeature = function(feature, opt_skipUpdate) {
     }
 
     // if the geometry is something we clearly can't interpolate, then skip it
-    /* var type = geom.getType();
+    var type = geom.getType();
     if (type === GeometryType.POINT || type === GeometryType.MULTI_POINT) {
       return;
-    } */
+    }
 
     if (geom) {
       feature.set(ORIGINAL_GEOM_FIELD, geom.clone());
@@ -231,49 +228,49 @@ export const interpolateGeom = function(geom, opt_skipUpdate) {
     return;
   }
 
-  /* var type = geom.getType();
+  var type = geom.getType();
   switch (type) {
     case GeometryType.GEOMETRY_COLLECTION:
-      var geoms = /** @type {ol.geom.GeometryCollection} */ // (geom).getGeometries();
-  /* geoms.forEach(interpolateGeomInternal_);
-      /** @type {ol.geom.GeometryCollection} */ // geom).setGeometries(geoms);
-  /* break;
+      var geoms = /** @type {ol.geom.GeometryCollection} */ (geom).getGeometries();
+      geoms.forEach(interpolateGeomInternal_);
+      /** @type {ol.geom.GeometryCollection} */ (geom).setGeometries(geoms);
+      break;
     case GeometryType.MULTI_POLYGON:
-      var polys = /** @type {ol.geom.MultiPolygon} */ // (geom).getCoordinates();
-  /* polys.forEach(interpolateRings);
-      /** @type {ol.geom.MultiPolygon} */ // (geom).setCoordinates(polys);
-  /* break;
+      var polys = /** @type {ol.geom.MultiPolygon} */ (geom).getCoordinates();
+      polys.forEach(interpolateRings);
+      /** @type {ol.geom.MultiPolygon} */ (geom).setCoordinates(polys);
+      break;
     case GeometryType.MULTI_LINE_STRING:
-      var lines = /** @type {ol.geom.MultiLineString} */ // (geom).getCoordinates();
-  /* lines.forEach(interpolateLine);
-      /** @type {ol.geom.MultiLineString} */ // (geom).setCoordinates(lines);
-  /* break;
+      var lines = /** @type {ol.geom.MultiLineString} */ (geom).getCoordinates();
+      lines.forEach(interpolateLine);
+      /** @type {ol.geom.MultiLineString} */ (geom).setCoordinates(lines);
+      break;
     case GeometryType.POLYGON:
-      var rings = /** @type {ol.geom.Polygon} */ // (geom).getCoordinates();
-  /* rings.forEach(interpolateRing);
-      /** @type {ol.geom.Polygon} */ // (geom).setCoordinates(rings);
-  /* break;
+      var rings = /** @type {ol.geom.Polygon} */ (geom).getCoordinates();
+      rings.forEach(interpolateRing);
+      /** @type {ol.geom.Polygon} */ (geom).setCoordinates(rings);
+      break;
     case GeometryType.LINE_STRING:
-      var coords = /** @type {ol.geom.LineString} */ // (geom).getCoordinates();
-  /* interpolateLine(coords);
-      /** @type {ol.geom.LineString} */ /* (geom).setCoordinates(coords);
+      var coords = /** @type {ol.geom.LineString} */ (geom).getCoordinates();
+      interpolateLine(coords);
+      /** @type {ol.geom.LineString} */ (geom).setCoordinates(coords);
       break;
     default:
       break;
-  } */
+  }
 };
 
 /**
  * @param {ol.geom.Geometry} geom The geometry to interpolate
  */
-/* const interpolateGeomInternal_ = function(geom) {
+const interpolateGeomInternal_ = function(geom) {
   interpolateGeom(geom, true);
-}; */
+};
 
 /**
  * @type {?TransformSet}
  */
-const localTransforms = null;
+let localTransforms = null;
 
 /**
  * @type {?TransformSet}
@@ -286,13 +283,13 @@ let tmpTransforms = null;
  * @suppress {accessControls}
  */
 export const updateTransforms = function() {
-  /* if (!localTransforms || osMap.PROJECTION.getCode() !== localTransforms.lastProj) {
+  if (!localTransforms || osMap.PROJECTION.getCode() !== localTransforms.lastProj) {
     localTransforms = {
-      coordToLonLat: olProj.getTransform(osMap.PROJECTION, EPSG4326),
-      lonLatToCoord: olProj.getTransform(EPSG4326, osMap.PROJECTION),
+      coordToLonLat: getTransform(osMap.PROJECTION, EPSG4326),
+      lonLatToCoord: getTransform(EPSG4326, osMap.PROJECTION),
       lastProj: osMap.PROJECTION.getCode()
     };
-  } */
+  }
 };
 
 /**
