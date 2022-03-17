4d325c3913086de2225d81e54fd261273fac35b5
Kevin Gilland <kgilland@caci.com>
2022-03-17 10:37:21 -0600

    fix: correcting geojson writing by using base class

--- src/plugin/file/geojson/geojsonmixin.js
+++ src/plugin/file/geojson/geojsonmixin.js
@@ -8,7 +8,6 @@ import GeoJSON from 'ol/src/format/GeoJSON';
  */
 let initialized = false;
 
-
 /**
  * Initialize the mixin.
  */
@@ -16,48 +15,40 @@ export const init = () => {
   if (!initialized) {
     initialized = true;
 
-    /**
-     * Encode a feature as a GeoJSON Feature object.
-     *
-     * @param {Feature} feature Feature.
-     * @param {olx.format.WriteOptions=} opt_options Write options.
-     * @return {GeoJSONFeature} Object.
-     * @override
-     * @suppress {accessControls,duplicate}
-     */
-    GeoJSON.prototype.writeFeatureObject = function(feature, opt_options) {
-      opt_options = this.adaptOptions(opt_options);
 
-      var object = /** @type {GeoJSONFeature} */ ({
-        'type': 'Feature'
-      });
-      var id = feature.getId();
-      if (id !== undefined) {
-        object.id = id;
-      }
-      var geometry = feature.getGeometry();
-      if (geometry) {
-        object.geometry = this.writeGeometry(geometry, opt_options);
-      } else {
-        object.geometry = null;
-      }
+    (function() {
+      const oldWriteFeatureObject = GeoJSON.prototype.writeFeatureObject;
 
-      var properties = feature.values_;
-      var fields = opt_options.fields;
-      for (var key in properties) {
+
+      /**
+       * Encode a feature as a GeoJSON Feature object.
+       *
+       * @param {Feature} feature Feature.
+       * @param {olx.format.WriteOptions=} opt_options Write options.
+       * @return {GeoJSONFeature} Object.
+       * @override
+       * @suppress {accessControls,duplicate}
+       */
+      GeoJSON.prototype.writeFeatureObject = function(feature, opt_options) {
+        const object = oldWriteFeatureObject.call(this, feature, opt_options);
+
+        var properties = feature.values_;
+        var fields = opt_options.fields;
+        for (var key in properties) {
         // exclude private keys, any fields passed in, and any non-primitive value
-        if ((opt_options.includePrivateFields || key.indexOf('_') !== 0) &&
+          if ((opt_options.includePrivateFields || key.indexOf('_') !== 0) &&
             ((!fields || fields.indexOf(key) > -1) &&
             Object(properties[key]) !== properties[key])) {
-          if (!object.properties) {
-            object.properties = {};
+            if (!object.properties) {
+              object.properties = {};
+            }
+
+            object.properties[key] = properties[key];
           }
-
-          object.properties[key] = properties[key];
         }
-      }
 
-      return object;
-    };
+        return object;
+      };
+    })();
   }
 };
